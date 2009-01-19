17555d151c6f6198e2f24fb263a758830b8d6b86
fay-pain <fay-pain@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-01-19 23:14:32 +0000

    lost in translation, added missing lang var (eng/comments)

--- lang/English/comments.php
+++ lang/English/comments.php
@@ -43,5 +43,6 @@ $cs_lang['nick_exists'] = '- Your guestnick is already a registered username.';
 
 //Options
 $cs_lang['show_avatars'] = 'Show avatars';
+$cs_lang['allow_unreg'] = 'Allow visitors to write comments';
 
 ?>
