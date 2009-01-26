1b55014c513d17cb79fad82eb1aa4924740d0f5a
fay-pain <fay-pain@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-01-26 14:48:11 +0000

    added theme to newsletter/manage

--- mods/newsletter/manage.php
+++ mods/newsletter/manage.php
@@ -1,76 +1,47 @@
-<? 
+<?php
+// ClanSphere 2008 - www.clansphere.net
+// $Id$
 
 $cs_lang = cs_translate('newsletter');
+$cs_post = cs_post('start,sort');
+$cs_get = cs_get('start,sort');
+$data = array();
 
-empty($_REQUEST['start']) ? $start = 0 : $start = $_REQUEST['start'];
+$start = empty($cs_get['start']) ? 0 : $cs_get['start'];
+if (!empty($cs_post['start']))  $start = $cs_post['start'];
+$sort = empty($cs_get['sort']) ? 1 : $cs_get['sort'];
+if (!empty($cs_post['sort']))  $sort = $cs_post['sort'];
+
 $cs_sort[1] = 'newsletter_time DESC';
 $cs_sort[2] = 'newsletter_time ASC'; 
 $cs_sort[3] = 'users_nick DESC';
 $cs_sort[4] = 'users_nick ASC';
-empty($_REQUEST['sort']) ? $sort = 1 : $sort = $_REQUEST['sort'];
 $order = $cs_sort[$sort];
 $newsletter_count = cs_sql_count(__FILE__,'newsletter');
 
 
-echo cs_html_table(1,'forum',1);
-echo cs_html_roco(1,'headb',0,3);
-echo $cs_lang['head_manage'];
-echo cs_html_roco(0);
-echo cs_html_roco(1,'leftb');
-echo cs_icon('editpaste') . cs_link($cs_lang['new_manage'],'newsletter','create');
-echo cs_html_roco(2,'leftb');
-echo cs_icon('contents') . $cs_lang['total'] . ': ' . $newsletter_count;
-echo cs_html_roco(2,'rightb');
-echo cs_pages('newsletter','manage',$newsletter_count,$start,0,$sort);
-echo cs_html_roco(0);
+$data['head']['count'] = $newsletter_count;
+$data['head']['pages'] = cs_pages('newsletter','manage',$newsletter_count,$start,0,$sort);
+$data['head']['getmsg'] = cs_getmsg();
+
+$data['sort']['user'] = cs_sort('newsletter','manage',$start,0,3,$sort);
+$data['sort']['date'] = cs_sort('newsletter','manage',$start,0,1,$sort);
 
 
-//Tabele
-echo cs_html_roco(0);
-echo cs_html_table(0);
-echo cs_html_br(1);
-
-echo cs_getmsg();
-
-echo cs_html_table(1,'forum',1);
-//echo cs_html_roco(1,'headb');
-//echo $cs_lang['to'];
-echo cs_html_roco(1,'headb');
-echo cs_sort('newsletter','manage',$start,0,3,$sort);
-echo $cs_lang['from'];
-echo cs_html_roco(2,'headb');
-echo $cs_lang['subject'];
-echo cs_html_roco(3,'headb');
-echo cs_sort('newsletter','manage',$start,0,1,$sort);
-echo $cs_lang['date'];
-echo cs_html_roco(4,'headb',1,2);
-echo $cs_lang['options'];
-echo cs_html_roco(0);
-
-//sql
 $from = 'newsletter nwl INNER JOIN {pre}_users usr ON nwl.users_id = usr.users_id ';
-$select = 'nwl.newsletter_id AS newsletter_id, nwl.newsletter_subject AS newsletter_subject, nwl.newsletter_time AS newsletter_time, nwl.newsletter_text AS newsletter_text, nwl.newsletter_to AS newsletter_to, nwl.users_id AS users_id, usr.users_nick AS users_nick';
-$cs_newsletter = cs_sql_select(__FILE__,$from,$select,0,$order,$start,$account['users_limit']);
+$select = 'nwl.newsletter_id AS newsletter_id, nwl.newsletter_subject AS newsletter_subject, nwl.newsletter_time AS newsletter_time, nwl.users_id AS users_id, usr.users_nick AS users_nick, usr.users_active AS users_active';
+$data['newsletter'] = cs_sql_select(__FILE__,$from,$select,0,$order,$start,$account['users_limit']);
+$newsletter_loop = count($data['newsletter']);
 
-$newsletter_loop = count($cs_newsletter);
-for($run=0; $run<$newsletter_loop; $run++) 
+for($run=0; $run<$newsletter_loop; $run++) {
 
-{
-	//echo cs_html_roco(1,'leftc');
-	//echo $cs_newsletter[$run]['newsletter_to'];
-	echo cs_html_roco(1,'leftc');
-	echo $cs_newsletter[$run]['users_nick'];
-	echo cs_html_roco(2,'leftc');
-	echo $cs_newsletter[$run]['newsletter_subject'];
-	echo cs_html_roco(3,'leftc');
-	echo cs_date('unix',$cs_newsletter[$run]['newsletter_time'],1);
-	echo cs_html_roco(4,'centerc');
-	$newsletter_id = $cs_newsletter[$run]['newsletter_id'];
-	echo cs_link(cs_icon('info'),'newsletter','view','id='.$newsletter_id); 
-	echo cs_html_roco(4,'centerc'); 
-	echo cs_link(cs_icon('cancel'),'newsletter','remove','id=' .$newsletter_id);
-	echo cs_html_roco(0);
+	$data['newsletter'][$run]['user'] = cs_user($data['newsletter'][$run]['users_id'],$data['newsletter'][$run]['users_nick'],$data['newsletter'][$run]['users_active']);
+	$data['newsletter'][$run]['subject'] = $data['newsletter'][$run]['newsletter_subject'];
+	$data['newsletter'][$run]['date'] = cs_date('unix',$data['newsletter'][$run]['newsletter_time'],1);
+	$data['newsletter'][$run]['id'] = $data['newsletter'][$run]['newsletter_id'];
+
 }
 
-echo cs_html_table(0);
+echo cs_subtemplate(__FILE__,$data,'newsletter','manage');
+
 ?>
---
+++ themes/base/newsletter/manage.tpl
@@ -0,0 +1,31 @@
+<table class="forum" style="width:{page:width}" cellpadding="0" cellspacing="{page:cellspacing}">
+	<tr>
+		<td class="headb" colspan="3">{lang:mod} - {lang:manage}</td>
+	</tr>
+	<tr>
+		<td class="leftb">{icon:editpaste} <a href="{url:newsletter_create}">{lang:new_manage}</a></td>
+		<td class="leftb">{icon:contents} {lang:total}: {head:count}</td>
+		<td class="rightb">{head:pages}</td>
+	</tr>
+</table>
+<br />
+
+{head:getmsg}
+
+<table class="forum" style="width:{page:width}" cellpadding="0" cellspacing="{page:cellspacing}">
+	<tr>
+		<td class="headb">{sort:user} {lang:from}</td>
+		<td class="headb">{lang:subject}</td>
+		<td class="headb">{sort:date} {lang:date}</td>
+		<td class="headb" colspan="2" rowspan="1">{lang:options}</td>
+	</tr>
+	{loop:newsletter}
+	<tr>
+		<td class="leftc">fay-pain</td>
+		<td class="leftc">test</td>
+		<td class="leftc">26.01.2009 um 15:24 Uhr</td>
+		<td class="centerc"><a href="{url:newsletter_view:id={newsletter:id}}" >{icon:info}</a></td>
+		<td class="centerc"><a href="{url:newsletter_remove:id={newsletter:id}}" >{icon:editdelete}</a></td>
+	</tr>
+	{stop:newsletter}
+</table>
