e330af8784192a2d6ec7b11690422a7354d67356
fay-pain <fay-pain@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-02-10 22:30:56 +0000

    added php dir fix for win os (thx@SaSa, thread#9421)

--- system/core/content.php
+++ system/core/content.php
@@ -42,6 +42,12 @@ if(empty($cs_main['def_path'])) {
 
 $cs_main['php_self'] = pathinfo($_SERVER['PHP_SELF']);
 $cs_main['php_self']['dirname'] = $cs_main['php_self']['dirname'] == '/' ? '/' : $cs_main['php_self']['dirname'] . '/';
+
+if (stristr(PHP_OS, 'WIN')) {
+    $cs_main['php_self']['dirname'] = str_replace($cs_main['php_self']['dirname'], '\\' , '');
+    $cs_main['php_self']['dirname'] = str_replace($cs_main['php_self']['dirname'], '\/\/' , '/');
+}
+
 $_SERVER['PHP_SELF'] = htmlentities($_SERVER['PHP_SELF'], ENT_QUOTES);
 
 $cs_main['template'] = $cs_main['def_tpl'];
