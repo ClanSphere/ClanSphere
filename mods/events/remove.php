a3e1df029f5f4cbeebd1a941f0eb087b28bf9d5b
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-02-22 15:40:20 +0000

    events remove now uses themes

--- mods/events/remove.php
+++ mods/events/remove.php
@@ -4,37 +4,29 @@
 
 $cs_lang = cs_translate('events');
 
-echo cs_html_table(1,'forum',1);
-echo cs_html_roco(1,'headb');
-echo $cs_lang['mod'] . ' - ' . $cs_lang['head_remove'];
-echo cs_html_roco(0);
-
 $events_form = 1;
 $events_id = $_REQUEST['id'];
+settype($events_id,'integer');
 
-if(isset($_POST['agree'])) {
+if(isset($_GET['agree'])) {
   $events_form = 0;
   cs_sql_delete(__FILE__,'events',$events_id);
-  cs_redirect($cs_lang['del_true'], 'events');
+
+ cs_redirect($cs_lang['del_true'], 'events');
 }
 
-if(isset($_POST['cancel'])) 
-  cs_redirect($cs_lang['del_false'], 'events');
+if(isset($_GET['cancel'])) 
+    cs_redirect($cs_lang['del_false'], 'events');
 
 if(!empty($events_form)) {
-
-  echo cs_html_roco(1,'leftb');
-  echo sprintf($cs_lang['del_rly'],$events_id);
-  echo cs_html_roco(0);
-
-  echo cs_html_roco(1,'centerc');
-  echo cs_html_form(1,'events_remove','events','remove');
-  echo cs_html_vote('id',$events_id,'hidden');
-  echo cs_html_vote('agree',$cs_lang['confirm'],'submit');
-  echo cs_html_vote('cancel',$cs_lang['cancel'],'submit');
-  echo cs_html_form (0);
-  echo cs_html_roco(0);
-  echo cs_html_table(0);
+  $data['lang']['remove'] = $cs_lang['head_remove']; 
+  $data['lang']['body'] = sprintf($cs_lang['del_rly'],$events_id);
+  
+  $data['lang']['content'] = cs_link($cs_lang['confirm'],'events','remove','id=' . $events_id . '&amp;agree');
+  $data['lang']['content'] .= ' - ';
+  $data['lang']['content'] .= cs_link($cs_lang['cancel'],'events','remove','id=' . $events_id . '&amp;cancel');
+  
+  echo cs_subtemplate(__FILE__,$data,'events','remove');
 }
 
 ?>
---
+++ themes/base/events/remove.tpl
@@ -0,0 +1,14 @@
+<table class="forum" cellpadding="0" cellspacing="{page:cellspacing}" style="width:{page:width}">
+  <tr>
+    <td class="headb"> {lang:mod} - {lang:remove} </td>
+  </tr>
+  <tr>
+    <td class="leftb"> {lang:body} </td>
+  </tr>
+</table>
+<br />
+<table class="forum" cellpadding="0" cellspacing="{page:cellspacing}" style="width:{page:width}">
+  <tr>
+    <td class="centerc"> {lang:content} </td>
+  </tr>
+</table>
