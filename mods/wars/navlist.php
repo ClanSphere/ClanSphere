9c477619e5b48ae0b43c172739d198b690b3c77d
duRiel <duRiel@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-02-26 17:23:52 +0000

    fixed a bug for other db types in wars navlist

--- mods/wars/navlist.php
+++ mods/wars/navlist.php
@@ -10,7 +10,7 @@ $data = array();
 
 $select = 'war.games_id AS games_id, cln.clans_short AS clans_short, war.wars_score1 AS wars_score1, war.wars_score2 AS wars_score2, war.wars_id AS wars_id';
 $from = 'wars war INNER JOIN {pre}_categories cat ON war.categories_id = cat.categories_id INNER JOIN {pre}_clans cln ON war.clans_id = cln.clans_id';
-$order = 'wars_date DESC';
+$order = 'war.wars_date DESC';
 $cs_wars = cs_sql_select(__FILE__,$from,$select,"war.wars_status = 'played'",$order,0,$cs_option['max_navlist']);
 
 if (!empty($cs_wars)) {
