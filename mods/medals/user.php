76902097df95a68c297678f51f660b743435e1e3
Mc.Alcatraz <Mc.Alcatraz@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-04 18:39:08 +0000

    Added and reworked medals mod to clansphere part II

---
+++ mods/medals/user.php
@@ -0,0 +1,58 @@
+<?php
+
+$cs_lang = cs_translate('medals');
+$data = array();
+
+if(isset($_POST['submit'])) {
+  $medals_id = $_POST['medals_id'];
+  if(!empty($_POST['users_nick'])) {
+    $users_nick = cs_sql_escape($_POST['users_nick']);
+    $users_id = cs_sql_select(__FILE__,'users','users_id',"users_nick = '$users_nick'",0,0,1);
+    $insertion = array('medals_id' => $medals_id, 'users_id' => $users_id['users_id'], 'medalsuser_date' => cs_time());
+    cs_sql_insert(__FILE__, 'medalsuser', array_keys($insertion), array_values($insertion)); 
+    cs_redirect($cs_lang['create_done'], 'medals', 'user', 'where='.$medals_id);
+  }
+} else {
+  $medals_id = $_GET['where'];
+}
+
+if(isset($_GET['delete'])) {
+  $medalsuser_id = cs_sql_escape($_GET['delete']);
+  cs_sql_delete(__FILE__,'medalsuser',$medalsuser_id);
+  cs_redirect($cs_lang['del_true'], 'medals', 'user', 'where='.$medals_id);
+}
+
+$start = empty($_GET['start']) ? 0 : $_GET['start'];
+$cs_sort[1] = 'md.medalsuser_date DESC';
+$cs_sort[2] = 'md.medalsuser_date ASC';
+$cs_sort[3] = 'usr.users_nick DESC';
+$cs_sort[4] = 'usr.users_nick ASC';
+$sort = empty($_GET['sort']) ? 1 : $_GET['sort'];
+$order = $cs_sort[$sort];
+
+$tables = 'medalsuser md LEFT JOIN {pre}_users usr ON usr.users_id = md.users_id';
+$cells  = 'usr.users_nick AS users_nick, md.users_id AS users_id, usr.users_active AS users_active, usr.users_delete AS users_delete, ';
+$cells .= 'md.medals_id AS medals_id, md.medalsuser_date AS medalsuser_date, md.medalsuser_id AS medalsuser_id';
+
+$data['medals_user'] = cs_sql_select(__FILE__,$tables,$cells,'medals_id = '.$medals_id,$order,$start,$account['users_limit']);
+$data['count']['medals_user'] = count($data['medals_user']);
+
+$data['sort']['date'] = cs_sort('medals','user',$start,$medals_id,1,$sort);
+$data['sort']['users_nick'] = cs_sort('medals','user',$start,$medals_id,3,$sort);
+
+$data['pages']['list'] = cs_pages('medals','user',$data['count']['medals_user'],$start,$medals_id,$sort);
+
+for ($i = 0; $i < $data['count']['medals_user']; $i++) {
+  
+  $data['medals_user'][$i]['user'] = cs_user($data['medals_user'][$i]['users_id'], $data['medals_user'][$i]['users_nick'], $data['medals_user'][$i]['users_active'], $data['medals_user'][$i]['users_delete']);
+  $data['medals_user'][$i]['remove_url'] = cs_url('medals','user','where=' . $data['medals_user'][$i]['medals_id'].'&delete=' . $data['medals_user'][$i]['medalsuser_id']);
+  $data['medals_user'][$i]['medals_date'] = cs_date('unix',$data['medals_user'][$i]['medalsuser_date'],1);
+}
+
+$data['medals']['id'] = $medals_id;
+$data['message']['medals'] = cs_getmsg();
+$data['form']['dirname'] = $cs_main['php_self']['dirname'];
+
+echo cs_subtemplate(__FILE__,$data,'medals','user');
+
+?>
---
+++ themes/base/medals/user.tpl
@@ -0,0 +1,40 @@
+<table class="forum" style="width:{page:width}" cellpadding="0" cellspacing="1">
+  <tr>
+    <td class="headb" colspan="3">{lang:mod} - {lang:manage}</td>
+  </tr>
+  <tr>
+    <td class="leftb"><a href="{url:medals_manage}">{lang:back}</a></td>
+    <td class="leftb">{icon:contents} {lang:total}: {count:medals_user}</td>
+    <td class="rightb">{pages:list}</td>
+  </tr>
+</table>
+<br />
+<form method="post" action="{url:medals_user}" name="medals_user" enctype="multipart/form-data">
+  <table class="forum" style="width:{page:width}" cellpadding="0" cellspacing="1">
+    <tr>
+      <td class="leftc">{icon:personal} {lang:user}</td>
+      <td class="leftb">
+        <input type="text" name="users_nick" id="name" value="" onkeyup="cs_ajax_getcontent('{form:dirname}mods/messages/getusers.php?name=' + document.getElementById('name').value,'names_list')" maxlength="200" size="50" class="form" />
+        <input type="submit" value="{lang:awardingto}" name="submit" class="form" />
+        <input type="hidden" value="{medals:id}" name="medals_id" class="form" />
+        <div id="names_list"></div>
+        </td>
+    </tr>
+  </table>
+</form>
+<br />
+{message:medals}
+<table class="forum" style="width:{page:width}" cellpadding="0" cellspacing="1">
+  <tr>
+    <td class="headb">{sort:date} {lang:date}</td>
+    <td class="headb">{sort:users_nick} {lang:user}</td>
+    <td class="headb">{lang:options}</td>
+  </tr>
+  {loop:medals_user}
+  <tr>
+    <td class="leftb">{medals_user:medals_date}</td>
+    <td class="leftb">{medals_user:user}</td>
+    <td class="centerb"><a href="{medals_user:remove_url}">{icon:editdelete}</a></td>
+  </tr>
+  {stop:medals_user}
+</table>
