2cbc9be8333a476e9a5bc4571969e67a963fdde0
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-07 17:22:58 +0000

    added check for sql tables without an ownership to database statistics

--- mods/database/statistic.php
+++ mods/database/statistic.php
@@ -12,23 +12,33 @@ $total_tables = 0;
 $total_datasets = 0;
 $run = 0;
 
+global $cs_db;
+
+$sql_infos = cs_sql_version(__FILE__);
+
+$names = array_flip($sql_infos['names']);
+
 foreach($modules as $mod) {
 
   if((isset($account['access_' . $mod['dir'] . '']) OR $mod['dir'] == 'captcha') AND !empty($mod['tables'][0])) {
 
-    $tables = '';
-    $counts = '';
-    if(!empty($mod['icon'])) {
-      $data['statistic'][$run]['icon'] = cs_icon($mod['icon']);
-    }
+  $tables = '';
+  $counts = '';
+
+  if(!empty($mod['icon'])) {
+    $data['statistic'][$run]['icon'] = cs_icon($mod['icon']);
+  }
   else {
     $data['statistic'][$run]['icon'] = ''; 
   }
-    $data['statistic'][$run]['url'] = cs_url('modules','view','dir=' . $mod['dir']);
+
+  $data['statistic'][$run]['url'] = cs_url('modules','view','dir=' . $mod['dir']);
   $data['statistic'][$run]['name'] = $mod['name'];
 
     asort($mod['tables']);
     foreach($mod['tables'] AS $mod_table) {
+
+      unset($names['' . $cs_db['prefix'] . '_' . $mod_table]);
 
       if(isset($static[$mod_table])) {
         cs_error(__FILE__, 'SQL-Table "' . $mod_table . '" is owned by two modules: "' . $static[$mod_table] . '" and "' . $mod['dir'] . '"');
@@ -51,5 +61,12 @@ foreach($modules as $mod) {
 $data['data']['tables'] = $total_tables;
 $data['data']['dataset'] = $total_datasets;
 
+$names = array_flip($names);
+
+foreach($names AS $missing) {
+ cs_error(__FILE__, 'SQL-Table "' . $missing . '" exists without an ownership');
+}
+
 echo cs_subtemplate(__FILE__,$data,'database','statistic');
+
 ?>
