88f228b55bb3a9e452fff2ef58a51d382bcad9b7
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-07 23:02:58 +0000

    fixed fckeditor uploads to work again

--- mods/fckeditor/editor/filemanager/connectors/php/config.php
+++ mods/fckeditor/editor/filemanager/connectors/php/config.php
@@ -43,17 +43,17 @@ else {
 // SECURITY: You must explicitly enable this "connector". (Set it to "true").
 // WARNING: don't just set "$Config['Enabled'] = true ;", you must be sure that only
 //		authenticated users can access this file or use some kind of session checking.
-$Config['Enabled'] = false ;
+$Config['Enabled'] = $enabled;
 
 
 // Path to user files relative to the document root.
-$Config['UserFilesPath'] = '/userfiles/' ;
+$Config['UserFilesPath'] = $userdir;
 
 // Fill the following value it you prefer to specify the absolute path for the
 // user files directory. Useful if you are using a virtual directory, symbolic
 // link or alias. Examples: 'C:\\MySite\\userfiles\\' or '/root/mysite/userfiles/'.
 // Attention: The above 'UserFilesPath' must point to the same directory.
-$Config['UserFilesAbsolutePath'] = '' ;
+$Config['UserFilesAbsolutePath'] = '../../../../../../uploads/fckeditor/';
 
 // Due to security issues with Apache modules, it is recommended to leave the
 // following setting enabled.
