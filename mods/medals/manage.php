682cfe160d8d8c110751866c25712bfc18334d5a
Fr33z3m4n <Fr33z3m4n@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-08 10:27:37 +0000

    fixed cs_pages list (manage) @medals mod manage (Thread ID: 9698)

--- mods/medals/manage.php
+++ mods/medals/manage.php
@@ -8,13 +8,12 @@ $cs_sort[1] = 'medals_name DESC';
 $cs_sort[2] = 'medals_name ASC';
 $sort = empty($_GET['sort']) ? 1 : $_GET['sort'];
 $order = $cs_sort[$sort];
+$medals_count = cs_sql_count(__FILE__,'medals');
 
 $data['medals'] = cs_sql_select(__FILE__,'medals','medals_name, medals_id',0,$order,$start,$account['users_limit']);
 $data['count']['medals'] = count($data['medals']);
-
 $data['sort']['name'] = cs_sort('medals','manage',$start,0,1,$sort);
-
-$data['pages']['list'] = cs_pages('medals','manage',$data['count']['medals'],$start,0,$sort);
+$data['pages']['list'] = cs_pages('medals','manage',$medals_count,$start,0,$sort);
 
 for ($i = 0; $i < $data['count']['medals']; $i++) {
   $data['medals'][$i]['medals_user'] = cs_sql_count(__FILE__,'medalsuser','medals_id='.$data['medals'][$i]['medals_id']);
