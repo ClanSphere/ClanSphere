abd06c53fdeaf26abf2a0ee96adad35f27c4f612
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-10 16:28:06 +0000

    fixed features.php to work again

--- features.php
+++ features.php
@@ -3,7 +3,7 @@
 // $Id$
 
 # Overwrite global settings by using the following array
-$cs_main = array('init_sql' => false, 'init_tpl' => true, 'tpl_file' => 'features.htm', 'def_width' => '100%');
+$cs_main = array('init_sql' => true, 'init_tpl' => true, 'tpl_file' => 'features.htm', 'def_width' => '100%');
 
 require_once 'system/core/functions.php';
 
