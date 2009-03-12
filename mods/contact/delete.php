df83306e88f51556c771ae6d47fa34b74f047f5e
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-12 01:00:29 +0000

    fixed contact delete to work again

--- mods/contact/delete.php
+++ mods/contact/delete.php
@@ -9,21 +9,17 @@ $data = array();
 $contact_id = empty($cs_get['id']) ? 0 : $cs_get['id'];
 
 if(isset($_GET['agree'])) {
-  cs_sql_delete(__FILE__,'contact',$contact_id);
-  cs_redirect($cs_lang['del_true_join'], 'contact');
+  cs_sql_delete(__FILE__,'mail',$contact_id);
+  cs_redirect($cs_lang['del_true'], 'contact');
 }
-
 elseif(isset($_GET['cancel']))   
   cs_redirect($cs_lang['del_false'], 'contact');
-
-else{
-  
+else {
   $data['head']['body'] = sprintf($cs_lang['del_rly'],$contact_id);
   $data['url']['agree'] = cs_url('contact','delete','id=' . $contact_id . '&amp;agree');
   $data['url']['cancel'] = cs_url('contact','delete','id=' . $contact_id . '&amp;cancel');
-  
+
   echo cs_subtemplate(__FILE__,$data,'contact','delete');
-  
 }
 
 ?>
