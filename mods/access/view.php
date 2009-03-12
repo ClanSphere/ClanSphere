096f340da27225358f4657f68221ef334c110631
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-12 01:15:09 +0000

    removed empty acccess mods from access view

--- mods/access/view.php
+++ mods/access/view.php
@@ -16,20 +16,21 @@ $modules = cs_checkdirs('mods');
 
 foreach($modules as $mod) {
   $acc_dir = 'access_' . $mod['dir'];
-  if(array_key_exists($acc_dir,$cs_access) AND $mod['dir'] != 'clansphere') {
+  if(array_key_exists($acc_dir,$cs_access) AND $mod['dir'] != 'clansphere' AND !empty($account[$acc_dir])) {
     if(!empty($mod['icon'])) {
       $data['access'][$run]['icon'] = cs_icon($mod['icon']);
     }
-  else {
-    $data['access'][$run]['icon'] = '';
-  }
-  
+    else {
+      $data['access'][$run]['icon'] = '';
+    }
+
     $data['access'][$run]['name'] = $mod['name'];
     $mod_acc = $cs_access[$acc_dir];
     $data['access'][$run]['access'] =  $mod_acc . ' - ' . $cs_lang['lev_' . $mod_acc];
-  $run++;
+    $run++;
   }
 }
 
-echo cs_subtemplate(__FILE__,$data,'access','view');  
+echo cs_subtemplate(__FILE__,$data,'access','view');
+
 ?>
