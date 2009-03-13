1df06c8e80c159698f26ccc8f11f621220d9ef5c
fay-pain <fay-pain@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-13 00:46:13 +0000

    fixed newsletter/create (bugtracker-373, thx to dopekick)

--- mods/newsletter/create.php
+++ mods/newsletter/create.php
@@ -38,23 +38,27 @@ if(!empty($error) OR !isset($_POST['submit'])) {
 
   $data['nl'] = $cs_nl;
   
-  $data['nl']['to_dropdown'] = cs_html_option('----','0',1);
-  $data['nl']['to_dropdown'] .= cs_html_option($cs_lang['all_users'],'1');
-  $data['nl']['to_dropdown'] .= cs_html_option('&raquo;' .$cs_lang['ac_group'],'');
+  $dp = cs_html_option('----','0',1);
+  $dp .= cs_html_option($cs_lang['all_users'],'1');
+  $dp .= cs_html_option('&raquo;' .$cs_lang['ac_group'],'');
   $usergroups = cs_sql_select(__FILE__,'access','access_id,access_name','access_id != 1',0,0,0);
-  foreach($usergroups as $value) {
-    $data['nl']['to_dropdown'] .= cs_html_option('&nbsp;&nbsp;&nbsp;' .$value['access_name'],'2?' .$value['access_id']);
+  foreach($usergroups AS $value) {
+    $dp .= cs_html_option('&nbsp;&nbsp;&nbsp;' .$value['access_name'],'2?' .$value['access_id']);
   }
-  $data['nl']['to_dropdown'] .= cs_html_option(' &raquo;' .$cs_lang['squad'],'');
-  $usergroups = cs_sql_select(__FILE__,'squads','squads_id,squads_name',0,0,0,0);
-  foreach($usergroups as $value) {
-    $data['nl']['to_dropdown'] .= cs_html_option('&nbsp;&nbsp;&nbsp;' .$value['squads_name'],'3?' .$value['squads_id']);
+  $dp .= cs_html_option(' &raquo;' .$cs_lang['squad'],'');
+  $squads = cs_sql_select(__FILE__,'squads','squads_id, squads_name',0,'squads_name',0,0);
+  $squads = empty($squads) ? array() : $squads;
+  foreach($squads AS $squad) {
+    $dp .= cs_html_option('&nbsp;&nbsp;&nbsp;' .$squad['squads_name'],'3?' .$squad['squads_id']);
   }   
-  $data['nl']['to_dropdown'] .= cs_html_option('&raquo;' .$cs_lang['clan'],'');
-  $usergroups = cs_sql_select(__FILE__,'clans','clans_id,clans_name',0,0,0,0);
-  foreach($usergroups as $value) {
-    $data['nl']['to_dropdown'] .= cs_html_option('&nbsp;&nbsp;&nbsp;' .$value['clans_name'],'4?' .$value['clans_id']);
+  $dp .= cs_html_option('&raquo;' .$cs_lang['clan'],'');
+
+  $clans = cs_sql_select(__FILE__,'clans','clans_id, clans_name',0,'clans_name',0,0);
+  $clans = empty($clans) ? array() : $clans;
+  foreach($clans AS $clan) {
+    $dp .= cs_html_option('&nbsp;&nbsp;&nbsp;' .$clan['clans_name'],'4?' .$clan['clans_id']);
   }
+  $data['nl']['to_dropdown'] = $dp;
 
  echo cs_subtemplate(__FILE__,$data,'newsletter','create');
 
