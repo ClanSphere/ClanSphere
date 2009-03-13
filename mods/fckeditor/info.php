b8385a09b72f1a6efb08559dce1ce855b87fb9de
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-13 18:59:01 +0000

    updated fckeditor info.php

--- mods/fckeditor/info.php
+++ mods/fckeditor/info.php
@@ -2,18 +2,18 @@
 // ClanSphere 2008 - www.clansphere.net
 // $Id$
 
-$mod_info['name']    = 'FCKEditor';
-$mod_info['version']  = $cs_main['version_name'];
-$mod_info['released']  = $cs_main['version_date'];
-$mod_info['creator']  = 'FCKEditor';
-$mod_info['team']    = 'ClanSphere';
-$mod_info['url']    = 'www.clansphere.net';
-$mod_info['text']    = 'Version 2.6.4 beta';
-$mod_info['icon']    = 'package_editors';
-$mod_info['show']    = array('options/roots' => 5);
+$mod_info['name']       = 'FCKEditor';
+$mod_info['version']    = $cs_main['version_name'];
+$mod_info['released']   = $cs_main['version_date'];
+$mod_info['creator']    = 'FCKEditor';
+$mod_info['team']       = 'ClanSphere';
+$mod_info['url']        = 'www.clansphere.net';
+$mod_info['text']       = 'Version 2.6.4';
+$mod_info['icon']       = 'package_editors';
+$mod_info['show']       = array('options/roots' => 5);
 $mod_info['categories'] = FALSE;
-$mod_info['comments']  = FALSE;
+$mod_info['comments']   = FALSE;
 $mod_info['protected']  = FALSE;
-$mod_info['tables']    = array();
+$mod_info['tables']     = array();
 
 ?>
