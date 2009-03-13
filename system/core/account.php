623c7800cb244f1f8ea5ed64e9a70da1c6e90c11
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-13 17:48:02 +0000

    merged core/lang.php into core/account.php as it is only used there

--- system/core/account.php
+++ system/core/account.php
@@ -114,7 +114,26 @@ else {
 }
 
 if(!empty($_GET['lang']) OR empty($account['users_id']) AND !empty($_COOKIE['cs_lang'])) {
-  require_once('system/core/lang.php');
+
+  $languages = cs_checkdirs('lang');
+  $cookie_lang = empty($_COOKIE['cs_lang']) ? '' : $_COOKIE['cs_lang'];
+  $cs_user['users_lang'] = empty($_GET['lang']) ? $cookie_lang : $_GET['lang'];
+  $allow = 0;
+
+  foreach($languages as $mod) {
+    if($mod['dir'] == $cs_user['users_lang']) { $allow++; }
+  }
+  $cs_user['users_lang'] = empty($allow) ? $cs_main['def_lang'] : $cs_user['users_lang'];
+
+  if(empty($account['users_id']) AND $cookie_lang != $cs_user['users_lang']) {
+    setcookie('cs_lang',$cs_user['users_lang'], $cookie['lifetime'], $cookie['path'], $cookie['domain']);
+  }
+  elseif(!empty($account['users_id']) AND $account['users_lang'] != $cs_user['users_lang']) {
+    $users_cells = array_keys($cs_user);
+    $users_save = array_values($cs_user);
+    cs_sql_update(__FILE__,'users',$users_cells,$users_save,$account['users_id']);
+  }
+  $account['users_lang'] = $cs_user['users_lang'];
 }
 
 $lang = empty($account['users_lang']) ? 'English' : $account['users_lang'];
