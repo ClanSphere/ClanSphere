a71ec554b66bd380c282d0da82d5088ed6041868
duRiel <duRiel@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-14 18:50:16 +0000

    ajax now also supports the new display of the debug features

--- navlists.php
+++ navlists.php
@@ -3,13 +3,10 @@
 // $Id$
 
 if (strpos($_SERVER['PHP_SELF'], 'content.php') === false) {
-  
   $cs_main = array('init_sql' => true, 'init_tpl' => false);
-
   require_once 'system/core/functions.php';
 
   cs_init($cs_main);
-  
 }
 
 if (empty($account['access_ajax'])) die('No access on AJAX');
@@ -18,29 +15,29 @@ if (isset($_GET['debug'])) {
   if (substr($cs_main['ajax_navlists'],-1) != ',') $cs_main['ajax_navlists'] .= ',';
 	$cs_main['ajax_navlists'] .= 'func_sql,func_errors,';
 }
+
 $ajaxes = explode(',',$cs_main['ajax_navlists']);
 array_pop($ajaxes);
 $string = str_replace(',','',$cs_main['ajax_navlists']);
 
 $logsql = '';
 if (!empty($cs_main['developer']) OR $account['access_clansphere'] > 4) {
-  $cs_logs['php_errors'] = nl2br($cs_logs['php_errors']);
-  $cs_logs['errors'] = nl2br($cs_logs['errors']);
+  $cs_logs['php_errors'] = str_replace('\'','\\\'',nl2br($cs_logs['php_errors']));
+  $cs_logs['errors'] = str_replace('\'','\\\'',nl2br($cs_logs['errors']));
   foreach($cs_logs['sql'] AS $sql_file => $sql_queries) {
     $logsql .= cs_html_big(1) . $sql_file . cs_html_big(0) . cs_html_br(1);
-    $logsql .= nl2br($sql_queries);
+    $logsql .= nl2br(cs_secure($sql_queries));
   }
-}
-else {
+} else {
   $cs_logs['php_errors'] = '';
   $cs_logs['errors'] = 'Developer mode is turned off';
 }
 
 if (!empty($string)) {
   $temp = '';
-  $specials = array('func_parse' => 'cs_parsetime($cs_micro)', 'func_queries' => $cs_logs['queries'], 'func_sql' => $logsql,
-    'func_errors' => '$cs_logs["php_errors"] . $cs_logs["errors"])', 'func_sql' => $logsql);
-  $special_names = array('func_errors' => 'errors');
+  $specials = array('func_parse' => 'cs_parsetime($cs_micro)', 'func_queries' => '2', 'func_sql' => '\'' . $logsql . '\'',
+    'func_errors' => '\'' . $cs_logs["php_errors"] . $cs_logs["errors"] . '\'');
+  $special_names = array('func_errors' => 'errors', 'func_sql' => 'sql');
   
   foreach ($ajaxes as $ajax) {
     if (empty($ajax)) continue;
