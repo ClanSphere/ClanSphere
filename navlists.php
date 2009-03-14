69c8a96c50c0d625035fe86e454757bb88a005db
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-14 13:57:58 +0000

    updated ajax navlist file to contain developer mode

--- navlists.php
+++ navlists.php
@@ -22,11 +22,25 @@ $ajaxes = explode(',',$cs_main['ajax_navlists']);
 array_pop($ajaxes);
 $string = str_replace(',','',$cs_main['ajax_navlists']);
 
+$logsql = '';
+if (!empty($cs_main['developer']) OR $account['access_clansphere'] > 4) {
+  $cs_logs['php_errors'] = nl2br($cs_logs['php_errors']);
+  $cs_logs['errors'] = nl2br($cs_logs['errors']);
+  foreach($cs_logs['sql'] AS $sql_file => $sql_queries) {
+    $logsql .= cs_html_big(1) . $sql_file . cs_html_big(0) . cs_html_br(1);
+    $logsql .= nl2br($sql_queries);
+  }
+}
+else {
+  $cs_logs['php_errors'] = '';
+  $cs_logs['errors'] = 'Developer mode is turned off';
+}
+
 if (!empty($string)) {
   $temp = '';
-  $specials = array('func_parse' => 'cs_parsetime($cs_micro)', 'func_queries' => $cs_logs['queries'], 'func_sql' => 'nl2br(htmlspecialchars($cs_logs[\'sql\']));',
-    'func_errors' => 'nl2br($cs_logs["php_errors"] . $cs_logs["errors"])');
-  $special_names = array('func_sql' => 'sql', 'func_errors' => 'errors');
+  $specials = array('func_parse' => 'cs_parsetime($cs_micro)', 'func_queries' => $cs_logs['queries'], 'func_sql' => $logsql,
+    'func_errors' => '$cs_logs["php_errors"] . $cs_logs["errors"])', 'func_sql' => $logsql);
+  $special_names = array('func_errors' => 'errors');
   
   foreach ($ajaxes as $ajax) {
     if (empty($ajax)) continue;
