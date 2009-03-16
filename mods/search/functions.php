50547722bd88562cb3d9badae9e0276122fde017
fay-pain <fay-pain@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-16 16:40:37 +0000

    added theme to search function

--- mods/search/functions.php
+++ mods/search/functions.php
@@ -4,22 +4,10 @@
 
 function cs_search($mod) {
   $cs_lang = cs_translate('search');
-  $text = '';
-  echo cs_html_form (1,'search_functions','search','list');
-  echo cs_html_table(1,'forum',1);
-  echo cs_html_roco(1,'headb',0,2);
-  echo $cs_lang['mod'];
-  echo cs_html_roco(0);
-  echo cs_html_roco(1,'leftc');
-  echo cs_icon('kedit');
-  echo $cs_lang['text']. ' *';
-  echo cs_html_roco(2,'leftc');
-  echo cs_html_input('text',$text,'text',200,50);
-  echo cs_html_vote('where',$mod,'hidden');
-  echo cs_html_vote('submit',$cs_lang['search'],'submit');
-  echo cs_html_vote('reset',$cs_lang['reset'],'reset');
-  echo cs_html_roco(0);
-  echo cs_html_table(0);
-  echo cs_html_form(0);
+
+	$data = array();
+  $data['search']['mod'] = $mod;
+  
+ echo cs_subtemplate(__FILE__,$data,'search','search_function');
 }
 ?>
---
+++ themes/base/search/search_function.tpl
@@ -0,0 +1,16 @@
+<table class="form" style="width:{page:width}" cellpadding="0" cellspacing="{page:cellspacing}">
+	<tr>
+		<td class="headb" colspan="2">{lang:mod}</td>
+	</tr>
+	<tr>
+		<td class="leftb">{icon:kedit} {lang:text} *</td>
+		<td class="leftc">
+			<form method="post" id="search_functions" action="{url:search_list}">
+				<input type="text" name="text" value="" maxlength="200" size="50" />
+				<input type="hidden" name="where" value="{search:mod}" />
+				<input type="submit" name="submit" value="{lang:search}" />
+				<input type="reset" name="reset" value="{lang:reset}" />
+			</form>
+		</td>
+	</tr>
+</table>
