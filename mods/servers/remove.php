693e29358e68a77eb3a7b42746c2c0f6b86029e9
fay-pain <fay-pain@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-16 16:21:24 +0000

    added theme to servers/remove

--- mods/servers/remove.php
+++ mods/servers/remove.php
@@ -1,39 +1,31 @@
 <?php
+// ClanSphere 2008 - www.clansphere.net
+// $Id$
 
 $cs_lang = cs_translate('servers');
+$cs_post = cs_post('id');
+$cs_get = cs_get('id');
 
-echo cs_html_table(1,'forum',1);
-echo cs_html_roco(1,'headb');
-echo $cs_lang['mod'] . ' - ' . $cs_lang['head_remove'];
-echo cs_html_roco(0);
+$servers_id = empty($cs_get['id']) ? 0 : $cs_get['id'];
+if (!empty($cs_post['id']))  $servers_id = $cs_post['id'];
 
-$servers_form = 1;
-$servers_id = $_REQUEST['id'];
 
 if(isset($_POST['agree'])) {
-  $servers_form = 0;
-  cs_sql_delete(__FILE__,'servers',$servers_id);
+
+ cs_sql_delete(__FILE__,'servers',$servers_id);
   include_once('mods/servers/rss.php');
-  cs_redirect($cs_lang['del_true'], 'servers');
+ cs_redirect($cs_lang['del_true'], 'servers');
 }
 
 if(isset($_POST['cancel']))
-  cs_redirect($cs_lang['del_false'], 'servers');
+ cs_redirect($cs_lang['del_false'], 'servers');
 
-if(!empty($servers_form)) {
+else {
 
-  echo cs_html_roco(1,'leftb');
-  echo sprintf($cs_lang['del_rly'],$servers_id);
-  echo cs_html_roco(0);
+  $data['head']['body'] = sprintf($cs_lang['del_rly'],$servers_id);
+  $data['servers']['id'] = $servers_id;
 
-  echo cs_html_roco(1,'centerc');
-  echo cs_html_form(1,'servers_remove','servers','remove');
-  echo cs_html_vote('id',$servers_id,'hidden');
-  echo cs_html_vote('agree',$cs_lang['confirm'],'submit');
-  echo cs_html_vote('cancel',$cs_lang['cancel'],'submit');
-  echo cs_html_form (0);
-  echo cs_html_roco(0);
-  echo cs_html_table(0);
+ echo cs_subtemplate(__FILE__,$data,'servers','remove');
 }
 
 ?>
---
+++ themes/base/servers/remove.tpl
@@ -0,0 +1,21 @@
+<table class="forum" style="width:{page:width}" cellpadding="0" cellspacing="{page:cellspacing}">
+	<tr>
+		<td class="headb">{lang:mod} - {lang:remove}</td>
+	</tr>
+	<tr>
+		<td class="leftb">{head:body}</td>
+	</tr>
+</table>
+<br />
+
+<table class="forum" style="width:{page:width}" cellpadding="0" cellspacing="{page:cellspacing}">
+	<tr>
+		<td class="centerc">
+			<form method="post" id="servers_remove" action="{url:servers_remove}">
+				<input type="hidden" name="id" value="{servers:id}" />
+				<input type="submit" name="agree" value="{lang:confirm}" />
+				<input type="submit" name="cancel" value="{lang:cancel}" />
+			</form>
+		</td>
+	</tr>
+</table>
