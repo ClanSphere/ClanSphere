72989b01172126ab69335360628e2aea2db62b9a
duRiel <duRiel@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-17 21:01:01 +0000

    removed imprint.txt so that the imprint is not overwritten after an update

--- mods/contact/imprint.php
+++ mods/contact/imprint.php
@@ -6,20 +6,22 @@ $cs_lang = cs_translate('contact');
 
 $filename = 'uploads/imprint/imprint.txt';
 
-if (file_exists($filename)) {
+$exists = file_exists($filename) ? TRUE : FALSE;
+
+if ($exists) {
   $fp = fopen ($filename, "r");
   $content = fread ($fp, filesize($filename));
   $imprint = explode("{laststandbreak}", $content);
   fclose ($fp);
 }
 
-$data['if']['date'] = file_exists($filename) ? TRUE : FALSE;
-if(file_exists($filename)) {
+$data['if']['date'] = $exists;
+if(!empty($data['if']['date'])) {
   $data['imprint']['content'] = cs_secure($imprint[1],1,1);
   $stand = '[b]' .$cs_lang['stand'] .' '. cs_date('unix',$imprint[0]) .'[/b]';
   $data['imprint']['date'] = cs_secure($stand,1,1);
 } else {
-  $data['imprint']['content'] = cs_secure ('-');
+  $data['imprint']['content'] = '-';
 
 }
 
