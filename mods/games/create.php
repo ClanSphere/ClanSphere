c7e7514d07c132d009bbaf426b9da0e0fd11b61d
duRiel <duRiel@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-18 14:04:54 +0000

    games: fixed create so if required information is not entered youre redirected back to the form, also fixed remove so that clansphere doesnt try to remove a non-existing image anymore

--- mods/games/create.php
+++ mods/games/create.php
@@ -88,60 +88,56 @@ if(!empty($_POST['games_url'])) {
 
 $data['lang']['body'] = !isset($_POST['submit']) ? $cs_lang['body_create'] : $errormsg;
 
-if(isset($_POST['submit'])) {
-  if(empty($games_error)) {
-    $games_form = 0;
+if(isset($_POST['submit']) && empty($games_error)) {
         
-    $games_cells = array('games_name','games_version','games_released','games_creator','categories_id','games_url','games_usk');
-    $games_save = array($games_name,$games_version,$games_release,$games_creator,$categories_id,$games_url,$games_usk);
-    cs_sql_insert(__FILE__,'games',$games_cells,$games_save);
-    
-        
-    if(!empty($files['symbol']['tmp_name']) AND $symbol_error == 0) {
-      $where = "games_name = '" . cs_sql_escape($games_name) . "'";
-      $getid = cs_sql_select(__FILE__,'games','games_id',$where);
-      $filename = $getid['games_id'] . '.' . $extension;
-      cs_upload('games',$filename,$files['symbol']['tmp_name']);
-    }
+  $games_cells = array('games_name','games_version','games_released','games_creator','categories_id','games_url','games_usk');
+  $games_save = array($games_name,$games_version,$games_release,$games_creator,$categories_id,$games_url,$games_usk);
+  cs_sql_insert(__FILE__,'games',$games_cells,$games_save);
   
-  }  
+      
+  if(!empty($files['symbol']['tmp_name']) AND $symbol_error == 0) {
+    $where = "games_name = '" . cs_sql_escape($games_name) . "'";
+    $getid = cs_sql_select(__FILE__,'games','games_id',$where);
+    $filename = $getid['games_id'] . '.' . $extension;
+    cs_upload('games',$filename,$files['symbol']['tmp_name']);
+  }
+  
   cs_redirect($cs_lang['create_done'],'games');
 }
 
 
-if(!empty($games_form)) {
-  $data['url']['form'] = cs_url('games','create');
+$data['url']['form'] = cs_url('games','create');
+
+$data['games']['name'] = $games_name;
+$data['games']['version'] = $games_version;
+$data['games']['genre'] = cs_categories_dropdown('games',$categories_id);
+$data['games']['release'] = cs_dateselect('datum','date',$games_release);
+$data['games']['creator'] = $games_creator;
+$data['games']['homepage'] = $games_url;
+      
+$usknum[0]['games_usk'] = '00';
+$usknum[0]['name'] = $cs_lang['usk_00'];
+$usknum[1]['games_usk'] = '06';
+$usknum[1]['name'] = $cs_lang['usk_06'];
+$usknum[2]['games_usk'] = '12';
+$usknum[2]['name'] = $cs_lang['usk_12'];
+$usknum[3]['games_usk'] = '16';
+$usknum[3]['name'] = $cs_lang['usk_16'];
+$usknum[4]['games_usk'] = '18';
+$usknum[4]['name'] = $cs_lang['usk_18'];
+$data['games']['usk'] = cs_dropdown('games_usk','name',$usknum,$games_usk);
   
-  $data['games']['name'] = $games_name;
-  $data['games']['version'] = $games_version;
-  $data['games']['genre'] = cs_categories_dropdown('games',$categories_id);
-  $data['games']['release'] = cs_dateselect('datum','date',$games_release);
-  $data['games']['creator'] = $games_creator;
-  $data['games']['homepage'] = $games_url;
-        
-  $usknum[0]['games_usk'] = '00';
-  $usknum[0]['name'] = $cs_lang['usk_00'];
-  $usknum[1]['games_usk'] = '06';
-  $usknum[1]['name'] = $cs_lang['usk_06'];
-  $usknum[2]['games_usk'] = '12';
-  $usknum[2]['name'] = $cs_lang['usk_12'];
-  $usknum[3]['games_usk'] = '16';
-  $usknum[3]['name'] = $cs_lang['usk_16'];
-  $usknum[4]['games_usk'] = '18';
-  $usknum[4]['name'] = $cs_lang['usk_18'];
-  $data['games']['usk'] = cs_dropdown('games_usk','name',$usknum,$games_usk);
-    
-  $matches[1] = $cs_lang['pic_infos'];
-  $return_types = '';
-  foreach($img_filetypes AS $add => $value) {
-    $return_types .= empty($return_types) ? $add : ', ' . $add;
-  }
-  $matches[2] = $cs_lang['max_width'] . ': ' . $options['max_width'] . ' px' . cs_html_br(1);
-  $matches[2] .= $cs_lang['max_height'] . ': ' . $options['max_height'] . ' px' . cs_html_br(1);
-  $matches[2] .= $cs_lang['max_size'] . ': ' . cs_filesize($options['max_size']) . cs_html_br(1);
-  $matches[2] .= $cs_lang['filetypes'] . $return_types;
-  $data['games']['clip'] = cs_abcode_clip($matches);
-  
-  echo cs_subtemplate(__FILE__,$data,'games','create');
+$matches[1] = $cs_lang['pic_infos'];
+$return_types = '';
+foreach($img_filetypes AS $add => $value) {
+  $return_types .= empty($return_types) ? $add : ', ' . $add;
 }
+$matches[2] = $cs_lang['max_width'] . ': ' . $options['max_width'] . ' px' . cs_html_br(1);
+$matches[2] .= $cs_lang['max_height'] . ': ' . $options['max_height'] . ' px' . cs_html_br(1);
+$matches[2] .= $cs_lang['max_size'] . ': ' . cs_filesize($options['max_size']) . cs_html_br(1);
+$matches[2] .= $cs_lang['filetypes'] . $return_types;
+$data['games']['clip'] = cs_abcode_clip($matches);
+
+echo cs_subtemplate(__FILE__,$data,'games','create');
+
 ?>
--- mods/games/remove.php
+++ mods/games/remove.php
@@ -4,30 +4,28 @@
 
 $cs_lang = cs_translate('games');
 
-$games_form = 1;
-$games_id = $_REQUEST['id'];
+$games_id = (int) $_GET['id'];
 
 if(isset($_GET['agree'])) {
-  $games_form = 0;
+
   cs_sql_delete(__FILE__,'games',$games_id);
   
-  cs_unlink('games', $games_id . '.gif');
+  if (file_exists('uploads/games/' . $games_id . '.gif'))
+    cs_unlink('games', $games_id . '.gif');
   
-    cs_redirect($cs_lang['del_true'], 'games');
+  cs_redirect($cs_lang['del_true'], 'games');
 }
 
 if(isset($_GET['cancel'])) {
   cs_redirect($cs_lang['del_false'], 'games');
 }
-
-if(!empty($games_form)) {
  
-  $data['lang']['body'] = sprintf($cs_lang['del_rly'],$games_id);
-  
-  $data['lang']['content'] = cs_link($cs_lang['confirm'],'games','remove','id=' . $games_id . '&amp;agree');
-  $data['lang']['content'] .= ' - ';
-  $data['lang']['content'] .= cs_link($cs_lang['cancel'],'games','remove','id=' . $games_id . '&amp;cancel');
-  
-  echo cs_subtemplate(__FILE__,$data,'games','remove');
-}
+$data['lang']['body'] = sprintf($cs_lang['del_rly'],$games_id);
+
+$data['lang']['content'] = cs_link($cs_lang['confirm'],'games','remove','id=' . $games_id . '&amp;agree');
+$data['lang']['content'] .= ' - ';
+$data['lang']['content'] .= cs_link($cs_lang['cancel'],'games','remove','id=' . $games_id . '&amp;cancel');
+
+echo cs_subtemplate(__FILE__,$data,'games','remove');
+
 ?>
