08f78c080adbb966bba15aadb776ca218e7372e2
fay-pain <fay-pain@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-19 19:14:27 +0000

    added theme to messages/autoresponder (test)

--- mods/messages/autoresponder.php
+++ mods/messages/autoresponder.php
@@ -3,155 +3,85 @@
 // $Id$
 
 $cs_lang = cs_translate('messages');
+$data = array();
 
-$users_id = $account['users_id'];
-$time = cs_time();
+$cs_messages = array();
+$cs_messages['users_id'] = $account['users_id'];
+$cs_messages['autoresponder_time'] = cs_time();
 
-echo cs_html_table(1,'forum',1);
-echo cs_html_roco(1,'headb');
-echo $cs_lang['mod'] . ' - ' . $cs_lang['edit'];
-echo cs_html_roco(0);
-echo cs_html_roco(1,'leftb');
 
-if(isset($_POST['submit']) OR isset($_POST['preview']))
-{
+if(isset($_POST['submit']) OR isset($_POST['preview'])) {
+
   $update = $_POST['update'];
-  $cs_messages['autoresponder_text'] = $_POST['autoresponder_text'];
-  $cs_messages['autoresponder_id'] = $_POST['autoresponder_id'];
   $cs_messages['autoresponder_subject'] = $_POST['autoresponder_subject'];
+  $cs_messages['autoresponder_text'] = $_POST['autoresponder_text'];
   $cs_messages['autoresponder_close'] = isset($_POST['autoresponder_close']) ? $_POST['autoresponder_close'] : 0;
-  $cs_messages['users_id'] = $users_id;
-  $cs_messages['autoresponder_time'] = $time;
+  $cs_messages['autoresponder_id'] = $_POST['autoresponder_id'];
 
   settype($cs_messages['autoresponder_id'], 'integer');
-  settype($cs_messages['autoresponder_close'], 'integer');  
+  settype($cs_messages['autoresponder_close'], 'integer');
+  
+  $error = '';
+  
+  if(empty($cs_messages['autoresponder_subject']))
+  	$error .= $cs_lang['no_subject'] . cs_html_br(1);
+  if(empty($cs_messages['autoresponder_text']))
+  	$error .= $cs_lang['no_text'] . cs_html_br(1);
+
 }
-else 
-{
-  $cells = 'autoresponder_id,autoresponder_text,autoresponder_subject,autoresponder_close';
-  $cs_messages = cs_sql_select(__FILE__,'autoresponder',$cells,"users_id = '" . $users_id . "'");
+else {
+  $cells = 'autoresponder_id, autoresponder_text, autoresponder_subject, autoresponder_close';
+  $cs_messages = cs_sql_select(__FILE__,'autoresponder',$cells,"users_id = '" . $account['users_id'] . "'",0,0,1);
   $update = count($cs_messages);
 }
+
+
 if(!isset($_POST['submit']) AND !isset($_POST['preview'])) 
-{
-  echo $cs_lang['body_edit'];
-}
+	$data['head']['body'] = $cs_lang['body_edit'];
 elseif(isset($_POST['preview'])) 
-{
-  echo $cs_lang['preview'];
-}
-else 
-{
-  echo $cs_lang['changes_done'];
-}
+	$data['head']['body'] = $cs_lang['preview'];
+elseif(!empty($error))
+	$data['head']['body'] = $error;
 
-echo cs_html_roco(0);
-echo cs_html_table(0);
-echo cs_html_br(1);
 
-if(isset($_POST['preview'])) 
-{
-    echo cs_html_table(1,'forum',1);
-    echo cs_html_roco(1,'headb',0,3);
-    echo $cs_lang['mod'] . ' - ' . cs_secure($cs_messages['autoresponder_subject']);
-    echo cs_html_roco(0);
-    echo cs_html_roco(1,'leftb');
-    echo $cs_lang['subject'];
-    echo cs_html_roco(2,'leftb');
-    $autoresponder = $cs_lang['autoresponder'];
-    echo cs_secure($autoresponder,1);
-    echo cs_secure($cs_messages['autoresponder_subject']);
-    echo cs_html_roco(0);
-    
-    echo cs_html_roco(1,'leftb');
-    echo $cs_lang['date'];
-    echo cs_html_roco(2,'leftb');
-    echo cs_date('unix',$cs_messages['autoresponder_time'],1);
-    echo cs_html_roco(0);
-    
-    echo cs_html_roco(1,'leftb');
-    echo $cs_lang['to'];
-    echo cs_html_roco(2,'leftb');
-    echo cs_html_roco(0);
-    
-    echo cs_html_roco(1,'leftb');
-    echo $cs_lang['options'];
-    echo cs_html_roco(2,'leftb');
-    
-    echo cs_icon('back',16,$cs_lang['back']);
-    echo cs_icon('mail_replay',16,$cs_lang['replay']);
-    echo cs_icon('mail_delete',16,$cs_lang['remove']);
-    echo cs_html_roco(0);
-    echo cs_html_table(0);
-    
-    echo cs_html_table(1,'forum',1);
-    echo cs_html_roco(1,'headb',0,3);
-    echo $cs_lang['mod'] . ' - ' . $cs_lang['text'];
-    echo cs_html_roco(0);
-    echo cs_html_roco(1,'leftb');
-    echo cs_secure($cs_messages['autoresponder_text'],1,1);
-    echo cs_html_roco(0);
-    echo cs_html_table(0);
-    echo cs_html_br(1);
+$data['if']['preview'] = FALSE;
+
+if(isset($_POST['preview'])) {
+
+	$data['if']['preview'] = TRUE;
+	$data['autoresponder']['subject'] = cs_secure($cs_messages['autoresponder_subject']);
+	$data['autoresponder']['time'] = cs_date('unix',$cs_messages['autoresponder_time'],1);
+	$data['autoresponder']['text'] = cs_secure($cs_messages['autoresponder_text'],1,1);
 }
 
-if(isset($_POST['preview']) OR !isset($_POST['submit'])) 
-{
-  echo cs_html_form (1,'messages_edit','messages','autoresponder');
-  echo cs_html_table(1,'forum',1);
-  
-  echo cs_html_roco(1,'leftc');
-  echo cs_icon('kedit') . $cs_lang['subject'] . ' *';
-  echo cs_html_roco(2,'leftb');
-  echo cs_html_input('autoresponder_subject',$cs_messages['autoresponder_subject'],'text',200,50);
-  echo cs_html_roco(0);
+if(!empty($error) OR isset($_POST['preview']) OR !isset($_POST['submit'])) {
 
-  echo cs_html_roco(1,'leftc');
-  echo cs_icon('kate') . $cs_lang['text'];
-  echo cs_html_br(2);
-  echo cs_abcode_smileys('autoresponder_text');
-  echo cs_html_roco(2,'leftb');
-  echo cs_abcode_features('autoresponder_text');
-  echo cs_html_textarea('autoresponder_text',$cs_messages['autoresponder_text'],'50','20');
-  echo cs_html_roco(0);
+	$data['data'] = $cs_messages;
 
-  echo cs_html_roco(1,'leftc');
-  echo cs_icon('configure') . $cs_lang['more'];
-  echo cs_html_roco(2,'leftb');
-  echo cs_html_vote('autoresponder_close','1','checkbox',$cs_messages['autoresponder_close']);
-  echo $cs_lang['messages_autoresponder'];
+	$data['abcode']['smileys'] = cs_abcode_smileys('autoresponder_text');
+	$data['abcode']['features'] = cs_abcode_features('autoresponder_text');
 
-  echo cs_html_roco(1,'leftc');
-  echo cs_icon('ksysguard') . $cs_lang['options'];
-  echo cs_html_roco(2,'leftb');  
-  echo cs_html_vote('autoresponder_id',$cs_messages['autoresponder_id'],'hidden');
-  echo cs_html_vote('update',$update,'hidden');
-  echo cs_html_vote('submit',$cs_lang['edit'],'submit');
-  echo cs_html_vote('preview',$cs_lang['preview'],'submit');
-  echo cs_html_vote('reset',$cs_lang['reset'],'reset');
-  echo cs_html_roco(0);
-  echo cs_html_table(0);
-  echo cs_html_form(0);
+  $data['check']['close'] = empty($cs_messages['autoresponder_close']) ? '' : 'checked="checked"';
+
+	$data['autoresponder']['id'] = $cs_messages['autoresponder_id'];
+	$data['autoresponder']['update'] = $update;
+
+
+ echo cs_subtemplate(__FILE__,$data,'messages','autoresponder');
 }
-else 
-{
-  if(empty($update))
-  {
-    $messages_cells = array_keys($cs_messages);
-    $messages_save = array_values($cs_messages);
-    cs_sql_insert(__FILE__,'autoresponder',$messages_cells,$messages_save);
-    
-    cs_redirect('','messages','center');
-  }
-  else
-  {
-    $messages_cells = array_keys($cs_messages);
-    $messages_save = array_values($cs_messages);
+else {
+
+	$messages_cells = array_keys($cs_messages);
+	$messages_save = array_values($cs_messages);
+
+  if(empty($update)) {
+	 	cs_sql_insert(__FILE__,'autoresponder',$messages_cells,$messages_save);
+    $msg = $cs_lang['create_done'];
+  } else {
     cs_sql_update(__FILE__,'autoresponder',$messages_cells,$messages_save,$cs_messages['autoresponder_id']);
-    
-
-    cs_redirect('','messages','center');
+    $msg = $cs_lang['changes_done'];
   }
+ cs_redirect($msg,'messages','center');
 } 
 
 ?>
---
+++ themes/base/messages/autoresponder.tpl
@@ -0,0 +1,79 @@
+<table class="forum" style="width:{page:width}" cellpadding="0" cellspacing="{page:cellspacing}">
+	<tr>
+		<td class="headb">{lang:mod} - {lang:edit}</td>
+	</tr>
+	<tr>
+		<td class="leftb">{head:body}</td>
+	</tr>
+</table>
+<br />
+
+{if:preview}
+<table class="forum" style="width:{page:width}" cellpadding="0" cellspacing="{page:cellspacing}">
+	<tr>
+		<td class="headb" colspan="2">{lang:mod} - {lang:text}</td>
+	</tr>
+	<tr>
+		<td class="leftb">{lang:subjeect}</td>
+		<td class="leftb">{lang:autoresponder} {autoresponder:subject}</td>
+	</tr>
+	<tr>
+		<td class="leftb">{lang:date}</td>
+		<td class="leftb">{autoresponder:time}</td>
+	</tr>
+	<tr>
+		<td class="leftb">{lang:to}</td>
+		<td class="leftb"></td>
+	</tr>
+	<tr>
+		<td class="leftb">{lang:options}</td>
+		<td class="leftb">
+			{icon:back}
+			{icon:mail_replay}
+			{icon:mail_delete}
+		</td>
+	</tr>
+	<tr>
+		<td class="headb">{autoresponder:subject}</td>
+	</tr>
+	<tr>
+		<td class="leftb" colspan="2">
+			{autoresponder:text}
+		</td>
+	</tr>
+</table>
+<br />
+{stop:preview}
+
+<form method="post" id="messages_autoresponder" action="{url:messages_autoresponder}">
+<table class="forum" style="width:{page:width}" cellpadding="0" cellspacing="{page:cellspacing}">
+	<tr>
+		<td class="leftc">{icon:kedit} {lang:subject} *</td>
+		<td class="leftb"><input type="text" name="autoresponder_subject" value="{data:autoresponder_subject}" maxlength="200" size="50" /></td>
+	</tr>
+	<tr>
+		<td class="leftc">{icon:kate} {lang:text} *<br />
+			<br />
+			{abcode:smileys}
+		</td>
+		<td class="leftb">
+			{abcode:features}
+			<textarea name="autoresponder_text" cols="50" rows="20" id="autoresponder_text">{data:autoresponder_text}</textarea>
+		</td>
+	</tr>
+	<tr>
+		<td class="leftc">{icon:configure} {lang:more}</td>
+		<td class="leftb"><input type="checkbox" name="autoresponder_close" value="1" {check:close}/> {lang:messages_autoresponder}</td>
+	</tr>
+	<tr>
+		<td class="leftc">{icon:ksysguard} {lang:options}</td>
+		<td class="leftb">
+			<input type="hidden" name="autoresponder_id" value="{autoresponder:id}"/>
+			<input type="hidden" name="update" value="{autoresponder:update}"/>
+			<input type="submit" name="submit" value="{lang:edit}"/>
+			<input type="submit" name="preview" value="{lang:preview}"/>
+			<input type="reset" name="reset" value="{lang:reset}"/>
+		</td>
+	</tr>
+</table>
+</form>
