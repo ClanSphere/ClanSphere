577b208296f7e0af1b7306090c22c3cf6f9a0508
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-21 16:13:39 +0000

    updated translation inside updates mod

---
+++ lang/English/updates.php
@@ -0,0 +1,40 @@
+<?php
+// ClanSphere 2009 - www.clansphere.net
+// $Id: updates.php 2266 2009-03-21 10:37:39Z duRiel $
+
+$cs_lang['mod']  = 'Updates';
+$cs_lang['mod_text']  = 'Receive online updates for ClanSphere';
+
+$cs_lang['not_avaible']  = "Your webspace does not support the online update feature. 
+The PHP extension zip is required to use it. More information:";
+
+$cs_lang['no_updates']  = "Could not find new update packages on the remote server. 
+As an alternative you can check on www.clansphere.net for new update packages";
+ 
+$cs_lang['notification'] = 'Notes';
+$cs_lang['packet']  = 'Pakage';
+$cs_lang['update']  = 'Update ClanSphere';
+$cs_lang['details']  = 'Update Details';
+$cs_lang['complete_changelog']  = '(Watch update details for more)';
+$cs_lang['notification_update'] = 'Update note:';
+
+$cs_lang['notice'] = "Welcome to the ClanSphere online update system.
+
+Please be sure that a backuped version of your current clansphere directory is available.
+The online update installs the selected version of ClanSphere on this webspace.
+
+Errors may occur since this update service is completely automated.
+ClanSphere is not responsible but also not accountable for any errors that appear due to the update procedure.
+If you encounter any problems please post them to the ClanSphere board.";
+
+$cs_lang['agree'] = 'Agree';
+$cs_lang['disagree'] = 'Disagree';
+
+$cs_lang['error_download'] = '- Failed to download the update pakage from update server';
+$cs_lang['error_download'] = '- Failed to unzip the update pakage from update server';
+$cs_lang['error_sql'] = '- Failed to install the new database update from update server';
+$cs_lang['error_delete'] = '- Failed to remove some unused pakage files';
+
+$cs_lang['success_update'] = 'Online update of ClanSphere finished successfully';
+
+?>
--- lang/German/updates.php
+++ lang/German/updates.php
@@ -9,31 +9,32 @@ $cs_lang['not_avaible']  = "Auf Ihrem Webspace sind Online Updates nicht verf&uu
 n&ouml;tige PHP Bibliothek zip. Weitere Infos unter:";
 
 $cs_lang['no_updates']  = "Aktuell sind keine neuen UpdatePakete auf dem Server vorhanden. 
-Auf www.clansphere.de werden Sie dar&uuml;ber informiert ob neue UpdatePakete zur verf&uuml;gung stehen.";
+Auf www.clansphere.net werden Sie dar&uuml;ber informiert ob neue UpdatePakete zur verf&uuml;gung stehen.";
  
 $cs_lang['notification'] = 'Hinweis';
-$cs_lang['packet']  = 'Paket'; 
-$cs_lang['update']  = 'Update ClanSphere';    
-$cs_lang['details']  = 'Updatedetails'; 
-$cs_lang['complete_changelog']  = '(Komplettes Changelog unter Details)';   
-$cs_lang['notification_update'] = 'Updatehinweis:';
-$cs_lang['notice'] = "Willkommen im OnlineUpdateSystem von ClanSphere.      
+$cs_lang['packet']  = 'Paket';
+$cs_lang['update']  = 'Update ClanSphere';
+$cs_lang['details']  = 'Update Details';
+$cs_lang['complete_changelog']  = '(Komplettes Changelog unter Details)';
+$cs_lang['notification_update'] = 'Update Hinweis:';
 
-Bitte stellen Sie sicher dass, Sie vor jedem Update ein Backup ihres ClanSphere Verzeichniss erstellen.
-Das OnlineUpdate aktualisiert Ihr ClanSphere auf die von Ihnen gewaehlte Version.
+$cs_lang['notice'] = "Willkommen im Online Update System von ClanSphere.
+
+Bitte stellen Sie sicher, dass Sie vor jedem Update ein Backup des ClanSphere Verzeichnises erstellen.
+Das Online-Update aktualisiert ClanSphere auf die von Ihnen gew&auml;hlte Version.
 
 Wie bei jedem automatisierten Service kann es auch hierbei zu Fehlern kommen. 
-Wir bitten diese zu Entschuldigen. 
-Desweiteren ist ClanSphere nicht zur Verantwortung zu ziehen, fuer Fehler die waehrend oder durch das OnlineUpdate an Ihrer Homepage entstehen. 
-F&uuml;r solche Probleme erkundigen Sie sich bitte im ClanSphere Forum.";
+Desweiteren ist ClanSphere nicht Haftbar bzw. nicht in der Verantwortung, sofern Fehler durch den Vorgang entstehen. 
+Bei Problemen erkundigen Sie sich bitte im ClanSphere Forum.";
 
 $cs_lang['agree'] = 'Einverstanden';
-$cs_lang['disagree'] = 'Nicht Einverstanden'; 
+$cs_lang['disagree'] = 'Nicht Einverstanden';
 
-$cs_lang['error_download'] = '- Fehler beim Download des Paketes vom UpdateServer';
-$cs_lang['error_download'] = '- Fehler beim Entpacken des Paketes vom UpdateServer'; 
-$cs_lang['error_sql'] = '- Fehler bei der Datenbank Aktualisierung des Paketes vom UpdateServer'; 
-$cs_lang['error_delete'] = '- Fehler beim Entfernen der nicht ben&ouml;tigten Paketdaten'; 
+$cs_lang['error_download'] = '- Fehler beim Download des Paketes vom Update-Server';
+$cs_lang['error_download'] = '- Fehler beim Entpacken des Paketes vom Update-Server';
+$cs_lang['error_sql'] = '- Fehler bei der Datenbank Aktualisierung des Paketes vom Update-Server';
+$cs_lang['error_delete'] = '- Fehler beim Entfernen der nicht ben&ouml;tigten Paketdaten';
 
-$cs_lang['success_update'] = 'OnlineUpdate von ClanSphere erfolgreich.';
+$cs_lang['success_update'] = 'Online-Update von ClanSphere erfolgreich.';
+
 ?>
