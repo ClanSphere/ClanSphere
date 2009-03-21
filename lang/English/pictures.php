3459eaa98f882763d486b69be5250be251993a41
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-21 16:15:47 +0000

    added translation of pictures mod

---
+++ lang/English/pictures.php
@@ -0,0 +1,11 @@
+<?php
+// ClanSphere 2009 - www.clansphere.net
+// $Id:  $
+
+$cs_lang['pictures'] = 'Pictures';
+$cs_lang['mod_info'] = 'Manage the pictures of modules';
+
+$cs_lang['picture'] = 'Picture';
+$cs_lang['del_picture'] = 'Remove picture';
+
+?>
