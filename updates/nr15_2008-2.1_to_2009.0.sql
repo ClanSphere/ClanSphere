df17f5b39a2769ef93e10bb94a0e44cda02b732c
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-21 13:38:26 +0000

    small changes to update sql file

--- updates/nr15_2008-2.1_to_2009.0.sql
+++ updates/nr15_2008-2.1_to_2009.0.sql
@@ -2,24 +2,6 @@ UPDATE {pre}_options SET options_value = '2009.0_svn' WHERE options_mod = 'clans
 UPDATE {pre}_options SET options_value = '2008-09-20' WHERE options_mod = 'clansphere' AND options_name = 'version_date';
 UPDATE {pre}_options SET options_value = '35' WHERE options_mod = 'clansphere' AND options_name = 'version_id';
 
-INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('articles','max_navlist','4');
-INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('board', 'list_subforums', '1');
-INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('clansphere','ajax_navlists','');
-INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('clansphere','cellspacing','1');
-INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('clansphere','developer','0');
-INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('comments', 'allow_unreg', '0');
-INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('events', 'max_height', '600');
-INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('events', 'max_size', '204800');
-INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('events', 'max_width', '800');
-INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('events', 'req_fulladress', '0');
-INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('events', 'req_fullname', '0');
-INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('events', 'req_mobile', '0');
-INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('events', 'req_phone', '0');
-INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('games', 'max_height', '30');
-INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('games', 'max_size', '15360');
-INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('games', 'max_width', '30');
-INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('users', 'def_picture', '0');
-
 CREATE TABLE {pre}_eventguests (
   eventguests_id {serial},
   events_id int(8) NOT NULL default '0',
@@ -84,14 +66,32 @@ ALTER TABLE {pre}_faq DROP faq_time;
 ALTER TABLE {pre}_gallery DROP gallery_close;
 ALTER TABLE {pre}_gallery DROP gallery_download;
 
+INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('articles','max_navlist','4');
+INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('board', 'list_subforums', '1');
+INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('clansphere','ajax_navlists','');
+INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('clansphere','cellspacing','1');
+INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('clansphere','developer','0');
+INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('comments', 'allow_unreg', '0');
+INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('events', 'max_height', '600');
+INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('events', 'max_size', '204800');
+INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('events', 'max_width', '800');
+INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('events', 'req_fulladress', '0');
+INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('events', 'req_fullname', '0');
+INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('events', 'req_mobile', '0');
+INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('events', 'req_phone', '0');
+INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('games', 'max_height', '30');
+INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('games', 'max_size', '15360');
+INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('games', 'max_width', '30');
+INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('users', 'def_picture', '0');
+
 CREATE INDEX {pre}_boardmods_categories_id_index ON {pre}_boardmods (categories_id);
-CREATE INDEX {pre}_eventguests_events_id_index ON {pre}_eventguests (events_id);
+CREATE INDEX {pre}_eventguests_events_id_index ON {pre}_eventguests (events_id);so
 CREATE INDEX {pre}_eventguests_users_id_index ON {pre}_eventguests (users_id);
 CREATE INDEX {pre}_pictures_pictures_fid_index ON {pre}_pictures (pictures_fid);
 CREATE INDEX {pre}_pictures_pictures_id_index ON {pre}_pictures (pictures_id);
 CREATE INDEX {pre}_medalsuser_medals_id_index ON {pre}_medalsuser (medals_id);
 CREATE INDEX {pre}_medalsuser_users_id_index ON {pre}_medalsuser (users_id);
 
-ALTER TABLE {pre}_count ADD INDEX (count_ip,count_id,count_time);
-ALTER TABLE {pre}_captcha ADD INDEX (captcha_ip,captcha_time,captcha_id);
-ALTER TABLE {pre}_comments ADD INDEX (comments_mod,comments_fid,comments_id);
+CREATE INDEX {pre}_captcha_speedup_index ON {pre}_captcha (captcha_id, captcha_ip, captcha_time);
+CREATE INDEX {pre}_comments_speedup_index ON {pre}_comments (comments_fid, comments_id, comments_mod);
+CREATE INDEX {pre}_count_speedup_index ON {pre}_count (count_id, count_ip, count_time);
