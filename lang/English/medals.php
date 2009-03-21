bb46f632aa4e856c84e1e5e49377d010f7ceb215
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-21 16:21:25 +0000

    added translation of medals mod

---
+++ lang/English/medals.php
@@ -0,0 +1,35 @@
+<?php
+
+$cs_lang['mod'] = 'Medals';
+$cs_lang['modtext'] = 'Manage and award medals';
+
+// Manage
+$cs_lang['new_medal'] = 'Create new medal';
+$cs_lang['awarded'] = 'times awarded';
+
+// Create
+$cs_lang['description'] = 'Description';
+$cs_lang['picture'] = 'Picture';
+$cs_lang['user'] = 'User';
+
+$cs_lang['user_not_found'] = '- User not found';
+$cs_lang['no_name'] = '- No name is given';
+$cs_lang['wrong_ext'] = '- The filetype is not compatible';
+
+// Users
+$cs_lang['since'] = 'Since';
+
+// User
+$cs_lang['awardingto'] = 'Award to';
+
+// Remove
+$cs_lang['rly_remove'] = 'Should the medal %s bei removed?';
+
+// Edit
+$cs_lang['current_picture'] = 'Current picture';
+$cs_lang['new_picture'] = 'New picture';
+$cs_lang['remove_pic'] = 'Remove current picture';
+$cs_lang['update_date'] = 'Update the date';
+$cs_lang['extended'] = 'More';
+
+?>
