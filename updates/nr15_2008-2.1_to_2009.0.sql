573c0f21d4dee9e3a79922819cf6bd5cd26086ca
duRiel <duRiel@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-21 10:42:18 +0000

    changed version name in updates sql file

--- updates/nr15_2008-2.1_to_2009.0.sql
+++ updates/nr15_2008-2.1_to_2009.0.sql
@@ -1,4 +1,4 @@
-UPDATE {pre}_options SET options_value = '2008.3_svn' WHERE options_mod = 'clansphere' AND options_name = 'version_name';
+UPDATE {pre}_options SET options_value = '2009.0_svn' WHERE options_mod = 'clansphere' AND options_name = 'version_name';
 UPDATE {pre}_options SET options_value = '2008-09-20' WHERE options_mod = 'clansphere' AND options_name = 'version_date';
 UPDATE {pre}_options SET options_value = '35' WHERE options_mod = 'clansphere' AND options_name = 'version_id';
 
