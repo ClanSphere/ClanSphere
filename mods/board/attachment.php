9022772e7c2864ceb61d5345bff67614beda3a13
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-22 13:05:10 +0000

    path fixes in attachment.php

--- mods/board/attachment.php
+++ mods/board/attachment.php
@@ -34,17 +34,17 @@ $name = strlen($file);
 $ext = substr($file,$name - $extension + 1,$name); 
 
 if(!empty($id)) { 
-  $file_path = "uploads/board/files/".$cs_thread_file['boardfiles_id'].'.'.$ext;
+  $file_path = "../../uploads/board/files/".$cs_thread_file['boardfiles_id'].'.'.$ext;
 }
 else {  
-  $file_path = "uploads/board/files/".$cs_thread_file['boardfiles_name'];
+  $file_path = "../../uploads/board/files/".$cs_thread_file['boardfiles_name'];
 }
 
 if(!is_file($file_path))
   die(cs_error_internal(0, 'File not found'));
 
 if($ext == 'jpg' OR $ext=='JPG' OR $ext == 'jpeg' OR $ext=='JPEG' OR $ext == 'gif' OR $ext=='GIF' OR $ext == 'bmp' OR $ext=='BMP' OR $ext == 'png' OR $ext=='PNG') {
-  echo "<img src='../../$file_path' alt='$file'></img>";  
+  echo "<img src='$file_path' alt='$file'></img>";  
 }
 else {
   header("Pragma: public");
