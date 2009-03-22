f98d75969a14ef97b332caf1595e3a23ba3eca74
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-22 12:24:42 +0000

    fixed translation in errors mod (bugtracker id 444)

--- lang/German/errors.php
+++ lang/German/errors.php
@@ -14,7 +14,7 @@ $cs_lang['403_action'] = 'Zugriff verweigert';
 $cs_lang['403_body']   = 'Die von Ihnen angeforderte Seite ben&ouml;tigt eine h&ouml;here Berechtigung.';
 
 $cs_lang['500_action']    = 'Interne Ursache';
-$cs_lang['500_body']      = 'Die ausf&uuml;hrung der Seite wurde angehalten. Details sind, sofern aktiviert, im Log verf&uuml;gbar.';
+$cs_lang['500_body']      = 'Die Ausf&uuml;hrung der Seite wurde angehalten. Details sind, sofern aktiviert, im Log verf&uuml;gbar.';
 $cs_lang['500_err_sql']   = 'Eine systemrelevante SQL-Anfrage ist aus folgendem Grund fehlgeschlagen:';
 $cs_lang['500_err_tpl']   = 'Beim verarbeiten des aktivierten Templates ist ein Fehler aufgetreten:';
 $cs_lang['500_err_setup'] = 'Die Datei setup.php konnte nicht gefunden werden. Sofern noch nicht geschehen bitte die Installation ausf&uuml;hren.';
