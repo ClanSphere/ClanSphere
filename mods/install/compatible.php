1504873d87f80c4a7d704869bfefcee56a225d49
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-22 00:25:50 +0000

    removed cs_html_div from install mod (done)

--- mods/install/compatible.php
+++ mods/install/compatible.php
@@ -5,10 +5,9 @@
 $cs_lang = cs_translate('install');
 
 $data = array();
-
 $ok = array();
 $ok[0] = cs_icon('stop');
-$ok[1] = cs_html_img('symbols/crystal_project/16/submit.png');#cs_icon('submit');
+$ok[1] = cs_html_img('symbols/crystal_project/16/submit.png'); #cs_icon('submit');
 
 $switch = array();
 $switch[0] = $cs_lang['off'];
@@ -44,37 +43,37 @@ $check_recommended = true;
 
 $data['rc']['php'] = '5.2.0';
 $comparison = version_compare($data['av']['php'],$data['rc']['php'],'>=');
-if (empty($comparison)) { $data['av']['php'] = cs_html_div(1,'color:#B91F1D') . $data['av']['php'] . cs_html_div(0); $check_recommended = 0;}
+if (empty($comparison)) { $data['av']['php'] = '<div style="color:#B91F1D">' . $data['av']['php'] . '</div>'; $check_recommended = 0;}
 
 $data['rc']['register_globals'] = $switch[0];
 $data['av']['register_globals'] = $switch[(int) ini_get('register_globals')];
-if ($data['av']['register_globals'] != $data['rc']['register_globals']) { $data['av']['register_globals'] = cs_html_div(1,'color:#B91F1D') . $data['av']['register_globals'] . cs_html_div(0); $check_recommended = 0; }
+if ($data['av']['register_globals'] != $data['rc']['register_globals']) { $data['av']['register_globals'] = '<div style="color:#B91F1D">' . $data['av']['register_globals'] . '</div>'; $check_recommended = 0; }
 
 $data['rc']['magic_quotes'] = $switch[0];
 $data['av']['magic_quotes'] = $switch[(int) ini_get('magic_quotes_gpc')];
-if ($data['av']['magic_quotes'] != $data['rc']['magic_quotes']) { $data['av']['magic_quotes'] = cs_html_div(1,'color:#B91F1D') . $data['av']['magic_quotes'] . cs_html_div(0); $check_recommended = 0; }
+if ($data['av']['magic_quotes'] != $data['rc']['magic_quotes']) { $data['av']['magic_quotes'] = '<div style="color:#B91F1D">' . $data['av']['magic_quotes'] . '</div>'; $check_recommended = 0; }
 
 $data['rc']['safe_mode'] = $switch[0];
 $data['av']['safe_mode'] = $switch[(int) ini_get('safe_mode')];
-if ($data['av']['safe_mode'] != $data['rc']['safe_mode']) { $data['av']['safe_mode'] = cs_html_div(1,'color:#B91F1D') . $data['av']['safe_mode'] . cs_html_div(0); $check_recommended = 0; }
+if ($data['av']['safe_mode'] != $data['rc']['safe_mode']) { $data['av']['safe_mode'] = '<div style="color:#B91F1D">' . $data['av']['safe_mode'] . '</div>'; $check_recommended = 0; }
 
 $data['rc']['trans_sid'] = $switch[0];
 $data['av']['trans_sid'] = $switch[(int) ini_get('session.use_trans_sid')];
-if ($data['av']['trans_sid'] != $data['rc']['trans_sid']) { $data['av']['trans_sid'] = cs_html_div(1,'color:#B91F1D') . $data['av']['trans_sid'] . cs_html_div(0); $check_recommended = 0; }
+if ($data['av']['trans_sid'] != $data['rc']['trans_sid']) { $data['av']['trans_sid'] = '<div style="color:#B91F1D">' . $data['av']['trans_sid'] . '</div>'; $check_recommended = 0; }
 
 $data['rc']['basedir'] = $switch[1];
 $basedir = ini_get('open_basedir');
 $basedir = empty($basedir) ? 0 : 1;
 $data['av']['basedir'] = $switch[$basedir];
-if ($data['av']['basedir'] != $data['rc']['basedir']) { $data['av']['basedir'] = cs_html_div(1,'color:#B91F1D') . $data['av']['basedir'] . cs_html_div(0); $check_recommended = 0; }
+if ($data['av']['basedir'] != $data['rc']['basedir']) { $data['av']['basedir'] = '<div style="color:#B91F1D">' . $data['av']['basedir'] . '</div>'; $check_recommended = 0; }
 
 $data['rc']['fopen'] = $switch[1];
 $data['av']['fopen'] = $switch[(int) ini_get('allow_url_fopen')];
-if ($data['av']['fopen'] != $data['rc']['fopen']) { $data['av']['fopen'] = cs_html_div(1,'color:#B91F1D') . $data['av']['fopen'] . cs_html_div(0); $check_recommended = 0; }
+if ($data['av']['fopen'] != $data['rc']['fopen']) { $data['av']['fopen'] = '<div style="color:#B91F1D">' . $data['av']['fopen'] . '</div>'; $check_recommended = 0; }
 
 $data['rc']['gd'] = $switch[1];
 $data['av']['gd'] = $switch[(int) extension_loaded('gd')];
-if ($data['av']['gd'] != $data['rc']['gd']) { $data['av']['gd'] = cs_html_div(1,'color:#B91F1D') . $data['av']['gd'] . cs_html_div(0); $check_recommended = 0; }
+if ($data['av']['gd'] != $data['rc']['gd']) { $data['av']['gd'] = '<div style="color:#B91F1D">' . $data['av']['gd'] . '</div>'; $check_recommended = 0; }
 
 if (!empty($check_required) && !empty($check_recommended)) {
   $data['info']['result'] = $cs_lang['check_perfect'] . cs_html_br(2) . cs_link($cs_lang['continue'],'install','license','lang=' . $account['users_lang']);
