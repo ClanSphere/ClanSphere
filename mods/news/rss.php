a12c898be6e3fad4cc6c535c598b1a8b086644df
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-24 22:40:34 +0000

    abcode feature options are now dynamic for rss feed generation (fckeditor content support added)

--- mods/news/rss.php
+++ mods/news/rss.php
@@ -10,7 +10,11 @@ $select .= 'usr.users_email AS author, cat.categories_name AS cat, nws.news_id A
 $where = 'nws.news_public = 1 AND cat.categories_access < 2';
 $cs_news = cs_sql_select(__FILE__,$from,$select,$where,'news_time DESC',0,8);
 
+$cs_option = cs_sql_option(__FILE__, 'news');
+$abcode = explode(",", $cs_option['abcode']);
+
 include_once('mods/rss/generate.php');
-cs_update_rss('news','view',$op_news['rss_title'],$op_news['rss_description'],$cs_news);
+
+cs_update_rss('news','view',$op_news['rss_title'],$op_news['rss_description'],$cs_news, $abcode);
 
 ?>
