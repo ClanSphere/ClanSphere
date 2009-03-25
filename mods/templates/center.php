b0dba761ec57885003b4b8ab9cfe9891945e6d33
duRiel <duRiel@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-25 00:57:13 +0000

    ajax: fixed users template (bugtracker id 456, thx to spongebob)

--- mods/templates/center.php
+++ mods/templates/center.php
@@ -4,6 +4,20 @@
 
 $cs_lang = cs_translate('templates');
 
+if (!empty($_SESSION['tpl_preview'])) $ending = substr($_SESSION['tpl_preview'],-3);
+if (!empty($ending) && $ending != '{2}') {
+  $_SESSION['tpl_preview'] .= '{2}';
+  die();
+} elseif (!empty($ending)) {
+  $cs_main['template'] = substr($_SESSION['tpl_preview'],0,-3);
+  $ajax_preview = 1;
+  unset($_SESSION['tpl_preview']);
+} elseif (!empty($_GET['template']) && !empty($account['users_ajax'])) {
+  $_SESSION['tpl_preview'] = $_GET['template'];
+  $shorten = "window.location.href = window.location.href.substr(0,window.location.href.lastIndexOf('template')); ";
+  die(ajax_js($shorten . "window.location.reload();"));
+}
+
 $templates = cs_checkdirs('templates');
 $tpl_all = count($templates);
 
@@ -13,8 +27,8 @@ $allow = 0;
 if(!empty($activate)) {
   foreach($templates as $mod) {
     if($mod['dir'] == $activate) {
-    $allow++;
-  }
+      $allow++;
+    }
   }
 }
 
@@ -26,11 +40,13 @@ if(!empty($activate) AND !empty($allow)) {
   $def_cont = array($activate);
   cs_sql_update(__FILE__,'users',$def_cell,$def_cont,0,$opt_where);
   
-  cs_redirect($cs_lang['success'],'templates','center');
-  
-  if(file_exists('themes/'.$activate)) {
-    cs_redirect($cs_lang['theme_found'],'templates','center'); 
+  if(!empty($account['users_ajax']) && $cs_main['php_self']['filename'] == 'content') {
+    cs_redirectmsg($cs_lang['success']);
+    die(ajax_js('window.location.href=window.location.href.substr(0,window.location.href.lastIndexOf(\'activate\'));window.location.reload()'));
+  } else {
+    cs_redirect($cs_lang['success'],'templates','center');
   }
+  
 }
 else {
   $data['lang']['getmsg'] = cs_getmsg();
@@ -41,15 +57,37 @@ else {
     $data['temp_list'] = '';
   }
   
+  if (empty($_GET['template']) && empty($ajax_preview)) {
+	  $cs_users = cs_sql_select(__FILE__,'users','users_tpl','users_id = "' . $account['users_id'] . '"');
+	  echo $cs_main['template'] . $cs_users['users_tpl'] . $cs_main['def_tpl'];
+	  if(!empty($cs_users['users_tpl']) && $cs_main['template'] == $cs_main['def_tpl']) {
+	    $cs_main['template'] = $cs_users['users_tpl'];
+	  } else {
+	    $cs_main['template'] = $cs_main['def_tpl'];
+	  }
+  }
+  
+  $cs_users = cs_sql_select(__FILE__,'users','users_tpl','users_id = "' . $account['users_id'] . '"');
+  
+  if(!empty($cs_users)) {
+    if(!empty($cs_users['users_tpl'])) {
+      $cs_main['def_tpl'] = $cs_users['users_tpl'];
+    } else {
+      $cs_main['def_tpl'] = $cs_main['def_tpl'];
+    }
+  } else {
+    $cs_main['def_tpl'] = $cs_main['def_tpl'];
+  }
+  
   foreach($templates as $mod) {
     $data['temp_list'][$run]['name'] = $mod['name'];
     $data['temp_list'][$run]['version'] = $mod['version'];
     $data['temp_list'][$run]['date'] = cs_date('date',$mod['released']);
-  
+  echo $cs_main['template'] . ' - '; 
     if(isset($_GET['template']) AND preg_match("=^[_a-z0-9-]+$=i",$_GET['template'])) {
-  $cs_main['template'] = $_GET['template'];
-}
-if($mod['dir'] == $cs_main['template']) { 
+      $cs_main['template'] = $_GET['template'];
+    }
+    if($mod['dir'] == $cs_main['template']) { 
       $data['temp_list'][$run]['preview'] = cs_icon('submit','16',$cs_lang['yes']);
     }
     elseif($mod['dir'] != 'install') {  
@@ -59,32 +97,7 @@ if($mod['dir'] == $cs_main['template']) {
       $data['temp_list'][$run]['preview'] = cs_icon('editdelete','16','----');
     }
   
-  $cs_users = cs_sql_select(__FILE__,'users','users_tpl','users_id = "' . $account['users_id'] . '"');
-    if(!empty($cs_users)) {
-          if(!empty($cs_users['users_tpl'])) {
-            $cs_main['template'] = $cs_users['users_tpl'];
-          }
-          else {
-            $cs_main['template'] = $cs_main['def_tpl'];
-          }
-        }
-        else {
-          $cs_main['template'] = $cs_main['def_tpl'];
-        }
     
-    
-    $cs_users = cs_sql_select(__FILE__,'users','users_tpl','users_id = "' . $account['users_id'] . '"');
-    if(!empty($cs_users)) {
-          if(!empty($cs_users['users_tpl'])) {
-            $cs_main['def_tpl'] = $cs_users['users_tpl'];
-          }
-          else {
-            $cs_main['def_tpl'] = $cs_main['def_tpl'];
-          }
-        }
-        else {
-          $cs_main['def_tpl'] = $cs_main['def_tpl'];
-        }
 
     if($mod['dir'] == $cs_main['def_tpl']) { 
       $data['temp_list'][$run]['active'] = cs_icon('submit','16',$cs_lang['yes']);
