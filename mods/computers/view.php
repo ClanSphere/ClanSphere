3383cab238ec3b9e87261dc120064728a4658bb8
GaHero <GaHero@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-27 13:44:32 +0000

    fixed computers/view linebreaks (#35)

--- mods/computers/view.php
+++ mods/computers/view.php
@@ -16,10 +16,10 @@ $cs_users = cs_sql_select(__FILE__,'users','users_nick, users_active, users_dele
 $data['com']['user'] = cs_user($cs_computers['users_id'],$cs_users['users_nick'], $cs_users['users_active'], $cs_users['users_delete']);
 
 $data['com']['since'] = cs_date('unix',$cs_computers['computers_since'],1);
-$data['com']['name'] = cs_secure($cs_computers['computers_name']);
-$data['com']['software'] = cs_secure($cs_computers['computers_software']);
-$data['com']['mainboard'] = cs_secure($cs_computers['computers_mainboard']);
-$data['com']['memory'] = cs_secure($cs_computers['computers_memory']);
+$data['com']['name'] = cs_secure($cs_computers['computers_name'],1);
+$data['com']['software'] = cs_secure($cs_computers['computers_software'],1);
+$data['com']['mainboard'] = cs_secure($cs_computers['computers_mainboard'],1);
+$data['com']['memory'] = cs_secure($cs_computers['computers_memory'],1);
 
 if(empty($cs_computers['computers_pictures'])) {
   $data['pictures'][0]['thumb'] = $cs_lang['nopic'];
@@ -34,15 +34,15 @@ else {
   }
 }
 
-$data['com']['processors'] = cs_secure($cs_computers['computers_processors']);
-$data['com']['graphics'] = cs_secure($cs_computers['computers_graphics']);
-$data['com']['sounds'] = cs_secure($cs_computers['computers_sounds']);
-$data['com']['harddisks'] = cs_secure($cs_computers['computers_harddisks']);
-$data['com']['drives'] = cs_secure($cs_computers['computers_drives']);
-$data['com']['screens'] = cs_secure($cs_computers['computers_screens']);
-$data['com']['interfaces'] = cs_secure($cs_computers['computers_interfaces']);
-$data['com']['networks'] = cs_secure($cs_computers['computers_networks']);
-$data['com']['more'] = cs_secure($cs_computers['computers_more']);
+$data['com']['processors'] = cs_secure($cs_computers['computers_processors'],1);
+$data['com']['graphics'] = cs_secure($cs_computers['computers_graphics'],1);
+$data['com']['sounds'] = cs_secure($cs_computers['computers_sounds'],1);
+$data['com']['harddisks'] = cs_secure($cs_computers['computers_harddisks'],1);
+$data['com']['drives'] = cs_secure($cs_computers['computers_drives'],1);
+$data['com']['screens'] = cs_secure($cs_computers['computers_screens'],1);
+$data['com']['interfaces'] = cs_secure($cs_computers['computers_interfaces'],1);
+$data['com']['networks'] = cs_secure($cs_computers['computers_networks'],1);
+$data['com']['more'] = cs_secure($cs_computers['computers_more'],1);
 
 
 echo cs_subtemplate(__FILE__,$data,'computers','view');
