78c2277507d66139c8aa5c63031419b9c2f4e63e
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-03-29 14:48:15 +0000

    added def_action to features.php

--- features.php
+++ features.php
@@ -3,7 +3,7 @@
 // $Id$
 
 # Overwrite global settings by using the following array
-$cs_main = array('init_sql' => true, 'init_tpl' => true, 'tpl_file' => 'features.htm', 'def_width' => '100%', 'def_mod' => 'abcode');
+$cs_main = array('init_sql' => true, 'init_tpl' => true, 'tpl_file' => 'features.htm', 'def_width' => '100%', 'def_mod' => 'abcode', 'def_action' => 'listimg');
 
 require_once 'system/core/functions.php';
 
