7cc5c18277672910fbb502b2d1816122ac6104da
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-04-02 15:23:18 +0000

    fixed users nopic to be shown with mod_rewrite

--- mods/users/options.php
+++ mods/users/options.php
@@ -6,10 +6,11 @@ $cs_lang = cs_translate('users');
 
 $data = array();
 $data['options'] = cs_sql_option(__FILE__,'users');
+
 $files = cs_files();
 
 if(isset($_POST['submit'])) {
-  
+
   require 'mods/clansphere/func_options.php';
 	$error = '';
   $save = array();
@@ -24,28 +25,26 @@ if(isset($_POST['submit'])) {
   if(!empty($files['def_picture']['tmp_name'])) $save['def_picture'] =  1;
 
   cs_optionsave('users', $save);
-  
+
   if (!empty($save['def_picture']) && empty($data['options']['def_picture']))
     cs_sql_update(__FILE__, 'users', array('users_picture'), array('nopicture.jpg'), 0, "users_picture = ''");
   elseif (empty($save['def_picture']) && !empty($data['options']['def_picture']))
     cs_sql_update(__FILE__, 'users', array('users_picture'), array(''), 0, "users_picture = 'nopicture.jpg'");
-  
+
   $data['link']['continue'] = cs_url('clansphere','system');
   $data['lang']['head'] = $cs_lang['options'];
-  
+
   if (!empty($files['def_picture']['tmp_name'])) {
     $img_size = getimagesize($files['def_picture']['tmp_name']);
-    
+
     if ($files['def_picture']['type'] != 'image/jpeg') $error .= cs_html_br(1) . $cs_lang['ext_error'];
     if ($files['def_picture']['size'] > $_POST['max_size']) $error .= cs_html_br(1) . $cs_lang['too_big'];
     if ($img_size[0] > $_POST['max_width']) $error .= cs_html_br(1) . $cs_lang['too_wide'];
     if ($img_size[1] > $_POST['max_height']) $error .= cs_html_br(1) . $cs_lang['too_high'];
-
     if (empty($error)) cs_upload('users','nopicture.jpg',$files['def_picture']['tmp_name']);
   }
-  
+
   if (empty($error)) cs_redirect($cs_lang['success'], 'options', 'roots');
-  
 }
 
 if(!isset($_POST['submit']) || !empty($error)) {
@@ -64,11 +63,11 @@ if(!isset($_POST['submit']) || !empty($error)) {
   $data['options']['register_off'] = cs_html_option($cs_lang['off'],0,$sel);
   $sel = !empty($data['options']['register']) ? 1 : 0;
   $data['options']['register_on'] =  cs_html_option($cs_lang['on'],1,$sel);
-  
+
   $data['selected']['def_picture'] = empty($data['options']['def_picture']) ? '' : 'checked="checked" ';
-  
+  $data['img']['nopic'] = cs_html_img('uploads/users/nopic.jpg');
+
   echo cs_subtemplate(__FILE__,$data,'users','options');
-  
 }
 
 ?>
