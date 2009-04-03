089d1eb215d5fe4cb0b383ee755b40b7a654e68d
duRiel <duRiel@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-04-03 15:48:05 +0000

    fixed a bug in sending emails to users with enabled mod rewrite (refs #98)

--- system/output/xhtml_10_old.php
+++ system/output/xhtml_10_old.php
@@ -106,21 +106,22 @@ function cs_html_img($url, $height = 0, $width = 0, $more = 0, $alt = '')
 function cs_html_mail($mail, $link = '')
 {
   global $cs_main;
-	$email = explode("@", $mail);
-  $domain = empty($email[1]) ? array(0,1) : explode(".", $email[1]);
-  $link = empty($link) ? $email[0] . ' (at) ' . $domain[0] . ' (dot) ' . $domain[1] : $link;
-  $str = base64_encode($mail);
-  
-  return '<a href="javascript:cs_ajax_request(\''.$cs_main['php_self']['dirname'].'mods/clansphere/mail.php?mail=' . $str . '\',function(request){window.location=\'mailto:\'+request.responseText;})">' . $link . '</a>';
-}
-function cs_html_msnmail($mail, $link = '')
-{
   $email = explode("@", $mail);
   $domain = empty($email[1]) ? array(0,1) : explode(".", $email[1]);
   $link = empty($link) ? $email[0] . ' (at) ' . $domain[0] . ' (dot) ' . $domain[1] : $link;
   $str = base64_encode($mail);
   
-  return '<a href="javascript: cs_ajax_request(\'mods/clansphere/mail.php?mail=' . $str . '\',function(http_request){window.location=\'http://members.msn.com/\'+http_request.responseText;})">' . $link . '</a>';
+  return '<a href="#" onclick="cs_ajax_request(\''.$cs_main['php_self']['dirname'].'mods/clansphere/mail.php?mail=' . $str . '\',function(http_request){window.location=\'mailto:\'+http_request.responseText;})">' . $link . '</a>';
+}
+function cs_html_msnmail($mail, $link = '')
+{
+  global $cs_main;
+  $email = explode("@", $mail);
+  $domain = empty($email[1]) ? array(0,1) : explode(".", $email[1]);
+  $link = empty($link) ? $email[0] . ' (at) ' . $domain[0] . ' (dot) ' . $domain[1] : $link;
+  $str = base64_encode($mail);
+  
+  return '<a href="#" onclick="cs_ajax_request(\''.$cs_main['php_self']['dirname'].'mods/clansphere/mail.php?mail=' . $str . '\',function(http_request){window.location=\'http://members.msn.com/\'+http_request.responseText;})">' . $link . '</a>';
 }
 
 function cs_html_link($url, $link, $use_target = 1, $class = 0, $title = 0, $more = 0)
