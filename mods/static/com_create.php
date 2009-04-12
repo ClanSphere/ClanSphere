3ff1aa60f23c5222bc523ef86e6b2b686094d617
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-04-12 07:02:44 +0000

    fixed #137

--- mods/static/com_create.php
+++ mods/static/com_create.php
@@ -3,6 +3,7 @@
 // Id: com_create.php (Tue Nov 18 10:44:11 CET 2008) fAY-pA!N
 
 $cs_lang = cs_translate('static');
+
 $cs_post = cs_post('fid');
 $cs_get = cs_get('id');
 
@@ -10,7 +11,7 @@ $fid = empty($cs_post['fid']) ? 0 : $cs_post['fid'];
 $quote_id = empty($cs_get['id']) ? 0 : $cs_get['id'];
 
 $cs_static = cs_sql_select(__FILE__,'static','static_comments',"static_id = '" . $fid . "'");
+$close = empty($cs_static['static_comments']) ? 1 : 0;
 
 require_once('mods/comments/functions.php');
-cs_commments_create($fid,'static','view',$quote_id,$cs_lang['mod'],$cs_static['static_comments']);
-?>
+cs_commments_create($fid,'static','view',$quote_id,$cs_lang['mod'],$close);
