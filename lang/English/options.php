32b573b3ab3dd57a107f37096166141c10cd4979
Fr33z3m4n <Fr33z3m4n@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-04-18 16:54:23 +0000

    fixed title for options

--- lang/English/options.php
+++ lang/English/options.php
@@ -2,6 +2,7 @@
 // ClanSphere 2009 - www.clansphere.net
 // $Id$
 
+$cs_lang['mod_name'] = 'Options';
 $cs_lang['head_roots'] = 'Base';
 $cs_lang['body_roots']  = 'All modules that support options.';
 $cs_lang['text']  = 'View and Set options';
--- lang/German/options.php
+++ lang/German/options.php
@@ -2,6 +2,7 @@
 // ClanSphere 2009 - www.clansphere.net
 // $Id$
 
+$cs_lang['mod_name'] = 'Optionen';
 $cs_lang['head_roots'] = 'Basis';
 $cs_lang['body_roots']  = 'Alle Module die Optionen anbieten.';
 $cs_lang['text']  = 'Speichern und Auslesen von Einstellungen';
