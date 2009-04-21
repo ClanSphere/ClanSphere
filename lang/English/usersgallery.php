3f9a5f14aa7495e452aa71a13738607850575b46
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-04-21 05:35:05 +0000

    added minimal usersgallery langfile for mod info

---
+++ lang/English/usersgallery.php
@@ -0,0 +1,8 @@
+<?php
+// ClanSphere 2009 - www.clansphere.net 
+// $Id$
+
+$cs_lang['mod_name'] = 'Usersgallery';
+$cs_lang['modtext'] = 'Use and manage Multimedia User Gallery';
+
+# further translation in gallery.php
---
+++ lang/German/usersgallery.php
@@ -0,0 +1,8 @@
+<?php
+// ClanSphere 2009 - www.clansphere.net 
+// $Id$
+
+$cs_lang['mod_name'] = 'Benutzer-Gallerie';
+$cs_lang['modtext'] = 'Multimedia Benutzer-Galerie nutzen und verwalten.';
+
+# further translation in gallery.php
