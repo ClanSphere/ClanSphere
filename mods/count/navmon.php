6ae76c844fcdbfa7d0342f7991d6da5fe9c05287
duRiel <duRiel@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-04-24 13:30:11 +0000

    Worked on the count archive so there is a daily archieve to increase the performance of big websites. please test since it is a bit beta

--- mods/count/navmon.php
+++ mods/count/navmon.php
@@ -8,4 +8,5 @@ $monthstart = mktime(0,0,0,$tmonth,1,$tyear);
 $monthstart = cs_timediff($monthstart, 1);
 
 $where = 'count_time > \'' . $monthstart . '\'';
-echo number_format(cs_sql_count(__FILE__,'count',$where),0,',','.');
+$count = cs_sql_count(__FILE__,'count',$where) + cs_sql_count(__FILE__, 'count_archive', 'count_mode = "1"');
+echo number_format($count,0,',','.');
--- updates/nr17_2009.0_RC2_to_2009.0_svn.sql
+++ updates/nr17_2009.0_RC2_to_2009.0_svn.sql
@@ -7,8 +7,12 @@ DELETE FROM {pre}_options WHERE options_mod = 'fckeditor' AND options_name = 'mo
 INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('abcode', 'rte_html', '');
 INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('abcode', 'rte_more', '');
 
+INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('counter', 'last_archiv_day', '1');
+
 ALTER TABLE {pre}_board ADD board_last_time int(14) NOT NULL DEFAULT '0';
 ALTER TABLE {pre}_board ADD board_last_user varchar(40) NOT NULL DEFAULT '';
 ALTER TABLE {pre}_board ADD board_last_userid int(8) NOT NULL DEFAULT '0';
 ALTER TABLE {pre}_board ADD board_last_thread varchar(200) NOT NULL default '';
 ALTER TABLE {pre}_board ADD board_last_threadid int(8) NOT NULL DEFAULT '0';
+
+ALTER TABLE {pre}_count_archiv ADD count_mode int(2) NOT NULL DEFAULT '0';
