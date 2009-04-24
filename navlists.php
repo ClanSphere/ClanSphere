d9f972a52389cf8bff82f4e1bcb0b314349a0e2d
duRiel <duRiel@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-04-24 11:16:26 +0000

    now the debug sql query log also includes the navlists of ajax

--- navlists.php
+++ navlists.php
@@ -22,24 +22,36 @@ $ajaxes = explode(',',$cs_main['ajax_navlists']);
 array_pop($ajaxes);
 $string = str_replace(',','',$cs_main['ajax_navlists']);
 
-$logsql = '';
-if (!empty($cs_main['developer']) OR $account['access_clansphere'] > 4) {
-  $cs_logs['php_errors'] = str_replace('\'','\\\'',nl2br($cs_logs['php_errors']));
-  $cs_logs['errors'] = str_replace('\'','\\\'',nl2br($cs_logs['errors']));
-  foreach($cs_logs['sql'] AS $sql_file => $sql_queries) {
-      $logsql .= cs_html_big(1) . str_replace('\\', '\\\\', $sql_file) . cs_html_big(0) . cs_html_br(1);
-    $logsql .= nl2br(htmlentities($sql_queries, ENT_QUOTES, $cs_main['charset']));
-  }
-} else {
-  $cs_logs['php_errors'] = '';
-  $cs_logs['errors'] = 'Developer mode is turned off';
+
+function cs_getlog() {
+	
+	global $cs_main, $account, $cs_logs;
+	
+	$logsql = '';
+	
+	if (!empty($cs_main['developer']) OR $account['access_clansphere'] > 4) {
+	  $cs_logs['php_errors'] = str_replace('\'','\\\'',nl2br($cs_logs['php_errors']));
+	  $cs_logs['errors'] = str_replace('\'','\\\'',nl2br($cs_logs['errors']));
+	  foreach($cs_logs['sql'] AS $sql_file => $sql_queries) {
+	      $logsql .= cs_html_big(1) . str_replace('\\', '\\\\', $sql_file) . cs_html_big(0) . cs_html_br(1);
+	    $logsql .= nl2br(htmlentities($sql_queries, ENT_QUOTES, $cs_main['charset']));
+	  }
+	} else {
+	  $cs_logs['php_errors'] = '';
+	  $cs_logs['errors'] = 'Developer mode is turned off';
+	}
+	
+	return $logsql;
 }
+
 
 if (!empty($string)) {
   $temp = '';
-  $specials = array('func_parse' => 'cs_parsetime($cs_micro)', 'func_queries' => '\''.$cs_logs['queries'].'\'', 'func_sql' => '\'' . $logsql . '\'',
+  $specials = array('func_parse' => 'cs_parsetime($cs_micro)', 'func_queries' => '\''.$cs_logs['queries'].'\'',
     'func_errors' => '\'' . $cs_logs["php_errors"] . $cs_logs["errors"] . '\'');
   $special_names = array('func_errors' => 'errors', 'func_sql' => 'sql');
+  
+  $last = end($ajaxes);
   
   foreach ($ajaxes as $ajax) {
     if (empty($ajax)) continue;
@@ -50,6 +62,9 @@ if (!empty($string)) {
       eval('$var = ' . $specials[$ajax] . ';');
       $temp .= '!33/' . $name . '!33/' . $var;
     }
+    if ($last == $ajax) $temp .= '!33/sql!33/' . cs_getlog();
   }
   echo $temp;
 }
+
+echo $logsql;
