61473829e8ca33ec2ad756b1152b11149121dbee
duRiel <duRiel@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-04-24 11:42:22 +0000

    forgot something in navlists.php

--- navlists.php
+++ navlists.php
@@ -15,7 +15,7 @@ if (empty($account['access_ajax'])) die('No access on AJAX');
 
 if (isset($_GET['debug'])) {
   if (substr($cs_main['ajax_navlists'],-1) != ',') $cs_main['ajax_navlists'] .= ',';
-	$cs_main['ajax_navlists'] .= 'func_sql,func_errors,func_queries,func_parse,';
+  $cs_main['ajax_navlists'] .= 'func_sql,func_errors,func_queries,func_parse,';
 }
 
 $ajaxes = explode(',',$cs_main['ajax_navlists']);
@@ -24,24 +24,24 @@ $string = str_replace(',','',$cs_main['ajax_navlists']);
 
 
 function cs_getlog() {
-	
-	global $cs_main, $account, $cs_logs;
-	
-	$logsql = '';
-	
-	if (!empty($cs_main['developer']) OR $account['access_clansphere'] > 4) {
-	  $cs_logs['php_errors'] = str_replace('\'','\\\'',nl2br($cs_logs['php_errors']));
-	  $cs_logs['errors'] = str_replace('\'','\\\'',nl2br($cs_logs['errors']));
-	  foreach($cs_logs['sql'] AS $sql_file => $sql_queries) {
-	      $logsql .= cs_html_big(1) . str_replace('\\', '\\\\', $sql_file) . cs_html_big(0) . cs_html_br(1);
-	    $logsql .= nl2br(htmlentities($sql_queries, ENT_QUOTES, $cs_main['charset']));
-	  }
-	} else {
-	  $cs_logs['php_errors'] = '';
-	  $cs_logs['errors'] = 'Developer mode is turned off';
-	}
-	
-	return $logsql;
+  
+  global $cs_main, $account, $cs_logs;
+  
+  $logsql = '';
+  
+  if (!empty($cs_main['developer']) OR $account['access_clansphere'] > 4) {
+    $cs_logs['php_errors'] = str_replace('\'','\\\'',nl2br($cs_logs['php_errors']));
+    $cs_logs['errors'] = str_replace('\'','\\\'',nl2br($cs_logs['errors']));
+    foreach($cs_logs['sql'] AS $sql_file => $sql_queries) {
+        $logsql .= cs_html_big(1) . str_replace('\\', '\\\\', $sql_file) . cs_html_big(0) . cs_html_br(1);
+      $logsql .= nl2br(htmlentities($sql_queries, ENT_QUOTES, $cs_main['charset']));
+    }
+  } else {
+    $cs_logs['php_errors'] = '';
+    $cs_logs['errors'] = 'Developer mode is turned off';
+  }
+  
+  return $logsql;
 }
 
 
@@ -55,7 +55,7 @@ if (!empty($string)) {
   
   foreach ($ajaxes as $ajax) {
     if (empty($ajax)) continue;
-  	$name = !empty($special_names[$ajax]) ? $special_names[$ajax] : 'cs_' . $ajax;
+    $name = !empty($special_names[$ajax]) ? $special_names[$ajax] : 'cs_' . $ajax;
     if (empty($specials[$ajax])) {
       $temp .= '!33/' . $name . '!33/' . cs_filecontent('mods/' . str_replace('_','/',$ajax) . '.php');
     } else {
@@ -66,5 +66,3 @@ if (!empty($string)) {
   }
   echo $temp;
 }
-
-echo $logsql;
