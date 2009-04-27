e5709c12726b07bd417ac9b6a7f08adf90e5c213
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-04-27 11:21:57 +0000

    fixed mod_name notice

--- lang/English/fckeditor.php
+++ lang/English/fckeditor.php
@@ -2,11 +2,12 @@
 // ClanSphere 2009 - www.clansphere.net
 // $Id$
 
+$cs_lang['mod_name']  = 'FCKeditor';
+$cs_lang['mod_text']  = 'WYSIWYG Editor';
+
 // Options
 $cs_lang['options_info'] = 'Administration of module settings.';
 $cs_lang['skin'] = 'Skin';
 $cs_lang['height'] = 'Height';
 
 $cs_lang['pixel'] = 'Pixel';
-
-?>
