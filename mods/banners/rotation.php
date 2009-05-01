47814fbff5b9cad2bc793b4f93180003e2aa8237
duRiel <duRiel@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-05-01 21:03:26 +0000

    fixed #267

--- mods/banners/rotation.php
+++ mods/banners/rotation.php
@@ -1,4 +1,7 @@
 <?php
+// ClanSphere 2009 - www.clansphere.net
+// $Id$
+
 $op_banners = cs_sql_option(__FILE__,'banners');
 
 if(!empty($_GET['cat_id'])) {
@@ -11,15 +14,18 @@ else {
 $cs_banners = cs_sql_select(__FILE__,'banners','banners_id, banners_picture, banners_alt, banners_url, categories_id',$where,'rand()',0,1);
 
 if(empty($cs_banners)) {
+  
   echo '----';
-}
-else {
+  
+} else {
   $go = cs_secure($cs_banners['banners_picture']);
   $picture = cs_html_img($go,0,0," style=\"margin-bottom:4px\"",cs_secure($cs_banners['banners_alt']));
+  
   echo cs_html_link('http://' . cs_secure($cs_banners['banners_url']),$picture) . cs_html_br(1);
-  $cells = array('options_value');
-  $values = array($cs_banners['banners_id']);
-  $where_op = 'options_mod = \'banners\' AND options_name = \'last_id\'';
-  cs_sql_update(__FILE__,'options',$cells,$values,0,$where_op);    
+  
+  require_once 'mods/clansphere/func_options.php';
+  
+  $save = array('last_id' => $cs_banners['banners_id']);
+  
+  cs_optionsave('banners', $save);
 }
-?>
