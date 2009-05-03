f5239eb5cccec83a401f2f09033f9184c94a8f97
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-05-03 13:46:34 +0000

    fixed #285 - add count of subcats to cat count

--- mods/files/list.php
+++ mods/files/list.php
@@ -3,6 +3,7 @@
 // $Id$
 
 $cs_lang = cs_translate('files');
+
 require 'mods/categories/functions.php';
 
 $where = "categories_mod = 'files' AND categories_access <= '" . $account['access_files'] . "' AND categories_subid = '0'";
@@ -15,26 +16,24 @@ $data = array();
 $data['categories'] = array();
 
 for($run=0; $run < $categories_loop; $run++) {
-  
+
   $data['categories'][$run]['id'] = $categories_data[$run]['categories_id'];
   $data['categories'][$run]['name'] = cs_secure($categories_data[$run]['categories_name']);
   $data['categories'][$run]['count'] = cs_sql_count(__FILE__,'files',"categories_id = '" . $categories_data[$run]['categories_id'] . "'");
-  
   $data['categories'][$run]['text'] = '';
   $data['categories'][$run]['if']['text'] = false;
   if(!empty($categories_data[$run]['categories_text'])) {
     $data['categories'][$run]['text'] = cs_secure($categories_data[$run]['categories_text'],1);
     $data['categories'][$run]['if']['text'] = true;
   }
-  
+
   $data['categories'][$run]['picture'] = '';
   $data['categories'][$run]['if']['picture'] = false;
   if(!empty($categories_data[$run]['categories_picture'])) {
     $data['categories'][$run]['picture'] = cs_html_img('uploads/categories/' . $categories_data[$run]['categories_picture']);
     $data['categories'][$run]['if']['picture'] = true;
   }  
-    
-  
+
   $sub_where = "categories_mod = 'files' AND categories_access <= '" . $account['access_files'] . "'";
   $sub_where .= " AND categories_subid = '" . $categories_data[$run]['categories_id'] . "'";
   $sub_data = cs_sql_select(__FILE__,'categories','*',$sub_where,'categories_name',0,0);
@@ -43,17 +42,14 @@ for($run=0; $run < $categories_loop; $run++) {
   if(!empty($sub_loop)) {
     $data['categories'][$run]['if']['subcats'] = true;
     for($runb=0; $runb < $sub_loop; $runb++) {
-      $sub_content = cs_sql_count(__FILE__,'files',"categories_id = '" . $sub_data[$runb]['categories_id'] . "'");
-      
       $data['categories'][$run]['subs'][$runb]['name'] = cs_secure($sub_data[$runb]['categories_name']);
       $data['categories'][$run]['subs'][$runb]['id'] = $sub_data[$runb]['categories_id'];
-      $data['categories'][$run]['subs'][$runb]['count'] = $sub_content;
-      
-      $data['categories'][$run]['subs'][$runb]['comma'] = ($runb == $sub_loop -1) ? '' : ', ';
 
+      $sub_content = cs_sql_count(__FILE__,'files',"categories_id = '" . $sub_data[$runb]['categories_id'] . "'");
+      $data['categories'][$run]['subs'][$runb]['count'] = $sub_content;
+      $data['categories'][$run]['count'] += $sub_content;
+    }
   }
-}
 }
 
 echo cs_subtemplate(__FILE__,$data,'files','list');
-?>
--- themes/base/files/list.tpl
+++ themes/base/files/list.tpl
@@ -11,14 +11,14 @@
 <table class="forum" cellpadding="0" cellspacing="{page:cellspacing}" style="width:{page:width}">
   {loop:categories}
   <tr>
-    <td class="headb"><a href="{url:files_listcat:where={categories:id}}" >{categories:name}</a> ({categories:count}) </td>
+    <td class="headb"><a href="{url:files_listcat:where={categories:id}}">{categories:name}</a> ({categories:count}) </td>
   </tr>
   <tr>
   <td class="leftb">{categories:text}</td>
   </tr>
   {if:subcats}
   <tr>
-    <td class="leftc"> {lang:subcats}: {loop:subs}<a href="{url:files_listcat:where={subs:id}}" >{subs:name}</a> ({subs:count}){stop:subs} </td>
+    <td class="leftc">{lang:subcats}: {loop:subs}<a href="{url:files_listcat:where={subs:id}}">{subs:name}</a> ({subs:count}) {stop:subs}</td>
   </tr>
   {stop:subcats}
   {stop:categories}
