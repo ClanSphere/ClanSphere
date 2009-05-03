06924f8925c9260671b8990c7f4946f272265965
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-05-03 15:12:50 +0000

    fixes for php closing tag missing problems

--- mods/gallery/print_now.php
+++ mods/gallery/print_now.php
@@ -1,32 +1,20 @@
-<?PHP
+<?php
 // ClanSphere 2009 - www.clansphere.net
 // $Id$
 
 //$cs_lang = cs_translate('gallery');
-<SCRIPT LANGUAGE="JavaScript">
-<!--
-if (window.print) {
-window.print();
-setTimeout('self.close()',5000);
-}
-//-->
-</SCRIPT>
+echo '<SCRIPT LANGUAGE="JavaScript"><!-- if (window.print) { window.print(); setTimeout(\'self.close()\',5000); } //--></SCRIPT>';
 
-<?PHP
-if(!empty($_REQUEST['pic'])) 
-{
+if(!empty($_REQUEST['pic'])) {
+
   $pic = $_REQUEST['pic'];
   $size = $_REQUEST['size'];
   
   echo "<img src=\"image.php?pic=$pic&size=$size class=\"mediumimage\" border=\"0\" alt=\"\">";
 }
-else
-{
-  <SCRIPT LANGUAGE="JavaScript">
-  <!--
-  setTimeout('self.close()',2000);
-  //-->
-  </SCRIPT>
-<?PHP
-echo 'bye bye';
+else {
+
+  echo '<SCRIPT LANGUAGE="JavaScript"><!-- setTimeout(\'self.close()\',2000); //--></SCRIPT>';
+
+  echo 'bye bye';
 }
--- upload.php
+++ upload.php
@@ -9,27 +9,27 @@ require_once 'system/core/functions.php';
 
 cs_init($cs_main);
 
-
 session_start();
 
-  if (!empty($_SESSION)) {
+if(!empty($_SESSION)) {
 
-    if($_POST['remove']) {
-      $file = $_POST['remove']; 
-      cs_unlink('cache',$_SESSION['ajaxuploads'][$file]);
-      unset($_SESSION['ajaxuploads'][$file]);
-      echo $file;
-    } else {
-      
-      $upload_name = $_POST['upload_name'];
-      $file = $_FILES[$upload_name]['tmp_name'];
-      $new_name = 'tmp_'.time().strrchr($_FILES[$upload_name]['name'],'.');
-      cs_upload('cache', $new_name, $_FILES[$upload_name]['tmp_name'], 0);
-      if (!isset($_SESSION['ajaxuploads'])) $_SESSION['ajaxuploads'] = array();
-      $_SESSION['ajaxuploads'][$upload_name] = $new_name;
-      <script language="javascript" type="text/javascript">
-        window.top.window.upload_complete('<?php echo $upload_name; ?>','<?php echo $_FILES[$upload_name]['name']; ?>');
-      </script> 
-<?php 
-    }
+  if($_POST['remove']) {
+    $file = $_POST['remove']; 
+    cs_unlink('cache',$_SESSION['ajaxuploads'][$file]);
+    unset($_SESSION['ajaxuploads'][$file]);
+    echo $file;
   }
+  else {
+    
+    $upload_name = $_POST['upload_name'];
+    $file = $_FILES[$upload_name]['tmp_name'];
+    $new_name = 'tmp_'.time().strrchr($_FILES[$upload_name]['name'],'.');
+    cs_upload('cache', $new_name, $_FILES[$upload_name]['tmp_name'], 0);
+    if (!isset($_SESSION['ajaxuploads'])) $_SESSION['ajaxuploads'] = array();
+    $_SESSION['ajaxuploads'][$upload_name] = $new_name;
+
+    echo '<script language="javascript" type="text/javascript">';
+    echo 'window.top.window.upload_complete(\'' . $upload_name . '\',\'' . $_FILES[$upload_name]['name'] . '\');';
+    echo '</script>';
+  }
+}
