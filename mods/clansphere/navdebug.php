304b4b66e7fe35958b7d39964e31172095e9ea16
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-05-22 22:37:09 +0000

    added navdebug feature for debug mode

---
+++ mods/clansphere/navdebug.php
@@ -0,0 +1,54 @@
+<?php
+// ClanSphere 2009 - www.clansphere.net
+// $Id$
+
+$nav_array = array('----',
+  'articles/navlist',
+  'banners/navlist',
+  'banners/navright',
+  'banners/rotation',
+  'board/navlist',
+  'buddys/navlist',
+  'count/navall',
+  'count/navday',
+  'count/navmon',
+  'count/navone',
+  'count/navyes',
+  'events/navcal',
+  'events/navnext',
+  'gallery/navlist',
+  'files/navlist',
+  'gallery/navlist',
+  'news/navlist',
+  'shoutbox/navlist',
+  'users/navbirth',
+  'users/navlast',
+  'users/navonline',
+  'votes/navlist',
+  'wars/navlist',
+  'wars/navnext'
+);
+
+$cs_lang = cs_translate('clansphere');
+
+if(isset($_REQUEST['debug_navfile']))
+  $_SESSION['debug_navfile'] = (int) $_REQUEST['debug_navfile'];
+
+if(empty($_SESSION['debug_navfile'])) {
+
+  $data = array();
+
+  foreach($nav_array AS $num => $value) {
+    $data['navfiles'][$num]['value'] = $value;
+    $data['navfiles'][$num]['num'] = $num;
+  }
+
+  echo cs_subtemplate(__FILE__,$data,'clansphere','navdebug');
+}
+else {
+
+  $nav_file = $nav_array[$_SESSION['debug_navfile']];
+  $remove = cs_link($cs_lang['remove'], $cs_main['def_mod'], $cs_main['def_action'], 'debug_navfile=0');
+  echo $nav_file . ' [ ' . $remove . ' ]' . cs_html_br(2);
+  include 'mods/' . $nav_file . '.php';
+}
---
+++ themes/base/clansphere/navdebug.tpl
@@ -0,0 +1,10 @@
+<form method="post" id="clansphere_navdebug" action="{page:self}">
+<fieldset style="border: 0; padding: 0">
+  <select name="debug_navfile">
+    {loop:navfiles}
+    <option value="{navfiles:num}">{navfiles:value}</option>
+    {stop:navfiles}
+  </select>
+  <input type="submit" name="submit" value="{lang:show}" />
+</fieldset>
+</form>
