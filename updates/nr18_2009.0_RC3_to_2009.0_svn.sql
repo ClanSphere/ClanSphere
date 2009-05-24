5835e69a937c517a8bcf283aae3b88442d76ebeb
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-05-24 18:14:33 +0000

    update to sql update files

---
+++ updates/nr18_2009.0_RC3_to_2009.0_svn.sql
@@ -0,0 +1,3 @@
+UPDATE {pre}_options SET options_value = '2009.0_svn' WHERE options_mod = 'clansphere' AND options_name = 'version_name';
+UPDATE {pre}_options SET options_value = '2009-05-24' WHERE options_mod = 'clansphere' AND options_name = 'version_date';
+UPDATE {pre}_options SET options_value = '41' WHERE options_mod = 'clansphere' AND options_name = 'version_id';
