0788aad8a025f8ba9f7da5c835dbe98d7e9d584b
duRiel <duRiel@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-05-27 20:15:12 +0000

    cups: added a first match tree. since the match generation is still by random, this is still experimental.

---
+++ mods/cups/tree.php
@@ -0,0 +1,6 @@
+<?php
+// ClanSphere 2009 - www.clansphere.net
+// $Id: view.php 3009 2009-05-03 14:57:11Z hajo $
+
+
+echo '<table class="forum"><tr><td><img src="' . $cs_main['php_self']['dirname'] . 'mods/cups/treeimg.php?id=' . $_GET['id'] . '" alt="Tree" /></td></tr></table>';
