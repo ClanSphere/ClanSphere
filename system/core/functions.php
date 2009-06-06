96cd40743d73e14607186fa01e10ff02ba1ee3ce
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-06-06 12:11:56 +0000

    added gzip compression (test)

--- system/core/functions.php
+++ system/core/functions.php
@@ -153,8 +153,12 @@ function cs_init($predefined) {
     cs_tasks('system/runstartup'); # load startup files
   }
 
-  if(!empty($predefined['init_tpl']))
-    echo cs_template($cs_micro, $predefined['tpl_file']);
+  if(!empty($predefined['init_tpl'])) {
+    $content = cs_template($cs_micro, $predefined['tpl_file']);
+    if(extension_loaded('zlib'))
+      ob_start('ob_gzhandler');
+    echo $content;
+  }
 }
 
 // Array walking with refereced altering
