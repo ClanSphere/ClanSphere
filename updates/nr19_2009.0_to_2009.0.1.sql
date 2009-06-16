1f1ecb862fc802bacda8591c41959a431cdecb7f
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-06-16 18:28:55 +0000

    fixed #420 - users_view contained integer values

--- updates/nr19_2009.0_to_2009.0.1.sql
+++ updates/nr19_2009.0_to_2009.0.1.sql
@@ -1,3 +1,5 @@
 UPDATE {pre}_options SET options_value = '2009.0.1 SVN' WHERE options_mod = 'clansphere' AND options_name = 'version_name';
 UPDATE {pre}_options SET options_value = '2009-06-14' WHERE options_mod = 'clansphere' AND options_name = 'version_date';
 UPDATE {pre}_options SET options_value = 43 WHERE options_mod = 'clansphere' AND options_name = 'version_id';
+
+UPDATE {pre}_users SET users_view = '' WHERE users_view NOT IN ('float', 'list');
