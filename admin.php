575dc469de42fdea25acd4fe370769ec5762ecc3
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-06-25 23:06:20 +0000

    admin.php now starts at clansphere/admin page by default

--- admin.php
+++ admin.php
@@ -3,7 +3,7 @@
 // $Id$
 
 # Overwrite global settings by using the following array
-$cs_main = array('init_sql' => true, 'init_tpl' => true, 'tpl_file' => 'admin.htm');
+$cs_main = array('init_sql' => true, 'init_tpl' => true, 'tpl_file' => 'admin.htm', 'def_mod' => 'clansphere', 'def_action' => 'admin');
 
 require_once 'system/core/functions.php';
 
