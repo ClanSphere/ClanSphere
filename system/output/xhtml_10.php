4e23227db93d64b4b431d524d0f64340906b5f7b
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-06-28 19:12:37 +0000

    shortened cs_html_msnmail function

--- system/output/xhtml_10.php
+++ system/output/xhtml_10.php
@@ -39,25 +39,20 @@ function cs_html_img($url, $height = 0, $width = 0, $more = 0, $alt = '')
   return $var . "alt=\"" . $alt . "\" />";
 }
 
-function cs_html_mail($mail, $link = '')
+function cs_html_mail($mail, $link = '', $service = 'mailto:')
 {
 	global $cs_main;
   $email = explode("@", $mail);
   $domain = empty($email[1]) ? array(0,1) : explode(".", $email[1]);
   $link = empty($link) ? $email[0] . ' (at) ' . $domain[0] . ' (dot) ' . $domain[1] : $link;
   $str = base64_encode($mail);
-  
-  return '<a href="#" onclick="cs_ajax_request(\''.$cs_main['php_self']['dirname'].'mods/ajax/mail.php?mail=' . $str . '\',function(http_request){window.location=\'mailto:\'+http_request.responseText;})">' . $link . '</a>';
+
+  return '<a href="#" onclick="cs_ajax_request(\''.$cs_main['php_self']['dirname'].'mods/ajax/mail.php?mail=' . $str
+       . '\',function(http_request){window.location=\'' . $service . '\'+http_request.responseText;})">' . $link . '</a>';
 }
 function cs_html_msnmail($mail, $link = '')
 {
-  global $cs_main;
-	$email = explode("@", $mail);
-  $domain = empty($email[1]) ? array(0,1) : explode(".", $email[1]);
-  $link = empty($link) ? $email[0] . ' (at) ' . $domain[0] . ' (dot) ' . $domain[1] : $link;
-  $str = base64_encode($mail);
-  
-  return '<a href="#" onclick="cs_ajax_request(\''.$cs_main['php_self']['dirname'].'mods/ajax/mail.php?mail=' . $str . '\',function(http_request){window.location=\'http://members.msn.com/\'+http_request.responseText;})">' . $link . '</a>';
+  return cs_html_mail($mail, $link, 'http://members.msn.com/');
 }
 
 function cs_html_link($url, $link, $use_target = 1, $class = 0, $title = 0, $more = 0)
