b37a1a1f1063118ba3fcfafc1bb64213e0f4c7bb
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-07-04 05:51:32 +0000

    fixed users navlogin to work with ajax again

--- system/core/templates.php
+++ system/core/templates.php
@@ -314,7 +314,7 @@ function cs_template($cs_micro, $tpl_file = 'index.htm')
      }
     if (!empty($ajaxes)) {
       if (strpos($cs_temp_get,'id="cs_users_navlogin"') === false)
-        $cs_temp_get = str_replace(array('{users:navlogin}','{users:navlogin2}','{users:navlogin3}','{users:navlogin4}'),array('<div id="cs_users_navlogin">{users:navlogin}</div>','<div id="cs_users_navlogin2">{users:navlogin2}</div>','<div id="cs_users_navlogin3">{users:navlogin3}</div>','<div id="cs_users_navlogin4">{users:navlogin4}</div>'),$cs_temp_get);
+        $cs_temp_get = preg_replace("={users:navlogin(.*?)}=i", "<div id=\"cs_users_navlogin\">{users:navlogin\\1}</div>", $cs_temp_get);
       $spans = array('count_navday','count_navone','count_navall','count_navmon','func_parse','func_queries');
       foreach ($ajaxes as $ajax) {
         $placeholder = '{'.str_replace('_',':',$ajax).'}';
