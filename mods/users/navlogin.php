805be75035059bb75b8878b4a672b7157c553968
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-07-04 13:43:00 +0000

    vertical navlogin is now corrected to horizontal

--- mods/users/navlogin.php
+++ mods/users/navlogin.php
@@ -4,7 +4,7 @@
 
 $cs_lang = cs_translate('users');
 
-$styles_array = array('icons', 'pictures', 'vertical');
+$styles_array = array('horizontal', 'icons', 'pictures');
 $style = (!empty($_GET['style']) AND in_array($_GET['style'], $styles_array)) ? '_' . $_GET['style'] : '';
 
 $uri = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
