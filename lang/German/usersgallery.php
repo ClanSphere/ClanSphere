38190d292a68da4b017c6b55c54f1c82cf9381a5
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-07-30 17:04:45 +0000

    fixed translation changes of rev 3469

--- lang/German/usersgallery.php
+++ lang/German/usersgallery.php
@@ -5,5 +5,6 @@
 $cs_lang['mod_name'] = 'Benutzer-Gallerie';
 $cs_lang['modtext'] = 'Multimedia Benutzer-Galerie nutzen und verwalten.';
 
-$cs_lang['not_own'] = 'Das Bild gehört einem anderen Benutzer';
+$cs_lang['not_own'] = 'Das Bild geh&ouml;rt einem anderen Benutzer';
+
 # further translation in gallery.php
