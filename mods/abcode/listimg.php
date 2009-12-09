52e18282135c11b97c38bd7cd18e67cae8e43e87
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2009-12-09 16:14:30 +0000

    fixed php notice when no textarea name was given

--- mods/abcode/listimg.php
+++ mods/abcode/listimg.php
@@ -4,7 +4,8 @@
 
 $cs_lang = cs_translate('abcode');
 
-$name = preg_replace('/[^\w]/s','',$_GET['name']);
+$name = empty($_GET['name']) ? '' : $_GET['name'];
+$name = preg_replace('/[^\w]/s','',$name);
 
 $where = "abcode_func = 'img'";
 $cs_abcode = cs_sql_select(__FILE__,'abcode','*',$where,'abcode_pattern ASC',0,0);
