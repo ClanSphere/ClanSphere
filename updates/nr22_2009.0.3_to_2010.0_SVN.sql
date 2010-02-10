a1762094e3001088f9e80360bac428e7d9991c21
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-02-10 16:26:53 +0000

    typo in sql update command

--- updates/nr22_2009.0.3_to_2010.0_SVN.sql
+++ updates/nr22_2009.0.3_to_2010.0_SVN.sql
@@ -2,7 +2,7 @@ UPDATE {pre}_options SET options_value = '2010.0_SVN' WHERE options_mod = 'clans
 UPDATE {pre}_options SET options_value = '2010-01-31' WHERE options_mod = 'clansphere' AND options_name = 'version_date';
 UPDATE {pre}_options SET options_value = 49 WHERE options_mod = 'clansphere' AND options_name = 'version_id';
 
-INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUE ('clansphere','maintenance_access','3');
+INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('clansphere','maintenance_access','3');
 
 UPDATE {pre}_options SET options_mod = 'ckeditor' WHERE options_mod = 'fckeditor';
 UPDATE {pre}_options SET options_value = 'kama' WHERE options_mod = 'ckeditor' AND options_name = 'skin';
