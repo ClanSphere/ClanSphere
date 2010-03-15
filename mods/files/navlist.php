2942267a2411bce6a93ac745e43ea07a5310bcf0
Spongebob <Spongebob@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-03-15 17:42:38 +0000

    add date to {files:navlist} - close #395

--- mods/files/navlist.php
+++ mods/files/navlist.php
@@ -5,13 +5,17 @@
 $cs_lang = cs_translate('files');
 
 $max = 4;
-
+$wordmax = 13;
 $data = array();
 
-$data['files'] = cs_sql_select(__FILE__,'files','files_name, files_id',0,'files_time DESC',0,$max);
+$data['files'] = cs_sql_select(__FILE__,'files','files_name, files_time, files_id',0,'files_time DESC',0,$max);
 
 if (!empty($data['files'])) {
-  
+  $files_loop = count($data['files']);
+  for($run=0; $run<$files_loop; $run++) {
+    $data['files'][$run]['date'] = cs_date('unix',$data['files'][$run]['files_time']);
+    $data['files'][$run]['files_name'] = strlen($data['files'][$run]['files_name']) > $wordmax ? substr($data['files'][$run]['files_name'], 0, $wordmax).'..' : $data['files'][$run]['files_name'];
+  }
   echo cs_subtemplate(__FILE__,$data,'files','navlist');
   
 } else {
--- themes/base/files/navlist.tpl
+++ themes/base/files/navlist.tpl
@@ -1,3 +1,3 @@
 {loop:files}
-<a href="{url:files_view:id={files:files_id}}">{files:files_name}</a>
+{files:date} <a href="{url:files_view:id={files:files_id}}">{files:files_name}</a>
 <br />{stop:files}
