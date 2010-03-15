8f7e254c395a866afb9ef7159a91e08a95431e36
Spongebob <Spongebob@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-03-15 17:13:09 +0000

    cache {board:navtop}, add {board:navtop2} - close #718

---
+++ mods/board/navtop2.php
@@ -0,0 +1,17 @@
+<?php
+// ClanSphere 2009 - www.clansphere.net
+// $Id: $
+
+$max = 5;
+$data = array();
+$time = (int) date('mdHi');
+$data = cs_cache_load('brd_navtop2');
+if (empty($data) OR $data['time'] < $time-100) {
+  $data['time'] = $time;
+  $tables  = "threads thr LEFT JOIN {pre}_users usr ON thr.users_id = usr.users_id AND usr.users_active = '1' GROUP BY usr.users_id, usr.users_nick";
+  $cells  = 'usr.users_id AS users_id, usr.users_nick AS users_nick, ';
+  $cells .= 'COUNT(DISTINCT thr.threads_id) AS threads';
+  $data['top2'] = cs_sql_select(__FILE__,$tables,$cells,0,'threads DESC, usr.users_nick',0,$max);
+  cs_cache_save('brd_navtop2', $data);
+}
+echo cs_subtemplate(__FILE__,$data, 'board', 'navtop2');
