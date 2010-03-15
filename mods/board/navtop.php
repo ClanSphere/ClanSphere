b9e5fe2c95c1f2b91817d1bd2a0da74581bee26b
Spongebob <Spongebob@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-03-15 17:12:43 +0000

    cache {board:navtop}, add {board:navtop2} - close #718

--- mods/board/navtop.php
+++ mods/board/navtop.php
@@ -1,14 +1,18 @@
 <?php
 // ClanSphere 2009 - www.clansphere.net
-// $Id:  $
+// $Id: $
 
 $max = 5;
 $data = array();
-
-$tables  = "comments cms LEFT JOIN {pre}_users usr ON cms.users_id = usr.users_id AND usr.users_active = '1' ";
-$tables .= 'LEFT JOIN {pre}_threads thr ON thr.users_id = usr.users_id GROUP BY usr.users_id, usr.users_nick';
-$cells  = 'usr.users_id AS users_id, usr.users_nick AS users_nick, ';
-$cells .= 'COUNT(DISTINCT cms.comments_id) + COUNT(DISTINCT thr.threads_id) AS comments';
-$data['top'] = cs_sql_select(__FILE__,$tables,$cells,0,'comments DESC, usr.users_nick',0,$max);
-
+$time = (int) date('mdHi');
+$data = cs_cache_load('brd_navtop');
+if (empty($data) OR $data['time'] < $time-100) {
+  $data['time'] = $time;
+  $tables  = "comments cms LEFT JOIN {pre}_users usr ON cms.users_id = usr.users_id AND usr.users_active = '1' ";
+  $tables .= 'LEFT JOIN {pre}_threads thr ON thr.users_id = usr.users_id GROUP BY usr.users_id, usr.users_nick';
+  $cells  = 'usr.users_id AS users_id, usr.users_nick AS users_nick, ';
+  $cells .= 'COUNT(DISTINCT cms.comments_id) + COUNT(DISTINCT thr.threads_id) AS comments';
+  $data['top'] = cs_sql_select(__FILE__,$tables,$cells,0,'comments DESC, usr.users_nick',0,$max);
+  cs_cache_save('brd_navtop', $data);
+}
 echo cs_subtemplate(__FILE__,$data, 'board', 'navtop');
