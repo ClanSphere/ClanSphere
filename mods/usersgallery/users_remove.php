83a9ae16037dbd3e2d854bf62684aef3759c1f19
Spongebob <Spongebob@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-03-17 13:08:30 +0000

    bug in user-gallery - fixed #739

--- mods/usersgallery/users_remove.php
+++ mods/usersgallery/users_remove.php
@@ -12,35 +12,32 @@ if (!empty($cs_post['id']))  $gallery_id = $cs_post['id'];
 $cs_gallery = cs_sql_select(__FILE__,'usersgallery','*',"usersgallery_id = '" . $gallery_id . "'");
 $pic = cs_secure($cs_gallery['usersgallery_name']);
 
-if($pic['users_id'] != $account['users_id'] OR $account['access_usersgallery'] < 4)
- cs_redirect($cs_lang['del_false'],'usersgallery','center');
+if ($cs_gallery['users_id'] != $account['users_id'] OR $account['access_usersgallery'] < 5)
+  cs_redirect($cs_lang['del_false'],'usersgallery','center');
 
+if (isset($_POST['agree'])) {
+  cs_unlink('usersgallery', $pic, 'pics');
+  cs_unlink('usersgallery', 'Thumb_' . $pic, 'thumbs');
 
-if(isset($_POST['agree'])) {
-
- cs_unlink('usersgallery', $pic, 'pics');
- cs_unlink('usersgallery', 'Thumb_' . $pic, 'thumbs');
-
- cs_sql_delete(__FILE__,'usersgallery',$gallery_id);
+  cs_sql_delete(__FILE__,'usersgallery',$gallery_id);
   $query = "DELETE FROM {pre}_voted WHERE voted_mod='usersgallery' AND ";
   $query .= "voted_fid='" . $gallery_id . "'";
- cs_sql_query(__FILE__,$query);
+  cs_sql_query(__FILE__,$query);
   $query = "DELETE FROM {pre}_comments WHERE comments_mod='usersgallery' AND ";
   $query .= "comments_fid='" . $gallery_id . "'";
- cs_sql_query(__FILE__,$query);
+  cs_sql_query(__FILE__,$query);
 
- cs_redirect($cs_lang['del_true'],'usersgallery','center');
+  cs_redirect($cs_lang['del_true'],'usersgallery','center');
 }
 
-if(isset($_POST['cancel']))
- cs_redirect($cs_lang['del_false'],'usersgallery','center');
-
+if (isset($_POST['cancel']))
+  cs_redirect($cs_lang['del_false'],'usersgallery','center');
 else {
   $id = cs_secure($cs_gallery['usersgallery_id']);
   $data['this']['img'] = cs_html_img("mods/gallery/image.php?usersthumb=" . $id);
   $data['head']['body'] = sprintf($cs_lang['del_pic_rly'],$pic);
   $data['hidden']['id'] = $gallery_id;
 
- echo cs_subtemplate(__FILE__,$data,'usersgallery','users_remove');
+  echo cs_subtemplate(__FILE__,$data,'usersgallery','users_remove');
 }
   
