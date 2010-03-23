20743bd52a5e8a11c9abf85bb9ba39c879b18975
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-03-23 15:06:09 +0000

    commented out mail content encoding - needs some rework - see thread id 12994 for more details

--- system/core/tools.php
+++ system/core/tools.php
@@ -349,7 +349,7 @@ function cs_mail($email,$title,$message,$from = 0,$type = 0) {
   @ini_set('sendmail_from', $from);
 
   $subject = '=?' . $cs_main['charset'] . '?B?' . base64_encode($subject) . '?=';
-  $message = '=?' . $cs_main['charset'] . '?B?' . base64_encode($message) . '?=';
+#  $message = '=?' . $cs_main['charset'] . '?B?' . base64_encode($message) . '?=';
 
   $result = mail($email,$subject,$message,$headers) ? TRUE : FALSE;
   return $result;
