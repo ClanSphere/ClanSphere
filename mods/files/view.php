d035b898d162a77b3abbd80fe8ca42b9df667477
Spongebob <Spongebob@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-03-29 20:05:30 +0000

    some fixes

--- mods/files/view.php
+++ mods/files/view.php
@@ -12,14 +12,22 @@ $file_id = !empty($_GET['where']) ? (int) $_GET['where'] : (int) $_GET['id'];
 
 $from = 'files fls INNER JOIN {pre}_users usr ON fls.users_id = usr.users_id';
 $from .= ' INNER JOIN {pre}_categories cat ON fls.categories_id = cat.categories_id';
-$select = 'fls.files_name AS files_name, fls.users_id AS users_id, usr.users_nick'; 
+$select = 'fls.files_name AS files_name, fls.users_id AS users_id, usr.users_nick';
 $select .= ' AS users_nick, usr.users_active AS users_active, fls.files_time AS files_time, fls.files_id AS files_id';
 $select .= ', fls.files_mirror AS files_mirror ,cat.categories_name AS categories_name';
-$select .= ', cat.categories_id AS categories_id, fls.files_count AS files_count';  
+$select .= ', cat.categories_id AS categories_id, fls.files_count AS files_count';
 $select .= ', fls.files_description AS files_description, fls.files_close AS files_close';
 $select .= ', fls.files_vote AS files_vote, fls.files_size AS files_size, fls.files_version AS files_version, fls.files_previews AS files_previews';
 $where = 'files_id = ' . $file_id;
 $cs_file = cs_sql_select(__FILE__,$from,$select,$where);
+
+if(!empty($_POST['brokenlink'])) {
+  require_once('mods/notifymods/functions.php');
+  notifymods_mail('files', $account['users_id'], $cs_file['files_name']);
+  $data['if']['brokenlink'] = FALSE;
+}
+else
+  $data['if']['brokenlink'] = TRUE;
 
 $from = 'voted';
 $select = 'users_id, voted_answer';
@@ -31,25 +39,6 @@ if(!empty($_POST['voted_answer']))
   $voted_answer = $_POST['voted_answer'];
 
 $users_id = !empty($account['users_id']) ? $account['users_id'] : 0;
-$data['category']['name'] = $cs_file['categories_name'];
-$data['category']['id'] = $cs_file['categories_id'];
-$data['file']['id'] = $cs_file['files_id'];
-$data['file']['name'] = cs_secure($cs_file['files_name']);
-$data['file']['version'] =  cs_secure($cs_file['files_version']);
-$data['file']['size'] =  cs_filesize($cs_file['files_size']);
-$data['file']['user'] = cs_user($cs_file['users_id'],$cs_file['users_nick'], $cs_file['users_active']);
-$data['file']['date'] = cs_date('unix',$cs_file['files_time'],1);
-$data['file']['count'] = cs_secure($cs_file['files_count']); 
-$data['if']['vote'] = false;
-$data['if']['unvoted'] = false;
-
-if(!empty($_POST['brokenlink'])) {
-  require_once('mods/notifymods/functions.php');
-  notifymods_mail('files', $account['users_id'], $cs_file['files_name']);
-  $data['if']['brokenlink'] = FALSE;
-}
-else
-  $data['if']['brokenlink'] = TRUE;
 
 $check_user_voted = 0;
 for ($run = 0; $run < $voted_loop; $run++) {
@@ -59,7 +48,7 @@ for ($run = 0; $run < $voted_loop; $run++) {
   }
 }
 
-if(empty($check_user_voted) AND isset($_POST['submit']) AND empty($_POST['brokenlink'])) {        
+if(empty($check_user_voted) AND isset($_POST['submit']) AND empty($_POST['brokenlink'])) {
   $time = cs_time();
   $voted_ip =$_SERVER['REMOTE_ADDR'];
   $votes_cells = array('voted_fid','users_id','voted_time','voted_answer','voted_ip','voted_mod');
@@ -68,10 +57,25 @@ if(empty($check_user_voted) AND isset($_POST['submit']) AND empty($_POST['broken
   header('location:' . $_SERVER['PHP_SELF'] . '?mod=files&action=view&where=' .$file_id);
 }
 
+
+$data['category']['name'] = $cs_file['categories_name'];
+$data['category']['id'] = $cs_file['categories_id'];
+$data['file']['id'] = $cs_file['files_id'];
+$data['file']['name'] = cs_secure($cs_file['files_name']);
+$data['file']['version'] =  cs_secure($cs_file['files_version']);
+$data['file']['size'] =  cs_filesize($cs_file['files_size']);
+$data['file']['user'] = cs_user($cs_file['users_id'],$cs_file['users_nick'], $cs_file['users_active']);
+$data['file']['date'] = cs_date('unix',$cs_file['files_time'],1);
+$data['file']['count'] = cs_secure($cs_file['files_count']); 
+
+$data['if']['vote'] = false;
+
+$data['if']['unvoted'] = false;
+
 if(!empty($cs_file['files_vote'])) {
   $data['if']['vote'] = true;
-  $data['votes'] = array();
-  if(empty($check_user_voted)) {  
+  if(empty($check_user_voted)) { 
+    $data['votes'] = array();
     $data['if']['unvoted'] = true;
     for($l = 1;$l < 7;$l++) {
       $data['votes'][$l-1]['name'] = $l . ' - ' . $cs_lang['vote_' . $l];
@@ -102,7 +106,7 @@ if(!empty($cs_file['files_previews'])) {
   $data['if']['preview'] = true;
   $data['previews'] = array();
   
-  $files_pics = explode("\n",$cs_file['files_previews']);  
+  $files_pics = explode("\n",$cs_file['files_previews']);
   $count = 1;
   foreach($files_pics AS $pic) {
     $data['previews'][$count-1]['image'] = cs_html_img('uploads/files/thumb-' . $pic);
@@ -116,14 +120,14 @@ $files_mirror = $cs_file['files_mirror'];
 $temp = explode("-----", $files_mirror);
 $temp_loop = count($temp);
 
-if (isset($_REQUEST['target'])) {       
+if (isset($_REQUEST['target'])) {
   $files_cells = array('files_count');
   $files_save = array(++$cs_file['files_count']);
   cs_sql_update(__FILE__,'files',$files_cells,$files_save,$file_id);
   $temp_a = explode("\n", $temp[$_REQUEST['target']]);
   $select_mirrow = $temp_a['1'];
   header("location:".$select_mirrow."");
-}
+} 
 
 require_once 'mods/clansphere/filetype.php';
 
@@ -132,7 +136,7 @@ for ($run = 1; $run < $temp_loop; $run++) {
   if($account['access_files'] >= $temp_a['4']) {
     $data['mirrors'][$run-1]['name'] = empty($temp_a['2']) ? $temp_a['1'] : $temp_a['2'];
     $data['mirrors'][$run-1]['id'] = $run;
-    $data['mirrors'][$run-1]['filetype_image'] = cs_filetype($temp_a['3']);	
+	  $data['mirrors'][$run-1]['filetype_image'] = cs_filetype($temp_a['3']);	
     $data['mirrors'][$run-1]['filetype_name'] = $temp_a['3'];
   }
 }
