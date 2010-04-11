59db85a6fa2cdcd7b7a1653f1016f867bffaddea
Spongebob <Spongebob@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-04-11 22:50:55 +0000

    fix lang/German/usersgallery.php

--- lang/German/usersgallery.php
+++ lang/German/usersgallery.php
@@ -2,7 +2,7 @@
 // ClanSphere 2009 - www.clansphere.net 
 // $Id$
 
-$cs_lang['mod_name'] = 'Benutzer-Gallerie';
+$cs_lang['mod_name'] = 'Benutzer-Galerie';
 $cs_lang['modtext'] = 'Multimedia Benutzer-Galerie nutzen und verwalten.';
 
 $cs_lang['not_own'] = 'Das Bild geh&ouml;rt einem anderen Benutzer';
