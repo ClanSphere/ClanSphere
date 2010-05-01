0c7ad8cf94e7a6398744b219e56420ebe3f56937
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-05-01 19:52:11 +0000

    updated trunk due to 2009.0.3.1 release

--- updates/nr23_2009.0.3.1_to_2010.0_SVN.sql
+++ updates/nr23_2009.0.3.1_to_2010.0_SVN.sql
@@ -1,6 +1,6 @@
 UPDATE {pre}_options SET options_value = '2010.0_SVN' WHERE options_mod = 'clansphere' AND options_name = 'version_name';
-UPDATE {pre}_options SET options_value = '2010-01-31' WHERE options_mod = 'clansphere' AND options_name = 'version_date';
-UPDATE {pre}_options SET options_value = 49 WHERE options_mod = 'clansphere' AND options_name = 'version_id';
+UPDATE {pre}_options SET options_value = '2010-05-02' WHERE options_mod = 'clansphere' AND options_name = 'version_date';
+UPDATE {pre}_options SET options_value = 51 WHERE options_mod = 'clansphere' AND options_name = 'version_id';
 
 INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('clansphere','maintenance_access','3');
 
