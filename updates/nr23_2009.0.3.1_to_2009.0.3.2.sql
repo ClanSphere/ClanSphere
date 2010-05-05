680bbdbca78de2c787b7037a6e2269cd79c58982
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-05-05 00:56:17 +0000

    trunk changes due to 2009.0.3.2

---
+++ updates/nr23_2009.0.3.1_to_2009.0.3.2.sql
@@ -0,0 +1,3 @@
+UPDATE {pre}_options SET options_value = '2009.0.3.2' WHERE options_mod = 'clansphere' AND options_name = 'version_name';
+UPDATE {pre}_options SET options_value = '2010-05-05' WHERE options_mod = 'clansphere' AND options_name = 'version_date';
+UPDATE {pre}_options SET options_value = 52 WHERE options_mod = 'clansphere' AND options_name = 'version_id';
