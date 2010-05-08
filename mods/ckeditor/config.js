82d15012830baf22f104a4b688185f72eb0144c2
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-05-08 13:59:53 +0000

    commented out ckeditor quick upload as long as it doesn't work

--- mods/ckeditor/config.js
+++ mods/ckeditor/config.js
@@ -19,7 +19,10 @@ CKEDITOR.editorConfig = function( config )
   config.filebrowserBrowseUrl      = CKEDITOR.basePath + 'filemanager/browser/default/browser.html?Connector=' + encodeURIComponent( _connector );
   config.filebrowserImageBrowseUrl = CKEDITOR.basePath + 'filemanager/browser/default/browser.html?Type=Image&Connector=' + encodeURIComponent( _connector );
   config.filebrowserFlashBrowseUrl = CKEDITOR.basePath + 'filemanager/browser/default/browser.html?Type=Flash&Connector=' + encodeURIComponent( _connector );
+
+  /* TODO: Make the quick upload work
   config.filebrowserUploadUrl      = CKEDITOR.basePath + 'filemanager/connectors/php/upload.php';
   config.filebrowserImageUploadUrl = CKEDITOR.basePath + 'filemanager/connectors/php/upload.php?Type=Image';
   config.filebrowserFlashUploadUrl = CKEDITOR.basePath + 'filemanager/connectors/php/upload.php?Type=Flash';
+  */
 };
