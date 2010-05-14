1293428a558f6fbffe6d750bce760b7b95e6deed
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-05-14 01:25:07 +0000

    tiny changes to info files

--- mods/lightbox/startup.php
+++ mods/lightbox/startup.php
@@ -2,13 +2,13 @@
 // ClanSphere 2009 - www.clansphere.net
 // $Id$
 
-// Load scripts for lightbox if needed
 global $cs_main, $account;
 
 $gallery = cs_sql_option(__FILE__, 'gallery');
 
 if($gallery['lightbox'] == 1) {
 
+# Slimbox requires jQuery - loaded in cs_template() function
   cs_scriptload('lightbox', 'javascript', 'js/slimbox2.js');
   cs_scriptload('lightbox', 'stylesheet', 'css/slimbox2.css');
 }
