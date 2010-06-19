1533afd4f9cd17cf9b47b31110441ba75006ddca
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-06-19 19:11:05 +0000

    ################################################# 2010.0 beta 1 RELEASE

--- install.php
+++ install.php
@@ -18,7 +18,7 @@ $cs_main = array( 'charset'         => $charset,
                   'public'          => 1,
                   'img_path'        => 'crystal_project',
                   'img_ext'         => 'png',
-                  'version_name'    => '2010.0_DEV',
+                  'version_name'    => '2010.0 beta 1',
                   'version_date'    => '2010-06-19');
 
 require_once 'system/core/functions.php';
--- updates/nr24_2009.0.3.1_to_2010.0_DEV.sql
+++ updates/nr24_2009.0.3.1_to_2010.0_DEV.sql
@@ -1,6 +1,6 @@
-UPDATE {pre}_options SET options_value = '2010.0_DEV' WHERE options_mod = 'clansphere' AND options_name = 'version_name';
+UPDATE {pre}_options SET options_value = '2010.0 beta 1' WHERE options_mod = 'clansphere' AND options_name = 'version_name';
 UPDATE {pre}_options SET options_value = '2010-06-19' WHERE options_mod = 'clansphere' AND options_name = 'version_date';
-UPDATE {pre}_options SET options_value = 53 WHERE options_mod = 'clansphere' AND options_name = 'version_id';
+UPDATE {pre}_options SET options_value = 54 WHERE options_mod = 'clansphere' AND options_name = 'version_id';
 
 CREATE TABLE {pre}_notifymods (
   notifymods_id {serial},
