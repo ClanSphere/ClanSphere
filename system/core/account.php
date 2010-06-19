98f31098b15b271bd2fa5569b140d136e04af9b8
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-06-19 02:12:09 +0000

    updated login cookie handling and generation - not finished

--- system/core/account.php
+++ system/core/account.php
@@ -2,6 +2,25 @@
 // ClanSphere 2010 - www.clansphere.net
 // $Id$
 
+function cs_login_cookies($userid = 0) {
+
+  global $cs_main;
+  $lifetime = empty($userid) ? 1 : $cs_main['cookie']['lifetime'];
+  $thistime = empty($userid) ? '' : cs_time();
+  # TODO: Generate cookie hash
+  $thishash = empty($userid) ? '' : 'to_be_done';
+
+  setcookie('cs_userid', $userid, $lifetime, $cs_main['cookie']['path'], $cs_main['cookie']['domain']);
+  setcookie('cs_cookietime', $thistime, $lifetime, $cs_main['cookie']['path'], $cs_main['cookie']['domain']);
+  setcookie('cs_cookiehash', $thishash, $lifetime, $cs_main['cookie']['path'], $cs_main['cookie']['domain']);
+
+  if(!empty($userid)) {
+    $cells = array('users_cookietime', 'users_cookiehash');
+    $content = array($thistime, $thishash);
+    cs_sql_update(__FILE__,'users',$cells,$content,$userid);
+  }
+}
+
 global $_COOKIE, $_POST, $cs_lang, $cs_main, $login;
 
 $user_agent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
@@ -14,8 +33,8 @@ if(version_compare(phpversion(),'5.2.0','>'))
   session_set_cookie_params(0,$cs_main['cookie']['path'],$cs_main['cookie']['domain'],FALSE,TRUE);
 else
   session_set_cookie_params(0,$cs_main['cookie']['path'],$cs_main['cookie']['domain']);
-session_start();
 
+session_start();
 
 # xsrf protection
 if($cs_main['xsrf_protection']===TRUE && !empty($_POST)) {
@@ -42,41 +61,43 @@ if(empty($_SESSION['users_id'])) {
     }
     $login_where = "users_nick = '" . cs_sql_escape($login['nick']) . "'";
   }
-  elseif(isset($_COOKIE['cs_userid']) AND isset($_COOKIE['cs_securepw'])) {
+  elseif(isset($_COOKIE['cs_userid']) AND isset($_COOKIE['cs_cookietime']) AND isset($_COOKIE['cs_cookiehash'])) {
     $login['method'] = 'cookie';
-    $login['userid'] = (int)$_COOKIE['cs_userid'];
-    $login['securepw'] = $_COOKIE['cs_securepw'];
+    $login['userid'] = (int) $_COOKIE['cs_userid'];
+    $login['cookietime'] = (int) $_COOKIE['cs_cookietime'];
+    $login['cookiehash'] = $_COOKIE['cs_cookiehash'];
     $login_where = "users_id = '" . $login['userid'] . "'";
   }
 
   if(isset($login['method'])) {
-    $login_db = cs_sql_select(__FILE__,'users','users_id, users_pwd, users_active, users_ajax',$login_where);
-    if(!empty($login_db['users_pwd']) AND $login_db['users_pwd'] == $login['securepw']) { 
-      if(empty($login_db['users_active']) || !empty($login_db['users_delete'])) {
+    // $login_sc = 'users','users_id, users_pwd, users_active, users_ajax, users_cookietime, users_cookiehash';
+    $login_db = cs_sql_select(__FILE__,'users','*',$login_where);
+
+    if(!empty($login_db['users_pwd']) AND ($login['method'] == 'cookie' OR $login_db['users_pwd'] == $login['securepw'])) {
+
+      if(empty($login_db['users_active']) || !empty($login_db['users_delete']))
         $login['error'] = 'closed'; 
-      }
+      elseif($login['method'] == 'cookie' AND $login_db['users_cookietime'] > $login['cookietime'])
+        $login['error'] = 'user_notfound';
+      elseif($login['method'] == 'cookie' AND $login_db['users_cookiehash'] != $login['cookiehash'])
+        $login['error'] = 'wrong_pwd';
       else {
         $login['mode'] = TRUE;
 
         $_SESSION['users_id'] = $login_db['users_id'];
         $_SESSION['users_ip'] = cs_getip();
         $_SESSION['users_agent'] = $user_agent;
-        $_SESSION['users_pwd'] = $login['securepw'];
-        //if (!empty($login_db['users_ajax']) && !empty($cs_main['ajax']))
-        //  empty($cs_main['mod_rewrite']) ? header('Location: index.php') : header('Location: ../../index.php');
+        $_SESSION['users_pwd'] = $login_db['users_pwd'];
       }
     }
-    elseif(!empty($login_db['users_id'])) { 
+    elseif(!empty($login_db['users_id']))
       $login['error'] = 'wrong_pwd';
-    }
-    else {
+    else
       $login['error'] = 'user_notfound';
-    }
 
     if(!empty($login['cookie']) AND !empty($login['mode'])) {
       $login['method'] = 'form_cookie';
-      setcookie('cs_userid',$login_db['users_id'], $cs_main['cookie']['lifetime'], $cs_main['cookie']['path'], $cs_main['cookie']['domain']);
-      setcookie('cs_securepw',$login['securepw'], $cs_main['cookie']['lifetime'], $cs_main['cookie']['path'], $cs_main['cookie']['domain']);
+      cs_login_cookies($login_db['users_id']);
     }
   }
 }
@@ -85,8 +106,10 @@ if(!empty($_SESSION['users_id'])) {
 
   if (empty($login['method'])) $login['method'] = 'session';
   $login['mode'] = TRUE;
-  // $acc_sc = 'users_id, users_nick, users_lang, access_id, users_limit, users_view, users_timezone, users_dstime, users_ajax, users_tpl, users_theme, users_pwd';
-  $account = cs_sql_select(__FILE__, 'users', '*', "users_id = '" . (int) $_SESSION['users_id'] . "' AND users_active = 1 AND users_delete = 0");
+  // $acc_sc = 'users_id, users_nick, users_lang, access_id, users_limit, users_view, users_timezone, 
+  // users_dstime, users_ajax, users_tpl, users_theme, users_pwd';
+  $acc_wr = "users_id = '" . (int) $_SESSION['users_id'] . "' AND users_active = 1 AND users_delete = 0";
+  $account = cs_sql_select(__FILE__, 'users', '*', $acc_wr);
   if (empty($account) OR ($account['users_pwd'] != $_SESSION['users_pwd'])) {
     session_destroy();
     $login['mode'] = FALSE;
@@ -96,8 +119,7 @@ if(!empty($_SESSION['users_id'])) {
 }
 
 if(!empty($_COOKIE['cs_userid'])) {
-  setcookie('cs_userid',$account['users_id'], $cs_main['cookie']['lifetime'], $cs_main['cookie']['path'], $cs_main['cookie']['domain']);
-  setcookie('cs_securepw',$account['users_pwd'], $cs_main['cookie']['lifetime'], $cs_main['cookie']['path'], $cs_main['cookie']['domain']);  
+  cs_login_cookies($account['users_id']);
 }
 if(!empty($account['users_id'])) {
   if($_SESSION['users_ip'] != cs_getip() OR $_SESSION['users_agent'] != $user_agent) {
@@ -105,11 +127,9 @@ if(!empty($account['users_id'])) {
     $login['mode'] = FALSE;
   }
   elseif($cs_main['mod']=='users' AND $cs_main['action']=='logout') {
-    setcookie('cs_userid', '', 1, $cs_main['cookie']['path'], $cs_main['cookie']['domain']);
-    setcookie('cs_securepw', '', 1, $cs_main['cookie']['path'], $cs_main['cookie']['domain']);
+    cs_login_cookies();
     session_destroy();
     $login['mode'] = FALSE;
-    //if (!empty($account['users_ajax']) AND !empty($cs_main['ajax'])) die(ajax_js('window.location.reload()'));
   }
   else {
     $cells = array('users_laston');
@@ -117,9 +137,9 @@ if(!empty($account['users_id'])) {
     cs_sql_update(__FILE__,'users',$cells,$content,$account['users_id']);
   }
 }
-else {
-  $account = array('access_id' => 1, 'users_id' => 0, 'users_lang' => $cs_main['def_lang'], 'users_limit' => $cs_main['data_limit'], 'users_timezone' => $cs_main['def_timezone'], 'users_dstime' => $cs_main['def_dstime']);
-}
+else
+  $account = array('access_id' => 1, 'users_id' => 0, 'users_lang' => $cs_main['def_lang'], 'users_limit' => $cs_main['data_limit'],
+                   'users_timezone' => $cs_main['def_timezone'], 'users_dstime' => $cs_main['def_dstime']);
 
 $gma = cs_sql_select(__FILE__,'access','*','access_id = "' . (int) $account['access_id'] . '"', 0,0,1, 'access_' . $account['access_id']);
 if(is_array($gma))
@@ -129,8 +149,7 @@ if(empty($cs_main['maintenance_access']))
   $cs_main['maintenance_access'] = 3;
 
 if(empty($cs_main['public']) AND !empty($account['users_id']) AND $account['access_clansphere'] < $cs_main['maintenance_access']) {
-  setcookie('cs_userid', '', 1, $cs_main['cookie']['path'], $cs_main['cookie']['domain']);
-  setcookie('cs_securepw', '', 1, $cs_main['cookie']['path'], $cs_main['cookie']['domain']);
+  cs_login_cookies();
   session_destroy();
   $login['mode'] = FALSE;
   $login['error'] = 'not_public'; 
