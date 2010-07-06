5401a00fd4dcefbecf6561646ada8275c7e3bd96
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-07-06 17:56:38 +0000

    removed notifications sql table from users mod info file

--- mods/users/info.php
+++ mods/users/info.php
@@ -4,16 +4,16 @@
 
 $cs_lang = cs_translate('users');
 
-$mod_info['name']    = $cs_lang['mod_name'];
-$mod_info['version']  = $cs_main['version_name'];
-$mod_info['released']  = $cs_main['version_date'];
-$mod_info['creator']  = 'Fr33z3m4n';
-$mod_info['team']    = 'ClanSphere';
-$mod_info['url']    = 'www.clansphere.net';
-$mod_info['text']    = $cs_lang['mod_text'];
-$mod_info['icon']    = 'personal';
-$mod_info['show']    = array('users/view' => 1, 'clansphere/admin' => 3, 'options/roots' => 5);
-$mod_info['categories']  = FALSE;
-$mod_info['comments']  = FALSE;
+$mod_info['name']       = $cs_lang['mod_name'];
+$mod_info['version']    = $cs_main['version_name'];
+$mod_info['released']   = $cs_main['version_date'];
+$mod_info['creator']    = 'Fr33z3m4n';
+$mod_info['team']       = 'ClanSphere';
+$mod_info['url']        = 'www.clansphere.net';
+$mod_info['text']       = $cs_lang['mod_text'];
+$mod_info['icon']       = 'personal';
+$mod_info['show']       = array('users/view' => 1, 'clansphere/admin' => 3, 'options/roots' => 5);
+$mod_info['categories'] = FALSE;
+$mod_info['comments']   = FALSE;
 $mod_info['protected']  = TRUE;
-$mod_info['tables']    = array('users', 'notifications', 'usernicks');
+$mod_info['tables']     = array('users', 'usernicks');
