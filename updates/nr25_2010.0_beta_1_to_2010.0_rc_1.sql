9848d79d8e79a8d01f2a7f474f309920c6e66382
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-07-07 20:37:15 +0000

    ################################################# 2010.0 RC 1 RELEASE

--- updates/nr25_2010.0_beta_1_to_2010.0_rc_1.sql
+++ updates/nr25_2010.0_beta_1_to_2010.0_rc_1.sql
@@ -1,5 +1,5 @@
 UPDATE {pre}_options SET options_value = '2010.0 RC 1' WHERE options_mod = 'clansphere' AND options_name = 'version_name';
 UPDATE {pre}_options SET options_value = '2010-07-07' WHERE options_mod = 'clansphere' AND options_name = 'version_date';
-UPDATE {pre}_options SET options_value = 55 WHERE options_mod = 'clansphere' AND options_name = 'version_id';
+UPDATE {pre}_options SET options_value = 56 WHERE options_mod = 'clansphere' AND options_name = 'version_id';
 
 DROP TABLE {pre}_notifications;
