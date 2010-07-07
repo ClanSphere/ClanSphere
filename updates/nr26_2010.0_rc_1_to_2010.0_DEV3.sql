631d24e96970bffccf97e43ebe7ec5a6287b0df8
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-07-07 21:20:00 +0000

    corrected update file content

--- updates/nr26_2010.0_rc_1_to_2010.0_DEV3.sql
+++ updates/nr26_2010.0_rc_1_to_2010.0_DEV3.sql
@@ -1,5 +1,3 @@
 UPDATE {pre}_options SET options_value = '2010.0 DEV3' WHERE options_mod = 'clansphere' AND options_name = 'version_name';
 UPDATE {pre}_options SET options_value = '2010-07-08' WHERE options_mod = 'clansphere' AND options_name = 'version_date';
 UPDATE {pre}_options SET options_value = 57 WHERE options_mod = 'clansphere' AND options_name = 'version_id';
-
-DROP TABLE {pre}_notifications;
