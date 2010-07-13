c83cd12d6abe52abf7613431d54167c94f702c77
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-07-13 15:17:39 +0000

    fixed gallery comments not forbidden while creating an advanced one

--- mods/gallery/com_create.php
+++ mods/gallery/com_create.php
@@ -9,7 +9,12 @@ $cs_get = cs_get('id');
 $fid = empty($cs_post['fid']) ? 0 : $cs_post['fid'];
 $quote_id = empty($cs_get['id']) ? 0 : $cs_get['id'];
 
-$gallery = cs_sql_select(__FILE__,'gallery','gallery_close',"gallery_id = '" . $fid . "'");
+$gallery = cs_sql_select(__FILE__,'gallery','folders_id',"gallery_id = '" . $fid . "'");
+$where = "folders_mod = 'gallery' AND folders_id = '" . $gallery['folders_id'] . "'";
+$cs_folders = cs_sql_select(__FILE__,'folders','folders_advanced',$where);
+
+$advanced = empty($cs_folders['folders_advanced']) ? '0,0,0,0' : $cs_folders['folders_advanced'];
+$advanced = explode(",",$advanced);
 
 require_once('mods/comments/functions.php');
-cs_commments_create($fid,'gallery','com_view',$quote_id,$cs_lang['mod_name'],$gallery['gallery_close'],'where');
+cs_commments_create($fid,'gallery','com_view',$quote_id,$cs_lang['mod_name'],$advanced[1],'where');
