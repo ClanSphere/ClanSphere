7f085212b34ba47cea0a16d2683e63a5486c61f5
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-07-18 02:12:37 +0000

    closed #888 - added wordmax and date to navtop

--- mods/files/navtop.php
+++ mods/files/navtop.php
@@ -5,22 +5,22 @@
 $cs_lang = cs_translate('files');
 
 $max = 4;
+$wordmax = 13;
+$data = array();
 
 $where = 'cat.categories_access <= ' . (int) $account['access_files'];
 $join = 'files fls INNER JOIN {pre}_categories cat ON fls.categories_id = cat.categories_id';
-$cs_files = cs_sql_select(__FILE__,$join,'files_name, files_id, files_count',$where,'files_count DESC',0,$max);
+$data['files'] = cs_sql_select(__FILE__,$join,'files_name, files_time, files_count, files_id',$where,'files_count DESC',0,$max);
 
-if (!empty($cs_files)) {
-
-  $data = array();
+if (!empty($data['files'])) {
   $count = 1;
-  $count_files = count($cs_files);
-  for ($run = 0; $run < $count_files; $run++) {
-    $cs_files[$run]['count'] = $count;
+  $files_loop = count($data['files']);
+  for($run=0; $run<$files_loop; $run++) {
+    $data['files'][$run]['date'] = cs_date('unix',$data['files'][$run]['files_time']);
+    $data['files'][$run]['files_name'] = strlen($data['files'][$run]['files_name']) > $wordmax ? substr($data['files'][$run]['files_name'], 0, $wordmax).'..' : $data['files'][$run]['files_name'];
+    $data['files'][$run]['count'] = $count;
     $count++;
   }
-  
-  $data['files'] = $cs_files;
   
   echo cs_subtemplate(__FILE__,$data,'files','navtop');
 }
