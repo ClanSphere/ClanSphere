8c7bc525b3fd3f1016a028dbfd4e616b8731879f
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-07-26 07:27:15 +0000

    ################################################# 2010.0 RC 2 RELEASE

--- updates/nr26_2010.0_rc_1_to_2010.0_rc_2.sql
+++ updates/nr26_2010.0_rc_1_to_2010.0_rc_2.sql
@@ -1,6 +1,6 @@
-UPDATE {pre}_options SET options_value = '2010.0 DEV3' WHERE options_mod = 'clansphere' AND options_name = 'version_name';
-UPDATE {pre}_options SET options_value = '2010-07-08' WHERE options_mod = 'clansphere' AND options_name = 'version_date';
-UPDATE {pre}_options SET options_value = 57 WHERE options_mod = 'clansphere' AND options_name = 'version_id';
+UPDATE {pre}_options SET options_value = '2010.0 RC 2' WHERE options_mod = 'clansphere' AND options_name = 'version_name';
+UPDATE {pre}_options SET options_value = '2010-07-26' WHERE options_mod = 'clansphere' AND options_name = 'version_date';
+UPDATE {pre}_options SET options_value = 58 WHERE options_mod = 'clansphere' AND options_name = 'version_id';
 
 ALTER TABLE {pre}_access ADD access_lightbox int(2) NOT NULL default '0';
 
