11a59e3fbc553ffb5c07da83568736fba11221ec
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-08-04 11:16:46 +0000

    a simple title is better than none

--- mods/ajax/navaddthis.php
+++ mods/ajax/navaddthis.php
@@ -6,7 +6,9 @@ $cs_lang = cs_translate('ajax');
 
 $data = array();
 
-$data['encoded']['uri'] = cs_url_self(1, 0, 1);
-$data['if']['share']    = empty($data['encoded']['uri']) ? 0 : 1;
+$data['encoded']['uri']   = cs_url_self(1, 0, 1);
+$data['encoded']['title'] = rawurlencode($cs_main['def_title']);
+
+$data['if']['share'] = empty($data['encoded']['uri']) ? 0 : 1;
 
 echo cs_subtemplate(__FILE__,$data,'ajax','navaddthis');
--- themes/base/ajax/navaddthis.tpl
+++ themes/base/ajax/navaddthis.tpl
@@ -1,5 +1,5 @@
 {if:share}
-{icon:bookmark} <a href="http://addthis.com/bookmark.php?url={encoded:uri}" class="noajax">{lang:bookmark_addthis}</a>
+{icon:bookmark} <a href="http://addthis.com/bookmark.php?title={encoded:title}&amp;url={encoded:uri}" class="noajax">{lang:bookmark_addthis}</a>
 {stop:share}
 {unless:share}
 {icon:bookmark} {lang:bookmark_not}
