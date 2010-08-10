6433582bef3d2a8e1213d807f639b28cb83becdc
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-08-10 14:29:43 +0000

    refs #926 - synced mail protection functions

--- mods/ajax/mail_func.php
+++ mods/ajax/mail_func.php
@@ -8,9 +8,9 @@ function cs_ajax_mail($mail, $link = '', $service = 'mailto:')
   $email = explode("@", $mail);
   $domain = empty($email[1]) ? array(0,1) : explode(".", $email[1]);
   $link = empty($link) ? $email[0] . ' (at) ' . $domain[0] . ' (dot) ' . $domain[1] : $link;
-  $mail = (binary) $mail;
+  $mail = (binary) $mail; # only difference between mail_func files
   $str = base64_encode($mail);
 
-  return '<a href="#" onclick="$.get(\'' . $cs_main['php_self']['dirname'] . 'mods/ajax/mail.php?mail=' . $str . '\',
-                   function(r) { window.location = \'mailto:\' + r; })">' . $link . '</a>';       
+  return '<a href="#" onclick="$.get(\'' . $cs_main['php_self']['dirname'] . 'mods/ajax/mail.php?mail='
+       . $str . '\', function(r) { window.location = \'' . $service . '\' + r; })">' . $link . '</a>';       
 }
