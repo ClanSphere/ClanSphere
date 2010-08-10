deedd98fb97fd20e619290a8fc8aebece26db8ba
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-08-10 15:11:52 +0000

    fixed #926 - email protection didn't handle multiple dots

--- mods/ajax/mail_func.php
+++ mods/ajax/mail_func.php
@@ -5,12 +5,10 @@
 function cs_ajax_mail($mail, $link = '', $service = 'mailto:')
 {
   global $cs_main;
-  $email = explode("@", $mail);
-  $domain = empty($email[1]) ? array(0,1) : explode(".", $email[1]);
-  $link = empty($link) ? $email[0] . ' (at) ' . $domain[0] . ' (dot) ' . $domain[1] : $link;
-  $mail = (binary) $mail; # only difference between mail_func files
-  $str = base64_encode($mail);
+  $mail_lnk = str_replace(array('@', '.'), array(' (at) ', ' (dot) '), $mail);
+  $mail_bin = (binary) $mail; # only difference between mail_func files
+  $mail_str = base64_encode($mail_bin);
 
   return '<a href="#" onclick="$.get(\'' . $cs_main['php_self']['dirname'] . 'mods/ajax/mail.php?mail='
-       . $str . '\', function(r) { window.location = \'' . $service . '\' + r; })">' . $link . '</a>';       
+       . $mail_str . '\', function(r) { window.location = \'' . $service . '\' + r; })">' . $mail_lnk . '</a>';       
 }
