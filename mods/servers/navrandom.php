41cb63f124aab2c2c3fd35d1f4c0305388f77654
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-08-25 21:22:00 +0000

    refs #951 - server pic in navrandom file

--- mods/servers/navrandom.php
+++ mods/servers/navrandom.php
@@ -10,7 +10,6 @@ $data = array('servers' => array());
 
 // Test if fsockopen active
 if (fsockopen("udp://127.0.0.1", 1)) {
-
 	include_once 'mods/servers/gameq/GameQ.php';
 
 	/* Get Server SQL-Data */
@@ -23,92 +22,93 @@ if (fsockopen("udp://127.0.0.1", 1)) {
 
 	$data['servers']['if']['live'] = false;
 	$data['servers']['hostname'] = $cs_servers['servers_name'];
-	$server_query_ex = explode(";",$cs_servers['servers_class']);
-	$cs_servers['servers_class'] = $server_query_ex[0];
-	$cs_servers['servers_game'] = $server_query_ex[1];
-	if(!empty($cs_servers['servers_stats'])) {
-
-		$gq->addServer(0, array($cs_servers['servers_class'],$cs_servers['servers_ip'],$cs_servers['servers_port']));
-		$gq->setOption('timeout', 200);
-		$gq->setFilter('stripcolor');
-		$results = $gq->requestData();
-		$server = $results[0];
-
-		if(!empty($server['gq_online'])) {
-			$data['if']['live'] = true;
-			$data['servers']['map'] = '';
-			$data['servers']['mappic'] = 'uploads/servers/no_response.jpg';
-			$data['servers']['mapname'] = '';
-			$data['servers']['max_players'] = isset($server['max_players']) ? $server['max_players'] : 0;
-			$data['servers']['num_players'] = isset($server['num_players']) ? $server['num_players'] : 0;
-			$data['servers']['game_descr'] = isset($server['game_descr']) ? $server['game_descr'] : '';
-
+			$server_query_ex = explode(";",$cs_servers['servers_class']);
+			$cs_servers['servers_class'] = $server_query_ex[0];
+			$cs_servers['servers_game'] = $server_query_ex[1];
 			if(!empty($cs_servers['servers_stats'])) {
-				$data['servers']['servers_ip'] = $cs_servers['servers_ip'];
-				$data['servers']['servers_port'] = $cs_servers['servers_port'];
-				if(isset($server['gamename']) AND !empty($server['gamename'])) {
-					$data['servers']['game_descr'] = $server['gamename'];
-				}
 
-				if(isset($server['map']) && !empty($server['map'])) {
-					$data['servers']['map'] = $server['map'];
-					if(file_exists('uploads/servers/' . $cs_servers['servers_class'] . '/' . $data['servers']['map'] . '.jpg')) {
-						$data['servers']['mappic'] = 'uploads/servers/' . $cs_servers['servers_class'] . '/' . $data['servers']['map'] . '.jpg';
+				$gq->addServer(0, array($cs_servers['servers_class'],$cs_servers['servers_ip'],$cs_servers['servers_port']));
+				$gq->setOption('timeout', 200);
+				$gq->setFilter('stripcolor');
+				$results = $gq->requestData();
+				$server = $results[0];
+
+				if(!empty($server['gq_online'])) {
+					$data['if']['live'] = true;
+					$data['servers']['map'] = '';
+					$data['servers']['mappic'] = 'uploads/servers/no_response.jpg';
+					$data['servers']['mapname'] = '';
+					$data['servers']['max_players'] = isset($server['max_players']) ? $server['max_players'] : 0;
+					$data['servers']['num_players'] = isset($server['num_players']) ? $server['num_players'] : 0;
+					$data['servers']['game_descr'] = isset($server['game_descr']) ? $server['game_descr'] : '';
+
+					if(!empty($cs_servers['servers_stats'])) {
+						$data['servers']['servers_ip'] = $cs_servers['servers_ip'];
+						$data['servers']['servers_port'] = $cs_servers['servers_port'];
+						if(isset($server['gamename']) AND !empty($server['gamename'])) {
+							$data['servers']['game_descr'] = $server['gamename'];
 					}
+
+					if(isset($server['map']) && !empty($server['map'])) {
+						$data['servers']['map'] = $server['map'];
+						if(file_exists('uploads/servers/' . $cs_servers['servers_game'] . '/' . $data['servers']['map'] . '.jpg')) {
+							$data['servers']['mappic'] = 'uploads/servers/' . $cs_servers['servers_game'] . '/' . $data['servers']['map'] . '.jpg';
+						}
+						else {
+							$data['servers']['mappic'] = 'uploads/servers/' . $cs_servers['servers_game'] . '/default.jpg';
+						}
+					}
+					elseif(isset($server['mapname']) && !empty($server['mapname'])) {
+						$data['servers']['map'] = $server['mapname'];
+						if(file_exists('uploads/servers/' . $cs_servers['servers_game'] . '/' . $data['servers']['mapname'] . '.jpg')) {
+							$data['servers']['mappic'] = 'uploads/servers/' . $cs_servers['servers_game'] . '/' . $data['servers']['mapname'] . '.jpg';
+						}
+						else {
+							$data['servers']['mappic'] = 'uploads/servers/' . $cs_servers['servers_game'] . '/default.jpg';
+						}
+					}
+
+					$select = 'maps_name, maps_picture, server_name';
+					$where = 'server_name = \'' . $data['servers']['map'] . '\'';
+					$sqlmap = cs_sql_select(__FILE__,'maps',$select,$where,0,0,1);
+					if(!empty($sqlmap)) {
+						$data['servers']['map'] = $sqlmap['maps_name'];
+						if(file_exists('uploads/maps/' . $sqlmap['maps_picture'])) {
+							$data['servers']['mappic'] = 'uploads/maps/' . $sqlmap['maps_picture'];
+						}
+					}
+
+						if(!isset($server['max_players'])) {
+							if(isset($server['sv_maxclients'])) {
+								$data['servers']['max_players'] = $server['sv_maxclients'];
+							}
+						}
+						if(!isset($server['num_players'])) {
+							if(isset($server['clients'])) {
+								$data['servers']['num_players'] = $server['clients'];
+							}
+						}
+
+
+
+						/* if TS View, use teamspeak:// */
+						if($cs_servers['servers_class'] == 'ts3') {
+						$data['servers']['proto'] = 'teamspeak://';
+							$data['servers']['num_players'] = 0;
+							for($a=0; $a<count($server['teams']); $a++) {
+								$data['servers']['num_players'] = $data['servers']['num_players'] + $server['teams'][$a]['total_clients'];
+							}
+						}
 					else {
-						$data['servers']['mappic'] = 'uploads/servers/' . $cs_servers['servers_class'] . '/default.jpg';
-					}
-				}
-				elseif(isset($server['mapname']) && !empty($server['mapname'])) {
-					$data['servers']['map'] = $server['mapname'];
-					if(file_exists('uploads/servers/' . $cs_servers['servers_game'] . '/' . $data['servers']['mapname'] . '.jpg')) {
-						$data['servers']['mappic'] = 'uploads/servers/' . $cs_servers['servers_game'] . '/' . $data['servers']['mapname'] . '.jpg';
-					}
-					else {
-						$data['servers']['mappic'] = 'uploads/servers/' . $cs_servers['servers_game'] . '/default.jpg';
-					}
-				}
+							$data['servers']['proto'] = 'hlsw://';
+						}
 
-				$select = 'maps_name, maps_picture, server_name';
-				$where = 'server_name = \'' . $data['servers']['map'] . '\'';
-				$sqlmap = cs_sql_select(__FILE__,'maps',$select,$where,0,0,1);
-				if(!empty($sqlmap)) {
-					$data['servers']['map'] = $sqlmap['maps_name'];
-					if(file_exists('uploads/maps/' . $sqlmap['maps_picture'])) {
-						$data['servers']['mappic'] = 'uploads/maps/' . $sqlmap['maps_picture'];
-					}
+						$data['servers']['pass'] = empty($data['servers']['pass']) ? $cs_lang['no'] : $cs_lang['yes'];
+						$data['servers']['id'] = $cs_servers['servers_id'];
+					flush();
 				}
-
-				if(!isset($server['max_players'])) {
-					if(isset($server['sv_maxclients'])) {
-						$data['servers']['max_players'] = $server['sv_maxclients'];
-					}
-				}
-				if(!isset($server['num_players'])) {
-					if(isset($server['clients'])) {
-						$data['servers']['num_players'] = $server['clients'];
-					}
-				}
-
-
-
-				/* if TS View, use teamspeak:// */
-				if($cs_servers['servers_class'] == 'ts3') {
-					$data['servers']['proto'] = 'teamspeak://';
-					$data['servers']['num_players'] = 0;
-					for($a=0; $a<count($server['teams']); $a++) {
-						$data['servers']['num_players'] = $data['servers']['num_players'] + $server['teams'][$a]['total_clients'];
-					}
-				}
-				else {
-					$data['servers']['proto'] = 'hlsw://';
-				}
-
-				$data['servers']['pass'] = empty($data['servers']['pass']) ? $cs_lang['no'] : $cs_lang['yes'];
-				$data['servers']['id'] = $cs_servers['servers_id'];
-				flush();
 			}
-		}
 	}
 }
+
 echo cs_subtemplate(__FILE__,$data,'servers','navrandom');
