b3a69cf42a249e445b31dc1583f6d8c2dc66005d
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-08-26 19:28:14 +0000

    undo last change

--- debug.php
+++ debug.php
@@ -3,7 +3,7 @@
 // $Id$
 
 # Overwrite global settings by using the following array
-$cs_main = array('init_sql' => true, 'init_tpl' => true, 'tpl_file' => 'index.htm', 'debug' => true, 'themebar' => true);
+$cs_main = array('init_sql' => true, 'init_tpl' => true, 'tpl_file' => 'debug.htm', 'debug' => true, 'themebar' => true);
 
 require_once 'system/core/functions.php';
 
