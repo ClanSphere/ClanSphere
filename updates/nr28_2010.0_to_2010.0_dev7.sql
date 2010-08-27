694f5bd61ea0464feea79acd001e32e921eab40b
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-08-27 01:10:49 +0000

    added new sql update file

---
+++ updates/nr28_2010.0_to_2010.0_dev7.sql
@@ -0,0 +1,3 @@
+UPDATE {pre}_options SET options_value = '2010.0 DEV 7' WHERE options_mod = 'clansphere' AND options_name = 'version_name';
+UPDATE {pre}_options SET options_value = '2010-08-27' WHERE options_mod = 'clansphere' AND options_name = 'version_date';
+UPDATE {pre}_options SET options_value = 61 WHERE options_mod = 'clansphere' AND options_name = 'version_id';
