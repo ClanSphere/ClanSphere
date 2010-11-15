2499e38a70ae944fa04aa5bc14bc53a56ced2013
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-11-15 16:54:07 +0000

    reworked svn after release

---
+++ updates/nr29_2010.1_to_2010.2_dev.sql
@@ -0,0 +1,3 @@
+UPDATE {pre}_options SET options_value = '2010.2 DEV' WHERE options_mod = 'clansphere' AND options_name = 'version_name';
+UPDATE {pre}_options SET options_value = '2010-11-16' WHERE options_mod = 'clansphere' AND options_name = 'version_date';
+UPDATE {pre}_options SET options_value = '63' WHERE options_mod = 'clansphere' AND options_name = 'version_id';
