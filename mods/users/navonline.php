a14000c7618e599551a65febfccffe2de9b5bb4f
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-11-28 20:11:45 +0000

    fixed a typo, well 6 times ^^

--- mods/users/navonline.php
+++ mods/users/navonline.php
@@ -6,7 +6,7 @@ $cs_lang = cs_translate('users');
 
 $five_min = cs_time() - 300;
 $select = 'users_id, users_nick, users_country, users_active, users_invisible';
-$invisble = ($account['access_users'] > 4) ? '' : " AND users_invisible = '0'";
+$invisible = ($account['access_users'] > 4) ? '' : " AND users_invisible = '0'";
 $upcome = "users_laston > " . $five_min . " AND users_active = '1'" . $invisible;
 $order = 'users_laston DESC';
 $cs_users = cs_sql_select(__FILE__,'users',$select,$upcome,$order,0,8);
@@ -24,7 +24,7 @@ if(empty($cs_users)) {
   for ($run = 0; $run < $count_users; $run++) {
 
     $data['users'][$run]['nick'] = cs_user($cs_users[$run]['users_id'], $cs_users[$run]['users_nick'], $cs_users[$run]['users_active']);
-    if(empty($invisble) AND !empty($cs_users[$run]['users_invisible']))
+    if(empty($invisible) AND !empty($cs_users[$run]['users_invisible']))
       $data['users'][$run]['nick'] = cs_html_italic(1) . $data['users'][$run]['nick'] . cs_html_italic(0);
 
     $data['users'][$run]['countryicon'] = cs_html_img('symbols/countries/'.$cs_users[$run]['users_country'].'.png');
--- mods/users/navonline_ava.php
+++ mods/users/navonline_ava.php
@@ -6,7 +6,7 @@ $cs_lang = cs_translate('users');
 
 $five_min = cs_time() - 300;
 $select = 'users_id, users_nick, users_country, users_active, users_invisible, users_avatar';
-$invisble = ($account['access_users'] > 4) ? '' : " AND users_invisible = '0'";
+$invisible = ($account['access_users'] > 4) ? '' : " AND users_invisible = '0'";
 $upcome = "users_laston > " . $five_min . " AND users_active = '1'" . $invisible;
 $order = 'users_laston DESC';
 $cs_users = cs_sql_select(__FILE__,'users',$select,$upcome,$order,0,8);
@@ -29,7 +29,7 @@ else {
       $data['users'][$run]['picture'] = 'symbols/users/no_pic.png';
     }
 
-    if(empty($invisble) AND !empty($cs_users[$run]['users_invisible']))
+    if(empty($invisible) AND !empty($cs_users[$run]['users_invisible']))
       $data['users'][$run]['nick'] = cs_html_italic(1) . $cs_users[$run]['users_nick'] . cs_html_italic(0);
     else
       $data['users'][$run]['nick'] = $cs_users[$run]['users_nick'];
