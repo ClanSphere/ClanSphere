8b2a66bb976a2c2264ad39ba012d8dce7c783c93
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-11-29 00:59:45 +0000

    reworked "mark all read" feature. may consume a little more resources now, but avoids duplicate entries

--- mods/board/mark.php
+++ mods/board/mark.php
@@ -8,41 +8,51 @@ $board_id = empty($_GET['id']) ? 0 : (int) $_GET['id'];
 
 $cs_readtime = cs_time() - $account['users_readtime'];
 
-$cells = 'thr.threads_id, thr.board_id, rea.users_id, rea.read_id, rea.read_since';
-$from ='threads thr LEFT JOIN {pre}_read rea ON thr.threads_id = rea.threads_id';
-$where = "rea.users_id= '". $account['users_id'] ."' AND thr.threads_last_time > '" . $cs_readtime . "'";
+# update alreay read threads with new time
+$cells = 'rea.read_id AS read_id, rea.threads_id, rea.users_id, thr.threads_last_time';
+$from = 'read rea INNER JOIN {pre}_threads thr ON rea.threads_id = thr.threads_id';
+$where = "rea.users_id = " . (int) $account['users_id'] . " AND thr.threads_last_time > " . (int) $cs_readtime;
 
-if(empty($board_id))
-  $thr_where = "threads_last_time > '" . $cs_readtime . "'";
-else {
-  $where .=" AND thr.board_id = '". $board_id ."'";
-  $thr_where = "board_id = '". $board_id ."' AND threads_last_time > '" . $cs_readtime . "'";
+if(!empty($board_id)) {
+  $cells .= ', thr.board_id';
+  $where = "thr.board_id = " . (int) $board_id . " AND " . $where;
 }
 
-$readed = cs_sql_select(__FILE__,$from,$cells,$where,'thr.threads_id',0,0);
-$threads = cs_sql_select(__FILE__,'threads','threads_id, threads_last_time',$thr_where,'threads_id',0,0);
-$threads_loop = count($threads);
+$readed = cs_sql_select(__FILE__, $from, $cells, $where, 0, 0, 0);
 
-for($run=0; $run < $threads_loop; $run++) {
-  if(isset($readed[$run]['threads_id']) AND in_array($readed[$run]['threads_id'], $threads[$run])) {
-    if($readed[$run]['read_since'] < $threads[$run]['threads_last_time']) {
-      $data['read_since'] = cs_time();
-      $cells = array_keys($data);
-      $save = array_values($data);  
-      cs_sql_update(__FILE__,'read',$cells,$save,$readed[$run]['read_id'], 0, 0);
+$read_cells = array('read_since');
+$read_save = array(cs_time());
+
+if(!empty($readed)) {
+foreach($readed AS $readme)
+  cs_sql_update(__FILE__, 'read', $read_cells, $read_save, $readme['read_id'], 0, 0);
+}
+
+# add threads that are not read at all
+$cells = 'thr.threads_id AS threads_id, thr.threads_last_time, rea.users_id AS users_id';
+$from = 'threads thr LEFT JOIN {pre}_read rea ON (thr.threads_id = rea.threads_id AND rea.users_id = ' . (int) $account['users_id'] . ')';
+$where = "thr.threads_last_time > " . (int) $cs_readtime;
+
+if(!empty($board_id)) {
+  $cells .= ', thr.board_id';
+  $where = "thr.board_id = " . (int) $board_id . " AND " . $where;
+}
+
+$readed = cs_sql_select(__FILE__, $from, $cells, $where, 0, 0, 0);
+
+$read_cells = array('read_since', 'users_id', 'threads_id');
+
+if(!empty($readed)) {
+  foreach($readed AS $readme) {
+    if(empty($readme['users_id'])) {
+      $read_save = array(cs_time(), $account['users_id'], $readme['threads_id']);
+      cs_sql_insert(__FILE__, 'read', $read_cells, $read_save);
     }
   }
-  else {
-    $data['threads_id'] = $threads[$run]['threads_id'];
-    $data['users_id'] = $account['users_id'];
-    $data['read_since'] = cs_time();
-    $cells = array_keys($data);
-    $save = array_values($data); 
-    cs_sql_insert(__FILE__,'read',$cells,$save);
-  }
 }
 
-if($thr_where == "threads_last_time > '" . $cs_readtime . "'")
+# redirect
+if(empty($board_id))
     cs_redirect($cs_lang['mark_all'], 'board', 'list');
 else
   cs_redirect($cs_lang['mark_board'], 'board', 'listcat', 'id=' . $board_id);
