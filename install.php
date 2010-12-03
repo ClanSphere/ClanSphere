374a0e3c98e9d10afdb9c4b7ed1c95b194ceece1
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-12-03 22:32:22 +0000

    reworked svn after release

--- install.php
+++ install.php
@@ -18,8 +18,8 @@ $cs_main = array( 'charset'         => $charset,
                   'public'          => 1,
                   'img_path'        => 'crystal_project',
                   'img_ext'         => 'png',
-                  'version_name'    => '2010.2',
-                  'version_date'    => '2010-12-03');
+                  'version_name'    => '2010.2.1 DEV',
+                  'version_date'    => '2010-12-04');
 
 require_once 'system/core/functions.php';
 
---
+++ updates/nr30_2010.2_to_2010.2.1_dev.sql
@@ -0,0 +1,3 @@
+UPDATE {pre}_options SET options_value = '2010.2.1 DEV' WHERE options_mod = 'clansphere' AND options_name = 'version_name';
+UPDATE {pre}_options SET options_value = '2010-12-04' WHERE options_mod = 'clansphere' AND options_name = 'version_date';
+UPDATE {pre}_options SET options_value = '65' WHERE options_mod = 'clansphere' AND options_name = 'version_id';
