312f6127d5e9d8dd2a4d208681a76a396e44011b
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2010-12-10 17:44:42 +0000

    fixed nextbirth again, does now work within the current and next year

--- mods/users/nextbirth.php
+++ mods/users/nextbirth.php
@@ -14,17 +14,21 @@ if($data['day'] != $time_now['2'].$time_now['1']) {
   $nextday = cs_datereal('d', cs_time()+$options['nextbirth_time_interval']);
   $nextmonth = cs_datereal('m', cs_time()+$options['nextbirth_time_interval']);
   $data['day'] = $time_now['2'].$time_now['1'];
+  $check_n = ($nextmonth == '01') ? (int) 13 . $nextday : (int) $nextmonth . $nextday;
+  $check_t = (int) $time_now['1'] . $time_now['2'];
 
   $select = 'users_id, users_nick, users_age';
   $where = "users_hidden NOT LIKE '%users_age%' AND users_active = 1 ";
   $where .= ($nextmonth == $time_now['1']) ? " AND users_age LIKE '%-". $time_now['1'] ."-%'" : "AND (users_age LIKE '%-". $time_now['1'] ."-%' OR users_age LIKE '%-". $nextmonth ."-%')";
   $cs_users = cs_sql_select(__FILE__,'users',$select,$where,0,0,0);
+
   if(!empty($cs_users)) {
     $run=0;
     foreach($cs_users as $birth_users) {
       $birth = explode('-', $birth_users['users_age']);
-      $time_now['1'] = ($time_now['1'] == 12) ? 0 : $time_now['1'];
-      if($birth[1].$birth[2] < $nextmonth.$nextday AND $birth[1].$birth[2] > $time_now['1'].$time_now['2']) {
+      $check_b = ($birth[1] == '01') ? (int) 13 . $birth[2] : (int) $birth[1] . $birth[2];
+
+      if($check_b < $check_n AND $check_b > $check_t) {
         $data['users'][$run]['users_id'] =  $birth_users['users_id'];
         $data['users'][$run]['users_nick'] = $birth_users['users_nick'];
         $data['users'][$run]['users_day'] = $birth[2];
