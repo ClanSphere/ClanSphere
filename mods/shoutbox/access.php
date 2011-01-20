d133d831aea7426e45bfdd0d71b65451b4d93e33
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2011-01-20 20:45:04 +0000

    fixed #1033 - shoutbox create does now require access level 2 instead of 1

--- mods/shoutbox/access.php
+++ mods/shoutbox/access.php
@@ -3,10 +3,10 @@
 // $Id$
 
 $axx_file['manage']       = 3;
-$axx_file['create']       = 1;
+$axx_file['create']       = 2;
 $axx_file['ip']           = 4;
 $axx_file['remove']       = 3;
-$axx_file['multiremove'] = 4;
-$axx_file['edit']       = 3;
-$axx_file['options']     = 4;
-$axx_file['list']       = 1;
+$axx_file['multiremove']  = 4;
+$axx_file['edit']         = 3;
+$axx_file['options']      = 4;
+$axx_file['list']         = 1;
