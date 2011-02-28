ccc0bfd7a609c9a5fe5352a0e0d7886574396718
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2011-02-28 12:23:03 +0000

    only show messages view theme when a message is found

--- mods/messages/view.php
+++ mods/messages/view.php
@@ -3,7 +3,9 @@
 // $Id$
 
 $cs_lang = cs_translate('messages');
+
 $data = array();
+
 $messages_id = (int) $_GET['id'];
 
 $cells = 'users_id, users_id_to, messages_subject, messages_time, messages_view, messages_text, messages_archiv_receiver, messages_archiv_sender';
@@ -19,16 +21,20 @@ if (empty($sender)) {
     $data['msg']['from'] = cs_user($data['msg']['users_id'], $user_data['users_nick'], $user_data['users_active'], $user_data['users_delete']);
     $data['if']['reply'] = true;
     $data['if']['archiv'] = empty($data['msg']['messages_archiv_receiver']) ? 1 : 0;
-} else {
+}
+else {
     $user_data = cs_sql_select(__FILE__, 'users', 'users_nick, users_active, users_delete', 'users_id = "' . $data['msg']['users_id_to'] . '"');
     $data['msg']['from'] = cs_user($data['msg']['users_id_to'], $user_data['users_nick'], $user_data['users_active'], $user_data['users_delete']);
     $data['lang']['from'] = $cs_lang['to'];
     $data['if']['reply'] = false;
     $data['if']['archiv'] = empty($data['msg']['messages_archiv_sender']) ? 1 : 0; 
 }
-$data['msg']['messages_time'] = cs_date('unix', $data['msg']['messages_time'],1);
-$data['msg']['messages_id'] = $messages_id;
-$data['msg']['messages_subject'] = cs_secure($data['msg']['messages_subject']);
-$data['msg']['messages_text'] = cs_secure($data['msg']['messages_text'],1,1);
 
-echo cs_subtemplate(__FILE__, $data, 'messages', 'view');
+if(isset($data['msg']['messages_id'])) {
+  $data['msg']['messages_time'] = cs_date('unix', $data['msg']['messages_time'],1);
+  $data['msg']['messages_id'] = $messages_id;
+  $data['msg']['messages_subject'] = cs_secure($data['msg']['messages_subject']);
+  $data['msg']['messages_text'] = cs_secure($data['msg']['messages_text'],1,1);
+
+  echo cs_subtemplate(__FILE__, $data, 'messages', 'view');
+}
