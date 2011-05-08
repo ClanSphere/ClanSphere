36801469f572594814ed3b0df84a3c082066fd71
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2011-05-08 18:12:11 +0000

    changed regex modifiers to workaround new detection problems

--- system/core/cachegen.php
+++ system/core/cachegen.php
@@ -116,7 +116,7 @@ function cs_cache_theme($mod, $action) {
   $tpl_data = str_replace('{page:path}', $cs_main['php_self']['dirname'], $tpl_data);
   $tpl_data = str_replace('{page:width}', $cs_main['def_width'], $tpl_data);
   $tpl_data = str_replace('{page:cellspacing}', $cs_main['cellspacing'], $tpl_data);
-  $tpl_data = preg_replace_callback("={icon:([\w]*?)}=i", 'cs_icon', $tpl_data);
+  $tpl_data = preg_replace_callback("={icon:([\S]*?)}=i", 'cs_icon', $tpl_data);
 
   $tpl_data = preg_replace_callback('={url(?:_([\w]*?))?:([\w]*?)(?:_([\w]*?)((?::(?:(?:[\S]*?{[\S]*?}[\S]*?)*?|[\S]*?))*?))?}=i', 'cs_templateurl', $tpl_data);
 
