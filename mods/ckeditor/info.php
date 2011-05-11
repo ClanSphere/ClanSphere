e063adef90fb01fcda89462c1fd77d92a3ddd33e
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2011-05-11 15:09:41 +0000

    updated ckeditor to 3.5.4

--- mods/ckeditor/info.php
+++ mods/ckeditor/info.php
@@ -8,7 +8,7 @@ $mod_info['released']   = $cs_main['version_date'];
 $mod_info['creator']    = 'CKSource';
 $mod_info['team']       = 'CKSource';
 $mod_info['url']        = 'www.ckeditor.com';
-$mod_info['text']       = 'Version 3.5.2';
+$mod_info['text']       = 'Version 3.5.4';
 $mod_info['icon']       = 'package_editors';
 $mod_info['show']       = array('abcode/rte_html' => 5, 'options/roots' => 5);
 $mod_info['categories'] = FALSE;
