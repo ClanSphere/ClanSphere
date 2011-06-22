d1cfd60c3c73bb7575c0fc6e2a086ec1b529ff6e
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2011-06-22 19:56:44 +0000

    changed ckeditor mod icon in info.php

--- mods/ckeditor/info.php
+++ mods/ckeditor/info.php
@@ -9,7 +9,7 @@ $mod_info['creator']    = 'CKSource';
 $mod_info['team']       = 'CKSource';
 $mod_info['url']        = 'www.ckeditor.com';
 $mod_info['text']       = 'Version 3.6.1';
-$mod_info['icon']       = 'package_editors';
+$mod_info['icon']       = 'ckeditor';
 $mod_info['show']       = array('abcode/rte_html' => 5, 'abcode/rte_more' => 5, 'options/roots' => 5);
 $mod_info['categories'] = FALSE;
 $mod_info['comments']   = FALSE;
