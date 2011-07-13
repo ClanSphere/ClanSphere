b54f039f3b7d54480b361b228047c059fd713c45
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2011-07-13 23:04:29 +0000

    fixed #1093 - squad access not checked in attachment download

--- mods/board/attachment.php
+++ mods/board/attachment.php
@@ -13,22 +13,29 @@ cs_init($cs_main);
 
 chdir('mods/board/');
 
+$forbidden = 0;
+
 if(!empty($_GET['id'])) {
     $id = (int) $_GET['id'];
     $tables = 'boardfiles fil INNER JOIN {pre}_threads thr ON thr.threads_id = fil.threads_id ';
     $tables .= 'INNER JOIN {pre}_board brd ON brd.board_id = thr.board_id ';
     $tables .= 'LEFT OUTER JOIN {pre}_boardpws bpw ON bpw.board_id = brd.board_id AND bpw.users_id = ' . (int) $account['users_id'];
-    $where = 'brd.board_access <= ' . (int) $account['access_board'] . ' AND fil.boardfiles_id = ' . (int) $id;
+    $where = '(brd.board_access <= ' . (int) $account['access_board'] . ' OR brd.squads_id > 0) AND fil.boardfiles_id = ' . (int) $id;
     $cs_thread_file = cs_sql_select(__FILE__,$tables,'*',$where,0,0,1);
     if(!empty($cs_thread_file)) {
-      if(!empty($cs_thread_file['board_pwd'])) {
-        if(empty($cs_thread_file['boardpws_id'])) {
-            die(cs_error_internal(0, 'Permission denied'));
-        }
+      if(!empty($cs_thread_file['squads_id']) AND $account['access_board'] < $cs_thread_file['board_access']) {
+	    $sq_where = 'users_id = "' . $account['users_id'] . '" AND squads_id = "' . $cs_thread_file['squads_id'] . '"';
+        $check_sq = cs_sql_count(__FILE__,'members',$sq_where);
+		if(empty($check_sq)) {
+          $forbidden = 'Not in valid squad';
+		}
+      }
+      elseif(!empty($cs_thread_file['board_pwd']) AND empty($cs_thread_file['boardpws_id'])) {
+          $forbidden = 'No valid password';
       }
     }
     else {
-        die(cs_error_internal(0, 'Permission denied'));
+        $forbidden = 'No result found';
     }
 }
 elseif (!empty($_GET['name'])) {
@@ -38,6 +45,9 @@ elseif (!empty($_GET['name'])) {
 else {
   die(cs_error_internal(0, 'File not found'));
 }
+
+if(!empty($forbidden))
+  die(cs_error_internal(0, 'Permission denied. Reason: ' . $forbidden));
 
 $com_cells = array('boardfiles_downloaded');
 $com_save = array((1+$cs_thread_file['boardfiles_downloaded']));
