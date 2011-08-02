1706895edc6b000a569376ae371f060ad410cb8b
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2011-08-02 16:50:48 +0000

    removed textareaexpander

--- mods/jquery/csp_func.js
+++ mods/jquery/csp_func.js
@@ -2,67 +2,7 @@
 // $Id$
 
 
-$(function(){ 
-
-  // jQuery plugin definition
-  $.fn.TextAreaExpander = function(minHeight, maxHeight) {
-
-    var hCheck = !($.browser.msie || $.browser.opera);
-
-    // resize a textarea
-    function ResizeTextarea(e) {
-
-      // event or initialize element?
-      e = e.target || e;
-
-      // find content length and box width
-      var vlen = e.value.length, ewidth = e.offsetWidth;
-      if (vlen != e.valLength || ewidth != e.boxWidth) {
-
-        if (hCheck && (vlen < e.valLength || ewidth != e.boxWidth)) e.style.height = "0px";
-        var h = Math.max(e.expandMin, Math.min(e.scrollHeight, e.expandMax));
-
-        e.style.overflow = (e.scrollHeight > h ? "auto" : "hidden");
-        e.style.height = h + "px";
-
-        e.valLength = vlen;
-        e.boxWidth = ewidth;
-      }
-
-      return true;
-    };
-
-    // initialize
-    this.each(function() {
-
-      // is a textarea?
-      if (this.nodeName.toLowerCase() != "textarea") return;
-
-      // set height restrictions
-      var p = this.className.match(/expand(\d+)\-*(\d+)*/i);
-      this.expandMin = minHeight || (p ? parseInt('0'+p[1], 10) : 0);
-      this.expandMax = maxHeight || (p ? parseInt('0'+p[2], 10) : 99999);
-
-      // initial resize
-      ResizeTextarea(this);
-
-      // zero vertical padding and add events
-      if (!this.Initialized) {
-        this.Initialized = true;
-        $(this).css("padding-top", 0).css("padding-bottom", 0);
-        $(this).bind("keyup", ResizeTextarea).bind("focus", ResizeTextarea);
-      }
-    });
-
-    return this;
-  };
-
-  $("textarea").not(".rte_html, .noexpand").TextAreaExpander(100, 500);
-
-  $(document).bind('csAjaxLoad', function(e,ele) {
-    $(ele).find('textarea').not(".rte_html, .noexpand").TextAreaExpander(100, 500);
-  });
-
+$(function(){
 
   $(".clip").live('click', function () { 
     $(this).children('img').toggle();
