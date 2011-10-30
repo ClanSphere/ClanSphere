2f7b2fbe83a161b66c5122b240ec9a3a652a03a3
hajo <hajo@a21cae8d-5634-0410-b85c-b410bfd0a5ac>
2011-10-30 06:14:11 +0000

    Thread:14989 - added codepaste 144 and 146

--- mods/board/users_home.php
+++ mods/board/users_home.php
@@ -12,9 +12,17 @@ $cs_homelimit = cs_sql_select(__FILE__,'users','users_homelimit, users_readtime'
 $cs_readtime = cs_time() - $cs_homelimit['users_readtime'];
 cs_sql_query(__FILE__, "DELETE FROM {pre}_read WHERE users_id = '" . $account['users_id'] . "' AND read_since < '" . $cs_readtime . "'");
 
-$from  = "threads thr INNER JOIN {pre}_board frm ON frm.board_id = thr.board_id INNER JOIN {pre}_categories cat ON cat.categories_id = frm.categories_id INNER JOIN {pre}_users usr ON thr.threads_last_user = usr.users_id LEFT JOIN {pre}_read red ON thr.threads_id = red.threads_id AND red.users_id = '" . $account['users_id'] . "'";
+$from  = "threads thr " .
+         "INNER JOIN {pre}_board frm ON frm.board_id = thr.board_id " . 
+         "INNER JOIN {pre}_categories cat ON cat.categories_id = frm.categories_id " .
+         "INNER JOIN {pre}_users usr ON thr.threads_last_user = usr.users_id " .
+         "LEFT JOIN {pre}_members mem ON frm.squads_id = mem.squads_id AND mem.users_id = '" . $account['users_id'] . "' " .
+         "LEFT JOIN {pre}_read red ON thr.threads_id = red.threads_id AND red.users_id = '" . $account['users_id'] . "'";
+
+$where = "(frm.board_access <= '" . $account['access_board'] . "' OR mem.users_id = '" . $account['users_id'] . "') AND frm.board_pwd = '' AND thr.threads_last_time > '" . $cs_readtime . "' AND (thr.threads_last_time > red.read_since OR red.threads_id IS NULL)";
+
 $select  = 'cat.categories_name AS categories_name, cat.categories_id AS categories_id, frm.board_name AS board_name, frm.board_id AS board_id, thr.threads_headline AS threads_headline, thr.threads_last_time AS threads_last_time, thr.threads_comments AS threads_comments, thr.threads_id AS threads_id, usr.users_nick AS users_nick, usr.users_id AS users_id, usr.users_active AS users_active, usr.users_delete AS users_delete';
-$where = "frm.board_access <= '" . $account['access_board'] . "' AND frm.board_pwd = '' AND thr.threads_last_time > '" . $cs_readtime . "' AND (thr.threads_last_time > red.read_since OR red.threads_id IS NULL)"; 
+
 $order = 'thr.threads_last_time DESC'; 
 $data['threads'] = cs_sql_select(__FILE__,$from,$select,$where,$order,0,$cs_homelimit['users_homelimit']);
 
