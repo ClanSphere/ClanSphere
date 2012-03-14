5fd4def31717d6f12daa2b2915bc91c3997e5696
Hans-Joachim Piepereit <hajo@csphere.eu>
2012-03-14 13:29:24 +0100

    improved fix for country storage problem - thanks to mindcrime

--- updates/nr37_2011.3_to_r1.sql
+++ updates/nr37_2011.3_to_r1.sql
@@ -2,6 +2,27 @@ UPDATE {pre}_options SET options_value = '2011.3 r1' WHERE options_mod = 'clansp
 UPDATE {pre}_options SET options_value = '2012-03-14' WHERE options_mod = 'clansphere' AND options_name = 'version_date';
 UPDATE {pre}_options SET options_value = '80' WHERE options_mod = 'clansphere' AND options_name = 'version_id';
 
+ALTER TABLE {pre}_clans ADD clans_country2 varchar(40) NOT NULL default '';
+UPDATE {pre}_clans SET clans_country2 = clans_country;
+ALTER TABLE {pre}_clans DROP clans_country;
+ALTER TABLE {pre}_clans ADD clans_country varchar(40) NOT NULL default '';
+UPDATE {pre}_clans SET clans_country = clans_country2;
+ALTER TABLE {pre}_clans DROP clans_country2;
+
+ALTER TABLE {pre}_fightus ADD fightus_country2 varchar(40) NOT NULL default '';
+UPDATE {pre}_fightus SET fightus_country2 = fightus_country;
+ALTER TABLE {pre}_fightus DROP fightus_country;
+ALTER TABLE {pre}_fightus ADD fightus_country varchar(40) NOT NULL default '';
+UPDATE {pre}_fightus SET fightus_country = fightus_country2;
+ALTER TABLE {pre}_fightus DROP fightus_country2;
+
+ALTER TABLE {pre}_joinus ADD joinus_country2 varchar(40) NOT NULL default '';
+UPDATE {pre}_joinus SET joinus_country2 = joinus_country;
+ALTER TABLE {pre}_joinus DROP joinus_country;
+ALTER TABLE {pre}_joinus ADD joinus_country varchar(40) NOT NULL default '';
+UPDATE {pre}_joinus SET joinus_country = joinus_country2;
+ALTER TABLE {pre}_joinus DROP joinus_country2;
+
 ALTER TABLE {pre}_users ADD users_country2 varchar(40) NOT NULL default '';
 UPDATE {pre}_users SET users_country2 = users_country;
 ALTER TABLE {pre}_users DROP users_country;
