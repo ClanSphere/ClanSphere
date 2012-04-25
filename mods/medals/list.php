9b8b11d3e8183deb8adbde5ae5da0babb43a87f5
Hans-Joachim Piepereit <hajo@csphere.eu>
2012-04-25 16:11:04 +0200

    fixed pagination in medals list not working (reported by astmaih)

--- mods/medals/list.php
+++ mods/medals/list.php
@@ -3,18 +3,19 @@
 // $Id: list.php 2266 2009-03-21 10:37:39Z duRiel $
 
 $cs_lang = cs_translate('medals');
+
 $data = array();
 
 $start = empty($_GET['start']) ? 0 : (int) $_GET['start'];
 
-$tables = "users u RIGHT JOIN {pre}_medalsuser m ON u.users_id = m.users_id AND u.users_active = 1 AND u.users_delete = 0"
-        . " GROUP BY u.users_id, u.users_nick, u.users_active, u.users_delete";
-$count = cs_sql_count(__FILE__, $tables);
+$count_c = cs_sql_count(__FILE__, 'medalsuser', 0, 'users_id');
 
-$data['pages']['list'] = cs_pages('medals','list',$count,$start);
+$data['pages']['list'] = cs_pages('medals','list',$count_c,$start);
 
 $cells = "u.users_id AS users_id, u.users_nick AS users_nick, COUNT(m.medals_id) AS medals";
 $cells .= ", u.users_active AS users_active, u.users_delete AS users_delete";
+
+$tables = "users u RIGHT JOIN {pre}_medalsuser m ON u.users_id = m.users_id AND u.users_active = 1 AND u.users_delete = 0 GROUP BY u.users_id, u.users_nick, u.users_active, u.users_delete";
 
 $data['users'] = cs_sql_select(__FILE__, $tables, $cells, 0, "medals DESC", $start, $account["users_limit"]);
 
@@ -25,7 +26,5 @@ for ($i = 0; $i < $count; $i++) {
   $data['users'][$i]['user'] = cs_user($data['users'][$i]['users_id'], $data['users'][$i]['users_nick'], $data['users'][$i]['users_active'], $data['users'][$i]['users_delete']);
   
 }
-
-
 
 echo cs_subtemplate(__FILE__, $data, "medals", "list");
