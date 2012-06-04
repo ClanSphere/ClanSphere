f7399e5b37ee907f763c9bcb493bb2b4cdb9194c
Hans-Joachim Piepereit <hajo@csphere.eu>
2012-06-04 04:37:38 +0200

    updated ckeditor to 3.6.3

--- mods/ckeditor/info.php
+++ mods/ckeditor/info.php
@@ -8,7 +8,7 @@ $mod_info['released']   = $cs_main['version_date'];
 $mod_info['creator']    = 'CKSource';
 $mod_info['team']       = 'CKSource';
 $mod_info['url']        = 'www.ckeditor.com';
-$mod_info['text']       = 'Version 3.6.2';
+$mod_info['text']       = 'Version 3.6.3';
 $mod_info['icon']       = 'ckeditor';
 $mod_info['show']       = array('abcode/rte_html' => 5, 'abcode/rte_more' => 5, 'options/roots' => 5);
 $mod_info['categories'] = FALSE;
