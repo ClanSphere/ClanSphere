d783b5fa57977455e29953d724b53105b3fa5836
Andrew Tajsic <atajsic@gmail.com>
2012-06-20 20:38:32 +1000

    Language Update
    Forgot to add the updated lang file.

--- lang/English/sessions.php
+++ lang/English/sessions.php
@@ -7,6 +7,11 @@ $cs_lang['mod_info'] = 'Login Sessions';
 $cs_lang['list'] = 'List';
 
 //list
+$cs_lang['last_active'] = 'Last active';
+$cs_lang['last_IP'] = 'Last accessed IP address';
+$cs_lang['remove'] = 'Remove';
+$cs_lang['saved_logins'] = 'Your saved logins:';
+$cs_lang['red'] = 'Red';
 $cs_lang['inactivity'] = 'Sessions are automatically deleted after 30 days of inactivity.';
 $cs_lang['red'] = 'Red';
 $cs_lang['this_session'] = 'indicates this login session';
