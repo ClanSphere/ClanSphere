dd2a6989663d09ade214cf4bce4fd026c73ae30b
Hans-Joachim Piepereit <hajo@csphere.eu>
2013-01-17 03:24:43 +0100

    today is a patch day

--- updates/nr39_2011.3_patch_2_to_3.sql
+++ updates/nr39_2011.3_patch_2_to_3.sql
@@ -1,5 +1,5 @@
-UPDATE {pre}_options SET options_value = '2011.3_Patch_3_GIT' WHERE options_mod = 'clansphere' AND options_name = 'version_name';
-UPDATE {pre}_options SET options_value = '2012-09-19' WHERE options_mod = 'clansphere' AND options_name = 'version_date';
-UPDATE {pre}_options SET options_value = '83' WHERE options_mod = 'clansphere' AND options_name = 'version_id';
+UPDATE {pre}_options SET options_value = '2011.3_Patch_3' WHERE options_mod = 'clansphere' AND options_name = 'version_name';
+UPDATE {pre}_options SET options_value = '2013-01-17' WHERE options_mod = 'clansphere' AND options_name = 'version_date';
+UPDATE {pre}_options SET options_value = '84' WHERE options_mod = 'clansphere' AND options_name = 'version_id';
 
 INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('gbook', 'captcha_users', '');
