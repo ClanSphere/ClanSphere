46ea0132452744be730ba390de8025bf02fee53c
Hans-Joachim Piepereit <hajo@csphere.eu>
2013-01-17 04:41:36 +0100

    revert test succeeded

--- updates/nr40_2011.3_patch_3_to_4.sql
+++ updates/nr40_2011.3_patch_3_to_4.sql
@@ -1,5 +1,3 @@
 UPDATE {pre}_options SET options_value = '2011.3_Patch_4_GIT' WHERE options_mod = 'clansphere' AND options_name = 'version_name';
 UPDATE {pre}_options SET options_value = '2013-01-18' WHERE options_mod = 'clansphere' AND options_name = 'version_date';
 UPDATE {pre}_options SET options_value = '85' WHERE options_mod = 'clansphere' AND options_name = 'version_id';
-
-INSERT INTO {pre}_options (options_mod, options_name, options_value) VALUES ('gbook', 'captcha_users', '');
