d9787a667bc960239c0cbb92b489f7775f7a8164
Hans-Joachim Piepereit <hajo@csphere.eu>
2013-01-21 13:17:26 +0100

    Fixed files download page errors

--- mods/files/download.php
+++ mods/files/download.php
@@ -19,29 +19,35 @@ $select = 'files_count, files_name, files_mirror';
 $where = "files_id = '" . $files_id . "'"; 
 $cs_files = cs_sql_select(__FILE__,$from,$select,$where,0,0,1); 
 $files_loop = count($cs_files);
-$files_count = $cs_files['files_count'];
-$files_mirror = $cs_files['files_mirror']; 
- 
-$temp_mirror1 = explode("-----", $files_mirror);
-$mirror = explode("\n", $temp_mirror1[$mirror_id]); 
 
-$files_count = $files_count + 1;
-$files_cells = array('files_count');
-$files_save = array($files_count);
-cs_sql_update(__FILE__,'files',$files_cells,$files_save,$files_id);
+if(!empty($files_loop)) {
+  $files_count = $cs_files['files_count'];
+  $files_mirror = $cs_files['files_mirror']; 
 
-if(empty($advanced) AND isset($mirror[1]))
-{
-  header('Location: ' . $mirror[1]);
-}  
-elseif(!empty($advanced))
-{
-  $data['file']['wait'] = cs_html_img('symbols/files/wait.gif',0,0,0); 
+  $temp_mirror1 = explode("-----", $files_mirror);
+  $mirror = explode("\n", $temp_mirror1[$mirror_id]); 
 
-  /*print("<meta http-equiv=refresh content='3; URL=$downloadfile'>");
-  header("Content-disposition: attachment; filename = $downloadfile");
-  header("Content-Type: application/force-download");
-  header("Content-Transfer-Encoding: binary");
-  header("Pragma: no-cache");
-  header("Expires: 0");*/
+  $files_count = $files_count + 1;
+  $files_cells = array('files_count');
+  $files_save = array($files_count);
+  cs_sql_update(__FILE__,'files',$files_cells,$files_save,$files_id);
+
+  if(empty($advanced) AND isset($mirror[1]))
+  {
+    header('Location: ' . $mirror[1]);
+  }  
+  elseif(!empty($advanced))
+  {
+    $data['file']['wait'] = cs_html_img('symbols/files/wait.gif',0,0,0); 
+
+    /*print("<meta http-equiv=refresh content='3; URL=$downloadfile'>");
+    header("Content-disposition: attachment; filename = $downloadfile");
+    header("Content-Type: application/force-download");
+    header("Content-Transfer-Encoding: binary");
+    header("Pragma: no-cache");
+    header("Expires: 0");*/
+  }
+}
+else {
+  include_once 'mods/errors/404.php';
 }
