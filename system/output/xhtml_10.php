c0ef1c8b510a1e5221eca17a588da23168a93de6
Hans-Joachim Piepereit <hajo@csphere.eu>
2013-01-22 18:59:53 +0100

    Fixed cs_html_img not able to handle basepath img

--- system/output/xhtml_10.php
+++ system/output/xhtml_10.php
@@ -37,7 +37,13 @@ function cs_html_hr($width)
 function cs_html_img($url, $height = 0, $width = 0, $more = 0, $alt = '')
 {
   global $cs_main;
-  $internal = strpos($url, '://') === false ? $cs_main['php_self']['dirname'] : '';
+  $internal = '';
+  if(strpos($url, '://') === false) {
+    $prefix = strpos($url, $cs_main['php_self']['dirname']);
+    if($prefix === false OR $prefix > 1) {
+      $internal = $cs_main['php_self']['dirname'];
+    }
+  }
   $var = "<img src=\"" . $internal . str_replace(' ', '%20', $url) . "\" ";
   if (!empty($height) or !empty($width)) {
     $var .= 'style="';
