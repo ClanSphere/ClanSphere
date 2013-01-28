9aee1f9d80ce2232dcab36c3945e75a0591d6e44
Hans-Joachim Piepereit <hajo@csphere.eu>
2013-01-28 01:10:57 +0100

    Revert "Revert "update to fckeditor filebrowser abcode changes""
    This reverts commit 90ee957b19e3d8abda60f28134e8e0e2656d238b.

---
+++ mods/ckeditor/config_abcode.js
@@ -0,0 +1,17 @@
+﻿CKEDITOR.editorConfig = function( config )
+{
+  config.defaultLanguage  = 'en';
+  config.resize_minHeight = '250';
+  config.resize_minWidth  = '450';
+  config.width            = '100%';
+
+  config.enterMode = CKEDITOR.ENTER_P;
+  config.shiftEnterMode = CKEDITOR.ENTER_BR;
+
+  var _fmbrowser = CKEDITOR.basePath + 'filemanager/browser/default/browser.html?Mode=abcode&';
+  var _connector = CKEDITOR.basePath + 'filemanager/connectors/php/connector.php';
+
+  config.filebrowserBrowseUrl      = _fmbrowser + 'Connector=' + encodeURIComponent( _connector );
+  config.filebrowserImageBrowseUrl = _fmbrowser + 'Type=Image&Connector=' + encodeURIComponent( _connector );
+  config.filebrowserFlashBrowseUrl = _fmbrowser + 'Type=Flash&Connector=' + encodeURIComponent( _connector );
+};
