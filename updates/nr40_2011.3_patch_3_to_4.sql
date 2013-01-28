458441f1a69d1b8534440b1e4aa1cae88a8e733f
Hans-Joachim Piepereit <hajo@csphere.eu>
2013-01-28 03:03:44 +0100

    today is a patch day

--- updates/nr40_2011.3_patch_3_to_4.sql
+++ updates/nr40_2011.3_patch_3_to_4.sql
@@ -1,3 +1,3 @@
-UPDATE {pre}_options SET options_value = '2011.3_Patch_4_GIT' WHERE options_mod = 'clansphere' AND options_name = 'version_name';
-UPDATE {pre}_options SET options_value = '2013-01-18' WHERE options_mod = 'clansphere' AND options_name = 'version_date';
-UPDATE {pre}_options SET options_value = '85' WHERE options_mod = 'clansphere' AND options_name = 'version_id';
+UPDATE {pre}_options SET options_value = '2011.3_Patch_4' WHERE options_mod = 'clansphere' AND options_name = 'version_name';
+UPDATE {pre}_options SET options_value = '2013-01-28' WHERE options_mod = 'clansphere' AND options_name = 'version_date';
+UPDATE {pre}_options SET options_value = '86' WHERE options_mod = 'clansphere' AND options_name = 'version_id';
