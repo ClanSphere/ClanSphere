36363ccd9a4d90b1b8dc2bf43e4a10e930cefd30
Hans-Joachim Piepereit <hajo@csphere.eu>
2013-05-20 18:19:20 +0200

    today is a patch day

---
+++ updates/nr42_2011.4_to_2011.4.1.sql
@@ -0,0 +1,3 @@
+UPDATE {pre}_options SET options_value = '2011.4.1' WHERE options_mod = 'clansphere' AND options_name = 'version_name';
+UPDATE {pre}_options SET options_value = '2013-05-20' WHERE options_mod = 'clansphere' AND options_name = 'version_date';
+UPDATE {pre}_options SET options_value = '90' WHERE options_mod = 'clansphere' AND options_name = 'version_id';
