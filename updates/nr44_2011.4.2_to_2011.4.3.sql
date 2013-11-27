61d37fa30e3d3f03192ebdaac369fb9c69551c2d
Hans-Joachim Piepereit <hajo@csphere.eu>
2013-11-27 15:29:18 +0100

    updated version information to beta

---
+++ updates/nr44_2011.4.2_to_2011.4.3.sql
@@ -0,0 +1,3 @@
+UPDATE {pre}_options SET options_value = '2011.4.3-beta' WHERE options_mod = 'clansphere' AND options_name = 'version_name';
+UPDATE {pre}_options SET options_value = '2013-11-27' WHERE options_mod = 'clansphere' AND options_name = 'version_date';
+UPDATE {pre}_options SET options_value = '93' WHERE options_mod = 'clansphere' AND options_name = 'version_id';
