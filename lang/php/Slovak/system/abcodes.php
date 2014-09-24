c184c02ff7e675368710beeb95775d8812392b22
callus <jan.callus@gmail.com>
2014-09-24 11:00:47 +0200

    Create abcodes.php
    Slovak version

---
+++ lang/php/Slovak/system/abcodes.php
@@ -0,0 +1,61 @@
+<?php
+
+// $Id$
+
+$cs_lang['abclist'] = 'Zoznam';
+
+$cs_lang['font_size'] = 'Veľkosť písma';
+$cs_lang['font_color'] = 'Farba písma';
+$cs_lang['text_align'] = 'Zarovnanie';
+
+$cs_lang['tiny'] = 'Veľmi malé';
+$cs_lang['small'] = 'malé';
+$cs_lang['medium'] = 'stredná';
+$cs_lang['large'] = 'veľké';
+$cs_lang['giant'] = 'Veľmi veľké';
+
+$cs_lang['aqua'] = 'Svetlomodrá';
+$cs_lang['black'] = 'Čierna';
+$cs_lang['blue'] = 'Modrá';
+$cs_lang['fuchsia'] = 'ružová';
+$cs_lang['gray'] = 'Šedá';
+$cs_lang['green'] = 'Zelená';
+$cs_lang['lime'] = 'Limetková';
+$cs_lang['maroon'] = 'Hnedočervená';
+$cs_lang['navy'] = 'Tmavomodrá';
+$cs_lang['olive'] = 'Olivová';
+$cs_lang['orange'] = 'Oranžová';
+$cs_lang['purple'] = 'Ružová';
+$cs_lang['red'] = 'Červená';
+$cs_lang['silver'] = 'Striborná';
+$cs_lang['teal'] = 'teal';
+$cs_lang['white'] = 'Biela';
+$cs_lang['yellow'] = 'Žltá'; 
+
+$cs_lang['left'] = 'Vľavo';
+$cs_lang['center'] = 'Stred';
+$cs_lang['right'] = 'Vpravo';
+$cs_lang['justify'] = 'Zarovnať';
+
+$cs_lang['bold'] = 'Tučné';
+$cs_lang['italic'] = 'Kurzíva';
+$cs_lang['underline'] = 'Podčiarknuté';
+$cs_lang['strike'] = 'Prečiarknuté';
+
+$cs_lang['headline'] = 'Nadpis';
+$cs_lang['horizontal_rule'] = 'Horizontálne pravidlá';
+$cs_lang['indent'] = 'Zarážka';
+$cs_lang['quote'] = 'Úvodzovka';
+$cs_lang['php'] = 'PHP zdrojový kód';
+$cs_lang['list'] = 'Zoznam';
+$cs_lang['image'] = 'Obrázok';
+$cs_lang['link'] = 'Odkaz';
+$cs_lang['mail'] = 'Email';
+$cs_lang['clip'] = 'Rozbaliť text';
+$cs_lang['thread'] = 'Vlákno Fóra';
+$cs_lang['html'] = 'HTML';
+
+$cs_lang['more'] = 'viac...';
+
+$cs_lang['rows'] = 'Riadky';
+$cs_lang['default'] = 'Predvolené';
