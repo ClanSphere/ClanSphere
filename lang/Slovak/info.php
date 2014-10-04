032b143c7aeede79c7e40d91cbafba556c405f2c
callus <jan.callus@gmail.com>
2014-10-04 14:28:32 +0200

    Slovak version

--- lang/Slovak/info.php
+++ lang/Slovak/info.php
@@ -5,8 +5,8 @@
 $mod_info['name']    = 'Slovak';
 $mod_info['version'] = $cs_main['version_name'];
 $mod_info['released'] = $cs_main['version_date'];
-$mod_info['creator']  = 'calluSVK';
-$mod_info['team']    = 'calluSVK team';
-$mod_info['url']    = 'www.ih8u2.eu';
+$mod_info['creator']  = 'callus';
+$mod_info['team']    = 'jan.callus@gmail.com';
+$mod_info['url']    = '';
 $mod_info['text']    = 'Official translation package';
 $mod_info['symbol']    = 'sk';
