cc31bd303ee252c94507cebed026ce3881d76005
sgraewe <stefan.graewe@gmail.com>
2016-08-30 19:20:55 +0200

    Downgrade bootstrap to version 3.3.7

--- mods/bootstrap/startup.php
+++ mods/bootstrap/startup.php
@@ -1,4 +1,5 @@
 <?php
 
-cs_scriptload('bootstrap', 'stylesheet', 'dist/css/bootstrap.min.css', 1);
-cs_scriptload('bootstrap', 'javascript', 'dist/js/bootstrap.min.js', 1);
+// cs_scriptload('bootstrap', 'stylesheet', 'css/bootstrap-theme.min.css', 1);
+cs_scriptload('bootstrap', 'stylesheet', 'css/bootstrap.min.css', 1);
+cs_scriptload('bootstrap', 'javascript', 'js/bootstrap.min.js', 1);
